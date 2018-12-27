6d313159dab70416fe6da38972d8fb2a21580991
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-27 03:25:32 -0200

    inicio da aula 02 do módulo 02 da parte 02

---
+++ Carreiras/01-Desenvolvedor-CSharp/02-02-02-UsandoNossaClasse/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_02_02_UsandoNossaClasse
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/02-02-02-UsandoNossaClasse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_02_02_UsandoNossaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
