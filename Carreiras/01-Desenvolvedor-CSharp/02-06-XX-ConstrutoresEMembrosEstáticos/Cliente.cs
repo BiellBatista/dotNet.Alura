0c34deebf9fe68b110e3d4578b2ccb4a2c8066d1
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-28 01:08:47 -0200

    conclusao da parte 2

---
+++ Carreiras/01-Desenvolvedor-CSharp/02-06-XX-ConstrutoresEMembrosEstáticos/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_06_XX_ConstrutoresEMembrosEstáticos
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        private string _cpf; // o _ serve para mostrar que o atributo é privado
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                // Escrevo minha lógica de validação de CPF
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/02-06-XX-ConstrutoresEMembrosEstáticos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_06_XX_ConstrutoresEMembrosEstáticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.ReadLine();
+        }
+    }
+}
