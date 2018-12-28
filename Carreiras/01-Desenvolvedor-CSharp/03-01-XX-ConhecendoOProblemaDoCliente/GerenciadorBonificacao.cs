74cfcf95896e87a87b1930f78a8c32ff2a3724a3
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-28 21:21:13 -0200

    conclusão da aula 01 da parte 3

---
+++ Carreiras/01-Desenvolvedor-CSharp/03-01-XX-ConhecendoOProblemaDoCliente/GerenciadorBonificacao.cs
@@ -0,0 +1,29 @@
+﻿using _03_01_XX_ConhecendoOProblemaDoCliente.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_01_XX_ConhecendoOProblemaDoCliente
+{
+    class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/03-01-XX-ConhecendoOProblemaDoCliente/Program.cs
@@ -0,0 +1,40 @@
+﻿using _03_01_XX_ConhecendoOProblemaDoCliente.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_01_XX_ConhecendoOProblemaDoCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "405.245.412-45";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "412.347.521-47";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonigicações: R$" + gerenciador.GetTotalBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
