c1d9d8605c25b31a540700b59c2460332c6ce371
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-27 23:45:56 -0200

    Conclusão da aula 03 da parte 02

---
+++ Carreiras/01-Desenvolvedor-CSharp/02-03-02-NossaPrimeiraFuncao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_02_NossaPrimeiraFuncao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.ReadLine();
+        }
+    }
+}
