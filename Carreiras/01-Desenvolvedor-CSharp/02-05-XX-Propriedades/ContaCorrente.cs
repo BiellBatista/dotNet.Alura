e5994a75ea6201a6f785a9356be2dfe6fa345bf0
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-28 00:50:56 -0200

    conclusão da aula 05 da parte 02

---
+++ Carreiras/01-Desenvolvedor-CSharp/02-05-XX-Propriedades/ContaCorrente.cs
@@ -0,0 +1,56 @@
+﻿namespace _02_05_XX_Propriedades
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100; //o _ serve para mostrar que o atributo é privado
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/02-05-XX-Propriedades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_05_XX_Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Gabriel";
+            cliente.CPF = "405.214.459-45";
+            cliente.Profissao = "Desenvovledor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
