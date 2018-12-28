a6dce75a8dabdf164993238ff95509c083f95abf
Gabriel Batista <gabriel.texteis@gmail.com>
2018-12-28 00:23:52 -0200

    Conclusao da aula 04 da parte 02

---
+++ Carreiras/01-Desenvolvedor-CSharp/02-04-XX-EntendendoNamespaces/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_04_XX_EntendendoNamespaces
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/02-04-XX-EntendendoNamespaces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_04_XX_EntendendoNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriel = new Cliente();
+
+            gabriel.nome = "Gabriel";
+            gabriel.profissao = "Desenvolvedor C#";
+            gabriel.cpf = "405.324.214.-56";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriel;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            Console.WriteLine(gabriel.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.ReadLine();
+        }
+    }
+}
