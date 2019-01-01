53dde100936c067de575bb5ab7adf15147520202
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-01 17:25:31 -0200

    conclusao da aula 01 da parte 06

--- Carreiras/01-Desenvolvedor-CSharp/05-XX-XX-BibliotecasDLLsDocumentaçãoUsandoNuGet/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/05-XX-XX-BibliotecasDLLsDocumentaçãoUsandoNuGet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _05_XX_XX_BibliotecasDLLs.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace _05_XX_XX_BibliotecasDLLsDocumentaçãoUsandoNuGet
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
         }
     }
 }
