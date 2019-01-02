570c103137e85f891b578fd28b005ac385a8af5f
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-02 00:47:31 -0200

    conclusão da aula 03 da parte 06

--- Carreiras/01-Desenvolvedor-CSharp/06-XX-XX-Strings/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/06-XX-XX-Strings/Program.cs
@@ -10,16 +10,23 @@ namespace _06_XX_XX_Strings
     {
         static void Main(string[] args)
         {
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
-            ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL(urlParametros);
-            string valor = extrator.GetValor("moedaDestino");
-            string valorDaMoedaOrigem = extrator.GetValor("moedaOrigem");
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
 
-            Console.WriteLine("Valor de moedaDestino: " + valor); //escreve Valor de moedaDestino: dolar
-            Console.WriteLine("Valor de moedaOrigem: " + valorDaMoedaOrigem); //escreve Valor de moedaOrigem: real
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com")); //verifica se uma string começa com uma string
+            Console.WriteLine(urlTeste.EndsWith("cambio")); //verifica se uma string termina com uma string
+            Console.WriteLine(urlTeste.Contains("cambio")); //verifica se uma string contem outra string
+            Console.WriteLine(indiceByteBank >= 0);
+            Console.ReadLine();
 
 
+            //string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            //ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL(urlParametros);
+            //string valor = extrator.GetValor("moedaDestino");
+            //string valorDaMoedaOrigem = extrator.GetValor("moedaOrigem");
 
+            //Console.WriteLine("Valor de moedaDestino: " + valor); //escreve Valor de moedaDestino: dolar
+            //Console.WriteLine("Valor de moedaOrigem: " + valorDaMoedaOrigem); //escreve Valor de moedaOrigem: real
 
             //string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
             //ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL(urlParametros);
