63afc55965549b89e2c18974f6e591a59a3fd966
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-02 00:20:52 -0200

    conclusão da aula 02 da parte 06

---
+++ Carreiras/01-Desenvolvedor-CSharp/06-XX-XX-Strings/ExtratorValorArgumentosURL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_XX_XX_Strings
+{
+    public class ExtratorValorArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+
+        public ExtratorValorArgumentosURL(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentException("O argumento URL não pode ser nujll ou uma string vazia", nameof(url));
+
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+        }
+
+        // moedaOrigem=real&moedaDestino=dolar
+        public string GetValor(string nomeParametro)
+        {
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            return _argumentos.Substring(indiceTermo + termo.Length);
+        }
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/06-XX-XX-Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_XX_XX_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtratorValorArgumentosURL extrator = new ExtratorValorArgumentosURL("");
+
+            string url = "pagina?argumentos";
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+            Console.WriteLine(url.Substring(0));
+
+            Console.ReadLine();
+        }
+    }
+}
