7b1606b9a9150e60366dbc19c1022a5db8bf8713
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-01 23:19:21 -0200

    conclusão da parte 05

--- Carreiras/01-Desenvolvedor-CSharp/05-XX-XX-BibliotecasDLLsDocumentaçãoUsandoNuGet/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/05-XX-XX-BibliotecasDLLsDocumentaçãoUsandoNuGet/Program.cs
@@ -1,7 +1,9 @@
 ﻿using _05_XX_XX_BibliotecasDLLs.Modelos;
 using _05_XX_XX_BibliotecasDLLs.Modelos.Funcionarios;
+using Humanizer;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,26 @@ namespace _05_XX_XX_BibliotecasDLLsDocumentaçãoUsandoNuGet
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
-            Console.WriteLine(conta.Numero);
+            DateTime dataFimPagamento = new DateTime(2022, 8, 17);
+            DateTime dataCorrente = DateTime.Now;
+            TimeSpan diferenca = dataFimPagamento.Subtract(dataCorrente);
+
+            //string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca, 1, new CultureInfo("pt-Br"));
+
+            Console.WriteLine(mensagem);
             Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                return timeSpan.Days + " meses";
+            }
+
+            return timeSpan.Days + " dias";
         }
     }
 }
