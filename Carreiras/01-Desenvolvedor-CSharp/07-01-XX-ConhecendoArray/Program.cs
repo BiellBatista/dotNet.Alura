373011fa4c769cd5fa0a4eee222029db6ccbee25
Gabriel Batista <gabriel.batista@kognita.com>
2019-01-03 14:28:35 -0200

    conclusão do módulo 03 da aula 01 da parte 07

---
+++ Carreiras/01-Desenvolvedor-CSharp/07-01-XX-ConhecendoArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_01_XX_ConhecendoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            Console.ReadLine();
+        }
+    }
+}
