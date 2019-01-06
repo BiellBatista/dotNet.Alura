bd4c749146acad93972c34c07c68bd701737a076
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-06 08:44:02 -0200

    conclusão da aula 02 da parte 08

--- Carreiras/01-Desenvolvedor-CSharp/08-XX-XX-ListTLambdaLinq/Extensoes/ListExtensoes.cs
+++ Carreiras/01-Desenvolvedor-CSharp/08-XX-XX-ListTLambdaLinq/Extensoes/ListExtensoes.cs
@@ -7,13 +7,16 @@ using System.Threading.Tasks;
 namespace _08_XX_XX_ListTLambdaLinq.Extensoes
 {
     //uma classe genérica para que o método de extensão seja genérico
-    public static class ListExtensoes<T>
+    //Não posos colocar um método de exntesão dentro de uma classe genérica, porque o compilador não saberá qual tipo de classe o método é extendido 
+    //public static class ListExtensoes<T>
+    public static class ListExtensoes
     {
         /**
          * Método de extensão serve para criar um novo método estátio em uma classe já definida, é necessário colocar this no parametro que irá receber a classe.
-         * Ele só serve para uma lista de inteiro
+         * Ele só serve para uma lista de inteiro.
          **/
-        public static void AdicionarVarios(this List<T> listaDeInteiros, params T[] itens)
+         //Estou criando um método genérico extensivel. POsos criar um método de extensão sem ser genérico
+        public static void AdicionarVarios<T>(this List<T> listaDeInteiros, params T[] itens)
         {
             foreach (T item in itens)
                 listaDeInteiros.Add(item);
