b393f7132eaf0071ce77934675b4bcec726eab80
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-06 05:48:10 -0200

    inicio parte 08

---
+++ Carreiras/01-Desenvolvedor-CSharp/08-XX-XX-ListTLambdaLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_XX_XX_ListTLambdaLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
