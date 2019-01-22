85b6b14a42b465177a57f0dccb6fc47b5d44eb93
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-22 21:46:27 -0200

    Conclusão da atividade 03 da aula 01 da parte 09

---
+++ Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/Program.cs
@@ -0,0 +1,17 @@
+﻿using _09_XX_XX_EntradaSaídaStreams.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_XX_XX_EntradaSaídaStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new ContaCorrente(456, 46546);
+        }
+    }
+}
