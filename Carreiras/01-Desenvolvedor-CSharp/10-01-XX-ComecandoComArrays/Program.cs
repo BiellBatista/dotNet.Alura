4a61617f34603ef2a6c0976c81835b3bab141826
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-26 17:14:23 -0200

    iniciando o curso C# Collections parte 1 - Listas, arrays, listas ligadas, dicionários e conjuntos

---
+++ Carreiras/01-Desenvolvedor-CSharp/10-01-XX-ComecandoComArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_01_XX_ComecandoComArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
