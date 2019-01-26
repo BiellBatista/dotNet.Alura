76c79183fb98336025c0bab616e354501c8e2d12
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-26 11:43:02 -0200

    conclusão do módulo 02 da aula 03 da parte 09

--- Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/Program.cs
@@ -29,12 +29,42 @@ namespace _09_XX_XX_EntradaSaídaStreams
                     while (!leitor.EndOfStream)
                     {
                         var linha = leitor.ReadLine(); //leia linha por linha
-                        Console.WriteLine(linha);
+                        var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                        Console.WriteLine($"Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}");
+                        //Console.WriteLine(linha);
                     }
                 }
             }
 
             Console.ReadLine();
+        }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(' '); //quebrando uma string a partir de um caracter
+            /*
+             * Gabriel de Almeida Batista
+             * campo1  2  campo3  campo4
+             */
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            var agenciaComoInt = int.Parse(agencia);
+            var numeroComoInt = int.Parse(numero);
+            var saldoComoDouble = double.Parse(saldo);
+
+            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+            resultado.Depositar(saldoComoDouble);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            resultado.Titular = titular;
+
+            return resultado;
         }
     }
 }
