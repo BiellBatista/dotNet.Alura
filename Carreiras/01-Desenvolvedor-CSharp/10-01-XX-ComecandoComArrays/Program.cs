e4ebd3bc82373962a28e0112114aa27cf594a8a9
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-26 17:40:27 -0200

    conclusão do módulo 03 da aula 01 das collections

--- Carreiras/01-Desenvolvedor-CSharp/10-01-XX-ComecandoComArrays/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/10-01-XX-ComecandoComArrays/Program.cs
@@ -10,6 +10,46 @@ namespace _10_01_XX_ComecandoComArrays
     {
         static void Main(string[] args)
         {
+            string aulaIntro = "Introdução às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSet = "Trabalhando com Conjuntos";
+
+            //string[] aulas = new string[]
+            //{
+            //    aulaIntro,
+            //    aulaModelando,
+            //    aulaSet
+            //};
+
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSet;
+
+            Console.WriteLine(aulas);
+            Imprimir(aulas);
+            Console.WriteLine(aulas[0]);
+            Console.WriteLine(aulas[aulas.Length - 1]);
+
+            aulaIntro = "Trabalhando com Arrays";
+            Imprimir(aulas);
+
+            aulas[0] = "Trabalhando com Arrays";
+            Imprimir(aulas);
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(string[] aulas)
+        {
+            //foreach (var aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+
+            for (int i = 0; i < aulas.Length; i++)
+            {
+                Console.WriteLine(aulas[i]);
+            }
         }
     }
 }
