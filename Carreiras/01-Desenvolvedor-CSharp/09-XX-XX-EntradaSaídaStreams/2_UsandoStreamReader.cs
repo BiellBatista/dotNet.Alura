61d1d9c21ee5210fcb75588a9d1785ebd3611873
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-26 12:13:50 -0200

    separando os métodos e renomando os arquivos para ficarem ordenados

---
+++ Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/2_UsandoStreamReader.cs
@@ -0,0 +1,63 @@
+﻿using _09_XX_XX_EntradaSaídaStreams.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_XX_XX_EntradaSaídaStreams
+{
+    partial class Program
+    {
+        static void UsandoStreamReader()
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                using (var leitor = new StreamReader(fluxoDeArquivo))
+                { //leitor do arquivo, devo passar o fluxo que será analisado
+                  //var linha = leitor.ReadToEnd(); //leia até o fim
+                  //var linha = leitor.Read(); //leia o primeiro byte
+
+                    while (!leitor.EndOfStream)
+                    {
+                        var linha = leitor.ReadLine(); //leia linha por linha
+                        var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                        Console.WriteLine($"{contaCorrente.Titular.Nome} : Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}");
+                        //Console.WriteLine(linha);
+                    }
+                }
+            }
+        }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(','); //quebrando uma string a partir de um caracter
+            /*
+             * Gabriel de Almeida Batista
+             * campo1  2  campo3  campo4
+             */
+            var agencia = campos[0];
+            var numero = campos[1];
+            var saldo = campos[2].Replace('.', ',');
+            var nomeTitular = campos[3];
+
+            var agenciaComoInt = int.Parse(agencia);
+            var numeroComoInt = int.Parse(numero);
+            var saldoComoDouble = double.Parse(saldo);
+
+            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
+            resultado.Depositar(saldoComoDouble);
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            resultado.Titular = titular;
+
+            return resultado;
+        }
+    }
+}
---
+++ Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/3_CriandoArquivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_XX_XX_EntradaSaídaStreams
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,78945,4785.50,Gustavo Santos";
+                var encoding = Encoding.UTF8; //encoding que irei usar para tranformar a cadeia de string
+                var bytes = encoding.GetBytes(contaComoString); //transformando para byte
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
