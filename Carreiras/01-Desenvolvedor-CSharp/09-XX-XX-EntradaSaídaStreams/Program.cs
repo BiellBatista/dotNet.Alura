06a7cc06ae266788c05d67a687ee2e0047f5f6d6
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-26 12:14:12 -0200

    conclusão do módulo 04 da aula 03 da parte 09

--- Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/09-XX-XX-EntradaSaídaStreams/Program.cs
@@ -16,55 +16,9 @@ namespace _09_XX_XX_EntradaSaídaStreams
         static void Main(string[] args)
         {
             //LidandoComFileStreamDiretamente(); //Este método está em outro arquivo, mas ambos são a classe Program
-
-            var enderecoDoArquivo = "contas.txt";
-
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            {
-                using (var leitor = new StreamReader(fluxoDeArquivo))
-                { //leitor do arquivo, devo passar o fluxo que será analisado
-                  //var linha = leitor.ReadToEnd(); //leia até o fim
-                  //var linha = leitor.Read(); //leia o primeiro byte
-
-                    while (!leitor.EndOfStream)
-                    {
-                        var linha = leitor.ReadLine(); //leia linha por linha
-                        var contaCorrente = ConverterStringParaContaCorrente(linha);
-
-                        Console.WriteLine($"{contaCorrente.Titular.Nome} : Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}");
-                        //Console.WriteLine(linha);
-                    }
-                }
-            }
+            CriarArquivo(); //Este método está em outro arquivo, mas ambos são a classe Program
 
             Console.ReadLine();
-        }
-
-        static ContaCorrente ConverterStringParaContaCorrente(string linha)
-        {
-            string[] campos = linha.Split(','); //quebrando uma string a partir de um caracter
-            /*
-             * Gabriel de Almeida Batista
-             * campo1  2  campo3  campo4
-             */
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.', ',');
-            var nomeTitular = campos[3];
-
-            var agenciaComoInt = int.Parse(agencia);
-            var numeroComoInt = int.Parse(numero);
-            var saldoComoDouble = double.Parse(saldo);
-
-            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
-            resultado.Depositar(saldoComoDouble);
-
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-
-            resultado.Titular = titular;
-
-            return resultado;
         }
     }
 }
