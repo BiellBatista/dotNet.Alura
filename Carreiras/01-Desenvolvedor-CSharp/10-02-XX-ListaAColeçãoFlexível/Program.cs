c6169284bc7826e98a01e1ce0cdc7d88c16beb50
Gabriel Batista <gabriel.texteis@gmail.com>
2019-01-27 01:46:11 -0200

    conclusão do módulo 03 da aula 02 da seção "collections" parte 01

--- Carreiras/01-Desenvolvedor-CSharp/10-02-XX-ListaAColeçãoFlexível/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/10-02-XX-ListaAColeçãoFlexível/Program.cs
@@ -10,6 +10,50 @@ namespace _10_02_XX_ListaAColeçãoFlexível
     {
         static void Main(string[] args)
         {
+            var aulaIntro = new Aula("Introdução às Coleções", 20);
+            var aulaModelando = new Aula("Modelando a Classe Aula", 18);
+            var aulaSet = new Aula("Trabalhando com Conjuntos", 16);
+
+            List<Aula> aulas = new List<Aula>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSet);
+            //aulas.Add("Conclusão");
+
+            Imprimir(aulas);
+
+            aulas.Sort(); //ordena a lista
+            Imprimir(aulas);
+            aulas.Sort((este, outro) => este.Tempo.CompareTo(outro.Tempo)); //ordenando pelo tempo
+            Imprimir(aulas);
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(List<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+                Console.WriteLine(aula.Titulo);
+        }
+
+        private static void Imprimir(List<string> aulas)
+        {
+            //foreach (var aula in aulas)
+            //    Console.WriteLine(aula);
+
+            //for (int i = 0; i < aulas.Count; i++)
+            //    Console.WriteLine(aulas[i]);
+            //para cada "aula" execute "=>" o código "{}"
+            aulas.ForEach(aula =>
+            {
+                Console.WriteLine(aula);
+            });
+            //equivalente ao for e foreach
+        }
+
+        private static void OperacaoComLista()
+        {
             string aulaIntro = "Introdução às Coleções";
             string aulaModelando = "Modelando a Classe Aula";
             string aulaSet = "Trabalhando com Conjuntos";
@@ -48,23 +92,30 @@ namespace _10_02_XX_ListaAColeçãoFlexível
 
             //removendo um intervalo de elementos da lista (inicío, fim)
             clone.RemoveRange(clone.Count - 2, clone.Count - 1);
-
-            Console.ReadLine();
         }
 
-        private static void Imprimir(List<string> aulas)
+        class Aula : IComparable
         {
-            //foreach (var aula in aulas)
-            //    Console.WriteLine(aula);
+            public string Titulo { get; set; }
+            public int Tempo { get; set; }
 
-            //for (int i = 0; i < aulas.Count; i++)
-            //    Console.WriteLine(aulas[i]);
-            //para cada "aula" execute "=>" o código "{}"
-            aulas.ForEach(aula =>
+            public Aula(string titulo, int tempo)
             {
-                Console.WriteLine(aula);
-            });
-            //equivalente ao for e foreach
+                Titulo = titulo;
+                Tempo = tempo;
+            }
+
+            public override string ToString()
+            {
+                return $"O título da aula é {Titulo} e possui a duração de {Tempo}";
+            }
+
+            public int CompareTo(object obj)
+            {
+                var tmp = obj as Aula;
+
+                return this.Titulo.CompareTo(tmp.Titulo);
+            }
         }
     }
 }
