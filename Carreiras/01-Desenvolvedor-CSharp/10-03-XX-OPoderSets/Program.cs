004c4c1ce07d6c7307a6274275be3c961981396a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-02 13:36:04 -0200

    conclusão do módulo 01 da aula 03 da seção "collections" parte 01

--- Carreiras/01-Desenvolvedor-CSharp/10-03-XX-OPoderSets/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/10-03-XX-OPoderSets/Program.cs
@@ -10,6 +10,58 @@ namespace _10_03_XX_OPoderSets
     {
         static void Main(string[] args)
         {
+            //SETS = CONJUNTOS
+
+            //Duas propriedades do Set
+            //1. Não permite duplicidade
+            //2. Os elementos não são mantidos em ordem específica
+
+            //declarando set de alunos
+            ISet<string> alunos = new HashSet<string>();
+            //adicionando: vanessa, ana, rafael
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            //imprimindo
+            //Console.WriteLine(alunos);
+            Console.WriteLine(string.Join(",", alunos));
+            //Imprimiu: Vanessa Tonini, Ana Losnak,Rafael Nercessian
+
+            //adicionando: priscila, rollo, fabio
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            Console.WriteLine(string.Join(",", alunos));
+            //Imprimiu: Vanessa Tonini, Ana Losnak,Rafael Nercessian, Priscila Stuani,Rafael Rollo, Fabio Gushiken
+
+            //removendo aluno e adicionando outro
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Olibeira");
+            Console.WriteLine(string.Join(",", alunos));
+            //imprimiu: Vanessa Tonini,Marcelo Olibeira, Rafael Nercessian,Priscila Stuani, Rafael Rollo,Fabio Gushiken
+            //repare que o Marcelo entrou no mesmo lugar que era ocupado pela ana
+
+            //adicionando o mesmo aluno
+            alunos.Add("Fabio Gushiken");
+            Console.WriteLine(string.Join(",", alunos));
+            //imprimiu: Vanessa Tonini,Marcelo Olibeira,Rafael Nercessian,Priscila Stuani,Rafael Rollo,Fabio Gushiken
+
+            /**
+             * Qual a vantagem do ISet<> sobre o IList<>?
+             * 1. A busca em ISet<> é mais rápida, porque no IList<> é necessário percorer a lista toda para verificar se o elemento que está sendo buscado contém na lista, enquanto que no ISet<> é necessário olhar a tabela Hash (maior que uma lista), porém redireciona para o lugar ocupado do elemento. O HashSet possui uma grande escalabilidade, porém ele ocupa muita memória em comparação com uma lista.
+             */
+
+            //ordenando: sort
+            //alunos.Sort(); não é possível, porque ISet<> não possui o método sort
+
+            //copiando o ISet<> para uma IList<>
+            List<string> alunosEmLista = new List<string>(alunos);
+            alunosEmLista.Sort();
+            Console.WriteLine(string.Join(",", alunosEmLista));
+            //imprimiu: Fabio Gushiken,Marcelo Olibeira,Priscila Stuani,Rafael Nercessian,Rafael Rollo,Vanessa Tonini
+
+            Console.ReadLine();
         }
     }
 }
