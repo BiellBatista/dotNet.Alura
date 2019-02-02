247cd7609bf0e4eb069bedcad6c8d260946561b2
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-02 13:15:51 -0200

    inicio da aula 03

---
+++ Carreiras/01-Desenvolvedor-CSharp/10-03-XX-OPoderSets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_03_XX_OPoderSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
