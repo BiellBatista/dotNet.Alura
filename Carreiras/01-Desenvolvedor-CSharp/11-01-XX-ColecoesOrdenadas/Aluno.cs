844bd7b88abff133f716fa40e50efcc0afbfe3a0
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-03 15:24:54 -0200

    conclusão do módulo 04 da aula 04 da seção "collections" parte 02

---
+++ Carreiras/01-Desenvolvedor-CSharp/11-01-XX-ColecoesOrdenadas/Aluno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_01_XX_ColecoesOrdenadas
+{
+    class Aluno
+    {
+        private string _nome;
+        private int _numeroMatricula;
+
+        public Aluno(string nome, int numeroMatricula)
+        {
+            _nome = nome;
+            _numeroMatricula = numeroMatricula;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public int NumeroMatricula
+        {
+            get { return _numeroMatricula; }
+            set { _numeroMatricula = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {_nome}, Matrícula: {_numeroMatricula}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+
+            if (outro == null)
+                return false;
+            else
+                return Nome.Equals(outro.Nome);
+        }
+
+        //importante: rapidez da busca depende do código de dispersão!
+        public override int GetHashCode()
+        {
+            //estou usando o nome, porque ele é usado no equals
+            return Nome.GetHashCode();
+        }
+
+        //IMPORTANTE! SEMPRE QUE EU SOBRESCREVER O EQUALS, DEVO SOBRESCREVER O HASHCODE
+
+        //Dois objetos que são iguais, possuem o mesmo hash code.
+        //PORÉM, o contrário não é verdadeiro:
+        //Dois objetos com mesmo hash codes não são necessaramente iguais!
+    }
+}
--- Carreiras/01-Desenvolvedor-CSharp/11-01-XX-ColecoesOrdenadas/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/11-01-XX-ColecoesOrdenadas/Program.cs
@@ -10,6 +10,43 @@ namespace _11_01_XX_ColecoesOrdenadas
     {
         static void Main(string[] args)
         {
+            
+
+            Console.ReadLine();
+        }
+
+        internal static void SortedList()
+        {
+            //vamos criar um dicionário de alunos
+            IDictionary<string, Aluno> alunos = new Dictionary<string, Aluno>();
+            alunos.Add("VT", new Aluno("Venssa", 34672));
+            alunos.Add("AL", new Aluno("Ana", 5617));
+            alunos.Add("RN", new Aluno("Rafael", 17645));
+            alunos.Add("WM", new Aluno("Wanderson", 11287));
+
+            foreach (var aluno in alunos)
+                Console.WriteLine(aluno);
+            //vamos remover AL
+            alunos.Remove("AL");
+            //vamos adicionar: MO
+            alunos.Add("MO", new Aluno("Marcelo", 12345));
+            Console.WriteLine();
+
+            foreach (var aluno in alunos)
+                Console.WriteLine(aluno);
+
+            //apresentando o SortedList (Dicionário Ordenado). Ele ordena em ordem alfabética, crescente... com base na chave
+            //o nome é SortedList<>(), porque as chaves são armazenadas em uma lista
+            IDictionary<string, Aluno> sorted = new SortedList<string, Aluno>();
+            sorted.Add("VT", new Aluno("Venssa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            Console.WriteLine();
+
+            foreach (var item in sorted)
+                Console.WriteLine(item);
         }
     }
 }
