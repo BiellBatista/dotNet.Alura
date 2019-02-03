82211f78a401601b3b44784946dd03bb3a79da39
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-03 20:58:22 -0200

    conclusão da aula 03 da seção "collections" parte 02

--- Carreiras/01-Desenvolvedor-CSharp/11-03-XX-ConsultandoLINQ/Program.cs
+++ Carreiras/01-Desenvolvedor-CSharp/11-03-XX-ConsultandoLINQ/Program.cs
@@ -10,15 +10,15 @@ namespace _11_03_XX_ConsultandoLINQ
     {
         static void Main(string[] args)
         {
-            //Problema: obter os nomes dos meses do ano que tem 31 dias, em maiúsculo e em ordem alfabética
-
-
+            ConsultandoCollections();
+            ConsultasLINQ();
 
             Console.ReadLine();
         }
 
         internal static void ConsultandoCollections()
         {
+            //Problema: obter os nomes dos meses do ano que tem 31 dias, em maiúsculo e em ordem alfabética
             List<Mes> meses = new List<Mes>()
             {
                new Mes("Janeiro     ", 31),
@@ -40,6 +40,41 @@ namespace _11_03_XX_ConsultandoLINQ
             foreach (var mes in meses)
                 if (mes.Dias == 31)
                     Console.WriteLine(mes.Nome.ToUpper());
+        }
+
+        internal static void ConsultasLINQ()
+        {
+            //Problema: obter os nomes dos meses do ano que tem 31 dias, em maiúsculo e em ordem alfabética
+            List<Mes> meses = new List<Mes>()
+            {
+               new Mes("Janeiro     ", 31),
+               new Mes("Fevereiro   ", 28),
+               new Mes("Março       ", 31),
+               new Mes("Abril       ", 30),
+               new Mes("Maio        ", 31),
+               new Mes("Junho       ", 30),
+               new Mes("Julho       ", 31),
+               new Mes("Agosto      ", 31),
+               new Mes("Setembro    ", 30),
+               new Mes("Outubro     ", 31),
+               new Mes("Novembro    ", 30),
+               new Mes("Dezembro    ", 31)
+            };
+
+            //LINQ = CONSULTA INTEGRADA À LINGUAGEM
+            //IEnumerable<Mes> consulta = meses
+            //                                .Where(m => m.Dias == 31)
+            //                                .OrderBy(m => m.Nome)
+            //                                .Select(m => m.Nome.ToUpper());
+            //.Select(); serve para modificar o resultado de uma busca
+            //deve ser string, porque estou selecionando uma string
+            IEnumerable<string> consulta = meses
+                                            .Where(m => m.Dias == 31)
+                                            .OrderBy(m => m.Nome)
+                                            .Select(m => m.Nome.ToUpper());
+
+            foreach (var item in consulta)
+                Console.WriteLine(item);
         }
     }
 }
