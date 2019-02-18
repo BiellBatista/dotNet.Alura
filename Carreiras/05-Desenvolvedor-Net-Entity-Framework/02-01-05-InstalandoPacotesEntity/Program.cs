4a20d372a8dce94c84c398f7327ae20edcbe6e54
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-17 23:18:07 -0300

    conclusão da aula 01 da seção "Entity Framework Core: Banco de dados de forma eficiente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-01-05-InstalandoPacotesEntity/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-01-05-InstalandoPacotesEntity/Program.cs
@@ -10,7 +10,23 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            //GravarUsandoAdoNet();
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+            // por conversao, devo utilizar a palavra "context" para declarar um contexto.
+            // neste caso, estou usando um contexto de loja que terá produto, vendedores...
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+            }
         }
 
         private static void GravarUsandoAdoNet()
