d09061a1b08e9f8c2e2728eaec04ca9ce4b61507
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-18 20:30:08 -0300

    conclusão módulo 03 da aula 02 da seção "Entity Framework Core: Banco de dados de forma eficiente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-02-XX-ManipulandoObjetos/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GravarUsandoAdoNet();
+            //GravarUsandoEntity();
+            RecuperarProdutos();
+            ExcluirProdutos();
+            RecuperarProdutos();
+        }
+
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new LojaContext())
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+
+                foreach (var item in produtos)
+                {
+                    repo.Produtos.Remove(item);
+                }
+                repo.SaveChanges();
+            }
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new LojaContext())
+            {
+                // indo até o banco para realizar um select * e gerar uma lista
+                IList<Produto> produtos = repo.Produtos.ToList();
+                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+            // por conversao, devo utilizar a palavra "context" para declarar um contexto.
+            // neste caso, estou usando um contexto de loja que terá produto, vendedores...
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
