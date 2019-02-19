4b0372e30c54357b62422c4318e4e0bc53c40d9c
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-18 21:24:33 -0300

    conclusão do módulo 01  da aula 03 da seção Entity Framework Core: Banco de dados de forma eficiente

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-03-03-LogandoSQLConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_03_LogandoSQLConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
