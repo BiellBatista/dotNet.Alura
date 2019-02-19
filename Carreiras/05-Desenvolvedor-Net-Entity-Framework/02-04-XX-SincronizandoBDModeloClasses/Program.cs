2114c952e14c07740b89a1834d982697f68e9ef8
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-19 04:41:25 -0300

    conclusao dao modulo 05 da aula 04 da seção "Entity Framework Core: Banco de dados de forma eficiente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-04-XX-SincronizandoBDModeloClasses/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-04-XX-SincronizandoBDModeloClasses/Program.cs
@@ -13,3 +13,18 @@ namespace Alura.Loja.Testes.ConsoleApp
         }
     }
 }
+
+/*
+ * Add-Migration Registrar uma versão (migration, nos termos do Entity).
+    Remove-Migration Esse comando é utilizado para remover a última migração não aplicada no banco de dados apontado pelo contexto.
+    Update-Database
+    Script-Migration Gera um script DDL para que seja executado no banco de dados
+    Drop-Database Esse comando é utilizado para dropar o banco de dados apontado pelo contexto.
+    Scaffold-DbContext Esse comando é utilizado para criar uma classe que estende de DbContext, além de classes que representam as tabelas do banco.
+
+A migração é feita em dois passos. O primeiro passo é executarmos o comando Add-Migration.
+
+Já o segundo passo pode ser feitas de duas maneiras diferentes, sendo a primeira gerando um script de linguagem DDL com o comando Script-Migration. Esse cenário é mais utilizado quando existe uma equipe de banco de dados separada da equipe de desenvolvimento.
+
+A outra maneira é usarmos o comando Update-Database, onde o Entity pega a nova versão que foi registrada e executa diretamente no banco de dados. Vamos utilizar essa segunda forma.
+ */
