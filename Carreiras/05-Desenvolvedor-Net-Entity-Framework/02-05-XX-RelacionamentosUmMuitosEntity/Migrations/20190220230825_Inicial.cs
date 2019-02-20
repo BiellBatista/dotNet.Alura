b05fae04a766ea0aa11774245ba77498e1abac9a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-20 20:52:13 -0300

    conclusão do módulo 01 da aula 05 da seção "Entity Framework Core: Banco de dados de forma eficiente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-05-XX-RelacionamentosUmMuitosEntity/Migrations/20190220230825_Inicial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace _02_04_XX_SincronizandoBDModeloClasses.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // É necessário comentar todas as linhas desse método, pois o banco de dados está executando todas a migration, porque a tabela do histórico está vazia e ele irá começar pela migration mais nova. Se a migration mais nova estiver na tabela do histórico, o Entity irá para a próxima, sucessivamente
+            // Após isso eu executo Update-Database Inicial (onde o Inicial é a migration especifica que quero colocar na table de histórico
+            // depois eu posso descomentar
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Categoria = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    Preco = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+            // após a execução desta migration, o nome dela vai para a tabela de histórico, pois assim o Entity não irá execuar ela quando for dado o comando Update-Database
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-05-XX-RelacionamentosUmMuitosEntity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // compra de 6 pães frances
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Franês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+        }
+    }
+}
+
+/*
+ * Add-Migration Registrar uma versão (migration, nos termos do Entity).
+    Remove-Migration Esse comando é utilizado para remover a última migração não aplicada no banco de dados apontado pelo contexto.
+    Update-Database Atualiza o banco de dados com base na tabela de histórico de migração
+    Script-Migration Gera um script DDL para que seja executado no banco de dados
+    Drop-Database Esse comando é utilizado para dropar o banco de dados apontado pelo contexto.
+    Scaffold-DbContext Esse comando é utilizado para criar uma classe que estende de DbContext, além de classes que representam as tabelas do banco.
+
+A migração é feita em dois passos. O primeiro passo é executarmos o comando Add-Migration.
+
+Já o segundo passo pode ser feitas de duas maneiras diferentes, sendo a primeira gerando um script de linguagem DDL com o comando Script-Migration. Esse cenário é mais utilizado quando existe uma equipe de banco de dados separada da equipe de desenvolvimento.
+
+A outra maneira é usarmos o comando Update-Database, onde o Entity pega a nova versão que foi registrada e executa diretamente no banco de dados. Vamos utilizar essa segunda forma.
+ */
