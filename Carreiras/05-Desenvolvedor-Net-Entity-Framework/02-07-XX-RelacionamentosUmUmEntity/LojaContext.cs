dda4cab3ccc5d26b9e58f91c3f9f380b963f15a1
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-20 22:39:14 -0300

    conclusão módulo 01 da aula 07 da seção "Entity Framework Core: Banco de dados de forma eficiente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-07-XX-RelacionamentosUmUmEntity/LojaContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        // falando para o Entity manipular a tabela Produtos que está representada pela classe produto
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Promocao> Promocoes { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public LojaContext()
+        { }
+
+        public LojaContext(DbContextOptions<LojaContext> options) : base(options)
+        { }
+
+        // este método é executado no evento de criação do modelo
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // montando uma chave composta para a abela de join
+            modelBuilder
+                .Entity<PromocaoProduto>()
+                .HasKey(pp => new { pp.PromocaoId, pp.ProdutoId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // falando qual o banco de dados que sera usado
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // verificando se o providre está configurado
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = LojaDB; Trusted_Connection = true;");
+            }
+        }
+    }
+}
---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/02-07-XX-RelacionamentosUmUmEntity/PromocaoProduto.cs
@@ -0,0 +1,13 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class PromocaoProduto
+    {
+        /*
+         * Tabela de join (N:M) não possui chave primária, porque ela só serve para realizar um join e as duas foreign key já são chave
+         */
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; set; }
+        public int PromocaoId { get; set; }
+        public Promocao Promocao { get; set; }
+    }
+}
