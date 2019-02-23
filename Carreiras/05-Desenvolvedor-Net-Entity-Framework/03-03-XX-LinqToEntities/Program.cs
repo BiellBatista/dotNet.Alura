9a6222ddea50eb19b6ab9c81de888d753b96f121
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-23 16:50:47 -0300

    conclusão do módulo 01 da aula 03 da seção "Entity LinQ parte 1: Crie queries poderosas em C#"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-03-XX-LinqToEntities/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-03-XX-LinqToEntities/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _03_03_XX_LinqToEntities.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace _03_03_XX_LinqToEntities
     {
         static void Main(string[] args)
         {
+            using (var contexto = new AluraTunesEntities())
+            {
+                contexto.Database.Log = Console.WriteLine; // o console recebe o log do contexto
+
+                // definição de consulta
+                var query = from g in contexto.Generos
+                            select g;
+
+                foreach (var genero in query)
+                {
+                    Console.WriteLine("{0}\t{1}", genero.GeneroId, genero.Nome);
+                }
+
+                Console.WriteLine("====================");
+
+                //realizando um JOIN entre a FAIXA e o GENERO
+                var faixaGenero = from g in contexto.Generos
+                             join f in contexto.Faixas on g.GeneroId equals f.GeneroId
+                             select new { f, g }; // aqui eu escrevo a query
+
+                // aqui eu executo a query
+                faixaGenero = faixaGenero.Take(10); // pegando apenas 10 itens
+
+                foreach (var item in faixaGenero)
+                {
+                    Console.WriteLine("{0}\t{1}", item.f.Nome, item.g.Nome);
+                }
+            }
         }
     }
 }
