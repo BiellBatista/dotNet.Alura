ed2a2cac5d6a41ce51681c3feac8bdec0b8a73b8
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-23 13:51:54 -0300

    conclusão do módulo 02 da aula 01 da seção "Entity LinQ parte 1: Crie queries poderosas em C#"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-01-XX-LinqToObjects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_01_XX_LinqToObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void ListandoGenerosRock()
+        {
+            // listar todos os generos que tem a palavra rock
+            var generos = new List<Genero>
+            {
+                new Genero { Id = 1, Nome = "Rock"},
+                new Genero { Id = 2, Nome =  "Reggae"},
+                new Genero { Id = 3, Nome =  "Rock Progressivo"},
+                new Genero { Id = 4, Nome =  "Punk"},
+                new Genero { Id = 5, Nome =  "Clássica"}
+            };
+            Console.WriteLine("Lista com resultado de uma busca sem query e com if");
+            foreach (var genero in generos)
+                if (genero.Nome.Contains("Rock"))
+                    Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+            Console.WriteLine("Lista com resultado de uma query");
+            // select * from generos
+            // o"g" é a variável interna que representa UM objeto da conjunta
+            //var query = from g in generos select g;
+            var query = from g in generos where g.Nome.Contains("Rock") select g;
+            foreach (var genero in query)
+                Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+        }
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
