1e8ac3f7ffe3d2c440fb3db9ca879bbb64ad3a32
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-23 20:51:36 -0300

    conclusão do módulo 02 da aula 04 da seção "Entity LinQ parte 1: Crie queries poderosas em C#"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-04-XX-AssociandoFiltrandoEntidades/Program.cs
@@ -0,0 +1,42 @@
+﻿using _03_04_XX_AssociandoFiltrandoEntidades.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_04_XX_AssociandoFiltrandoEntidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var textoBusca = "Led";
+                // realizando um join para pegar o nome do artista e do album
+                var query = from a in contexto.Artistas
+                            join alb in contexto.Albums on a.ArtistaId equals alb.ArtistaId
+                            where a.Nome.Contains(textoBusca)
+                            select new { NomeArtista = a.Nome, NomeAlbum = alb.Titulo };
+
+                foreach (var item in query)
+                    Console.WriteLine("{0}\t{1}", item.NomeArtista, item.NomeAlbum);
+
+                Console.WriteLine("=========Select sem o Where==========");
+                // consulta sem o join, usando a propriedade de navegação
+                // só é possível fazer isso com o entity framework e linq
+                var query2 = from alb in contexto.Albums
+                             where alb.Artista.Nome.Contains(textoBusca)
+                             select new
+                             {
+                                 NomeArtista = alb.Artista.Nome,
+                                 NomeAlbum = alb.Titulo
+                             };
+
+                foreach (var item in query2)
+                    Console.WriteLine("{0}\t{1}", item.NomeArtista, item.NomeAlbum);
+            }
+        }
+    }
+}
