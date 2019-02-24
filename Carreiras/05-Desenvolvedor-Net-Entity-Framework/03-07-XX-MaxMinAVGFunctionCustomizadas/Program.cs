57462ab688ef1162e8c44c8e177396cfc5f7b9cc
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-24 12:24:40 -0300

    conclusão do módulo 01 da aula 07 da seção "Entity LinQ parte 1: Crie queries poderosas em C#"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-07-XX-MaxMinAVGFunctionCustomizadas/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/03-07-XX-MaxMinAVGFunctionCustomizadas/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _03_07_XX_MaxMinAVGFunctionCustomizadas.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace _03_07_XX_MaxMinAVGFunctionCustomizadas
     {
         static void Main(string[] args)
         {
+            using (var contexto = new AluraTunesEntities())
+            {
+                // mostrando as consulta no console
+                contexto.Database.Log = Console.WriteLine;
+
+                //pegando o maior valor de uma nota fiscal, o atributo Total possui o valor total de uma nota fiscal
+                var maiorVenda = contexto.NotaFiscals.Max(nf => nf.Total);
+                var menorVenda = contexto.NotaFiscals.Min(nf => nf.Total);
+                var mediaVenda = contexto.NotaFiscals.Average(nf => nf.Total);
+
+                Console.WriteLine("A amior venda é de R${0}", maiorVenda);
+                Console.WriteLine("A menor venda é de R${0}", menorVenda);
+                Console.WriteLine("A média da venda é de R${0}", mediaVenda);
+
+                // realizando uma consulta que retorna diversos valores
+                var vendas = (from nf in contexto.NotaFiscals
+                             group nf by 1 into agrupado
+                             select new
+                             {
+                                 MaiorVenda = agrupado.Max(nf => nf.Total),
+                                 MenorVenda = agrupado.Min(nf => nf.Total),
+                                 MediaVenda = agrupado.Average(nf => nf.Total)
+                             }).Single(); // o método Single();, tranforma uma consulta em um objeto. Com isso, consigo acessar os atributos MaiorVenda, MenorVenda e MediaVenda
+
+                Console.WriteLine("A amior venda é de R${0}", vendas.MaiorVenda);
+                Console.WriteLine("A menor venda é de R${0}", vendas.MenorVenda);
+                Console.WriteLine("A média da venda é de R${0}", vendas.MediaVenda);
+            }
         }
     }
 }
