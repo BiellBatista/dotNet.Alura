a1e04c7d37c10eb91b3e8b136b0ea15e1939af24
Gabriel Batista <gabriel.texteis@gmail.com>
2019-02-26 02:25:15 -0300

    conclusão do módulo 02 da aula 01 da seção "Entity LinQ parte 2: Store Procedures e consultas com o LinQPad"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/04-01-XX-AprendaCriarRelatorioPaginacao/Program.cs
@@ -0,0 +1,76 @@
+﻿using _04_01_XX_AprendaCriarRelatorioPaginacao.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01_XX_AprendaCriarRelatorioPaginacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // quebrando as páginas. AUla01
+            //const int TAMANHO_PAGINA = 10;
+
+            //using (var contexto = new AluraTunesEntities())
+            //{
+            //    var numeroNotasFiscais = contexto.NotaFiscals.Count();
+            //    var numeroPaginas = Math.Ceiling((decimal)numeroNotasFiscais / TAMANHO_PAGINA);
+
+            //    for (int p = 1; p <= numeroPaginas; p++)
+            //        ImprimirPagina(TAMANHO_PAGINA, contexto, p);
+
+            //}
+
+            using (var contexto = new AluraTunesEntities())
+            {
+                decimal queryMedia = contexto.NotaFiscals.Average(n => n.Total);
+                var query = from nf in contexto.NotaFiscals
+                            where nf.Total > /*subconsulta ->*/queryMedia
+                            orderby nf.Total descending //ordenação descrecente
+                            select new
+                            {
+                                Numero = nf.NotaFiscalId,
+                                Data = nf.DataNotaFiscal,
+                                Cliente = nf.Cliente.PrimeiroNome + nf.Cliente.Sobrenome,
+                                Valor = nf.Total
+                            };
+
+                foreach (var nf in query)
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                        nf.Numero,
+                        nf.Data,
+                        nf.Cliente,
+                        nf.Valor);
+            }
+        }
+
+        private static void ImprimirPagina(int TAMANHO_PAGINA, AluraTunesEntities contexto, int numeroPagina)
+        {
+            // paginando uma consulta. Limitando para X (10) páginas
+
+            var query = from nf in contexto.NotaFiscals
+                        orderby nf.NotaFiscalId
+                        select new
+                        {
+                            Numero = nf.NotaFiscalId,
+                            Data = nf.DataNotaFiscal,
+                            Cliente = nf.Cliente.PrimeiroNome + nf.Cliente.Sobrenome,
+                            Total = nf.Total
+                        };
+
+            int numeroDePulos = (numeroPagina - 1) * TAMANHO_PAGINA;
+
+            query = query.Skip(numeroDePulos); //ele só funciona em lista ordenada
+            query = query.Take(TAMANHO_PAGINA);
+
+            Console.WriteLine();
+            Console.WriteLine("Número da Página: {0}", numeroPagina);
+
+            foreach (var nf in query)
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t", nf.Numero, nf.Data, nf.Cliente, nf.Total);
+        }
+    }
+}
