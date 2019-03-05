d44fde8cf1efe5deb3ce308d2003d2a13a9653ce
Gabriel Batista <gabriel.texteis@gmail.com>
2019-03-05 19:59:09 -0300

    conclusão da aula 10 da seção "ASP.NET MVC 5: O framework pronto para a web"

---
+++ Carreiras/02-Desenvolvedor-ASP-NET-MVC5/02-Desenvolvedor-ASP-NET-MVC5/Controllers/ContadorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_Desenvolvedor_ASP_NET_MVC5.Controllers
+{
+    public class ContadorController : Controller
+    {
+        // GET: Contador
+        public ActionResult Index()
+        {
+            var valorNaSession = Session["contador"];
+            int contador = Convert.ToInt32(valorNaSession);
+            contador++;
+            Session["contador"] = contador;
+            return View(contador);
+        }
+    }
+}
+
+/*
+ * A Session do Asp.Net MVC funciona como um IDictionary<string, object> do C#, então a busca de uma chave armazenada sempre devolve um valor do tipo object. Mesmo quando armazenamos um valor do tipo int.
+ */
---
+++ Carreiras/02-Desenvolvedor-ASP-NET-MVC5/02-Desenvolvedor-ASP-NET-MVC5/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using _02_Desenvolvedor_ASP_NET_MVC5.DAO;
+using _02_Desenvolvedor_ASP_NET_MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_Desenvolvedor_ASP_NET_MVC5.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autentica(string login, string senha)
+        {
+            UsuariosDAO dao = new UsuariosDAO();
+            Usuario usuario = dao.Busca(login, senha);
+
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
