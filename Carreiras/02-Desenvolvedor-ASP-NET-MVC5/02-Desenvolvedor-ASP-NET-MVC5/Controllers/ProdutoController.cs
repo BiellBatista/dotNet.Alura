c0aafee100ac1e87a8d410ce7fcb0bc9a4bfd477
Gabriel Batista <gabriel.texteis@gmail.com>
2019-03-05 15:39:02 -0300

    conclusão da aula 04 da seção "ASP.NET MVC 5: O framework pronto para a web"

--- Carreiras/02-Desenvolvedor-ASP-NET-MVC5/02-Desenvolvedor-ASP-NET-MVC5/Controllers/ProdutoController.cs
+++ Carreiras/02-Desenvolvedor-ASP-NET-MVC5/02-Desenvolvedor-ASP-NET-MVC5/Controllers/ProdutoController.cs
@@ -20,5 +20,61 @@ namespace _02_Desenvolvedor_ASP_NET_MVC5.Controllers
 
             return View();
         }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        ///*
+        // * Para cada informação que eu quero pegar no formulário, devo colocar os parametros com o mesmo nome que estão no formulário
+        // */ 
+        //public ActionResult Adiciona(string nome, decimal preco, string descricao, int quantidade, int categoriaId)
+        //{
+        //    Produto produto = new Produto()
+        //    {
+        //        Nome = nome,
+        //        Preco = preco,
+        //        Descricao = descricao,
+        //        Quantidade = quantidade,
+        //        CategoriaId = categoriaId
+        //    };
+
+        //    ProdutosDAO dao = new ProdutosDAO();
+        //    dao.Adiciona(produto);
+
+        //    return View();
+        //}
+
+        /*
+         * Este método faz a mesma coisa que o de cima. Porém, ele recebe um objeto do tipo produto e para isso ser possível, os input da view devem seguir uma regra no atributo name. 
+         * Se eu usar o método de cim,a basta remover os trechos produto.... dos atributos names dos input.
+         * O [HttpPost] faz com que o método só aceite dados vindo pelo POST
+         */
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            /*
+             * O cara responsável por criar o modelo que será armazenado no banco de dados é chamado de Model Binder
+             */
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+
+            // redirecionando o usuário para o método index do controller produto. Posso omitir o controller, caso o método esteja invocando o próprio
+            // Para redirecionar o usuário para uma action de outro controller, devemos utilizar a versão do RedirectToAction que recebe duas Strings, o nome da action e o nome do controller. Nesse caso, RedirectToAction("Index","Home").
+            return RedirectToAction("Index", "Produto");
+        }
     }
 }
+/* Aula 04
+ * 
+ * O Model Binder do ASP.NET MVC espera que os nomes dos parâmetros da requisição sejam da forma <Nome do Argumento do Controller>.<Nome do atributo>. Logo, nesse caso, devemos utilizar categoria.Id, além dessa convenção, o model binder do ASP.NET MVC também consegue preencher o modelo se enviarmos apenas o nome da propriedade, sem o nome do argumento da action, então também poderíamos enviar um parâmetro chamado Id para preencher a propriedade Id da categoria.
+
+Como o model binder padrão não é case sensitive, as seguintes alternativas estão corretas: categoria.Id, categoria.ID e ID
+
+Parabéns, você acertou!
+
+ */
