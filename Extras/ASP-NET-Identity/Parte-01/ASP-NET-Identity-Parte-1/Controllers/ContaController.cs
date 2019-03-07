5c76727659445f0e9469e06cac9be64e05ccc571
Gabriel Batista <gabriel.texteis@gmail.com>
2019-03-06 22:00:16 -0300

    conclusão da aula 01 da seção "ASP.NET Identity parte 1: Gerencie contas de usuários"

---
+++ Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Controllers/ContaController.cs
@@ -0,0 +1,24 @@
+﻿using ASP_NET_Identity_Parte_1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_NET_Identity_Parte_1.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
