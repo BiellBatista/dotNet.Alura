c1349cb25f6791a9548c82fec3b774dab3e31169
Gabriel Batista <gabriel.texteis@gmail.com>
2019-03-09 12:11:27 -0300

    conclusão módulo 02 da aula 02 da seção "ASP.NET Identity parte 1: Gerencie contas de usuários"

--- Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Controllers/ContaController.cs
+++ Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Controllers/ContaController.cs
@@ -1,4 +1,6 @@
-﻿using ASP_NET_Identity_Parte_1.ViewModels;
+﻿using ASP_NET_Identity_Parte_1.Models;
+using ASP_NET_Identity_Parte_1.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +20,28 @@ namespace ASP_NET_Identity_Parte_1.Controllers
         [HttpPost]
         public ActionResult Index(ContaRegistrarViewModel modelo)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                /*
+                 * O contexto (DbContext (EntityFramework)) para gerenciar o banco de dados. Passo o a classe do banco de dados, para ele manipular
+                 */
+                var dbContext = new IdentityDbContext<UserAplication>();
+                /*
+                 * Criando um objeto para armazenar os dados do usuario
+                 */
+                var novoUsuario = new UserAplication();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.FullName = modelo.NomeCompleto;
+
+                dbContext.Users.Add(novoUsuario);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(modelo);
         }
     }
 }
