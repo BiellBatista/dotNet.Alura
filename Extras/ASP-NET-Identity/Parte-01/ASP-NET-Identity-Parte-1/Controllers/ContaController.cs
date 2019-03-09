a855d4325d3b8eafe61c63a73f46eef936474c5a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-03-09 13:26:35 -0300

    conclusão aula 03 da seção "ASP.NET Identity parte 1: Gerencie contas de usuários"

--- Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Controllers/ContaController.cs
+++ Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Controllers/ContaController.cs
@@ -2,6 +2,7 @@
 using ASP_NET_Identity_Parte_1.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,27 @@ namespace ASP_NET_Identity_Parte_1.Controllers
 {
     public class ContaController : Controller
     {
+        // backfiled da propriedade
+        private UserManager<UserAplication> _userManager;
+        public UserManager<UserAplication> UserManager
+        {
+            get
+            {
+                if(_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    // este método GetUserManager() vem do using Microsoft.AspNet.Identity.Owin;
+                    _userManager = contextOwin.GetUserManager<UserManager<UserAplication>>();
+                }
+
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
         // GET: Conta
         public ActionResult Index()
         {
@@ -24,31 +46,33 @@ namespace ASP_NET_Identity_Parte_1.Controllers
         {
             if(ModelState.IsValid)
             {
-                /*
-                 * O contexto (DbContext (EntityFramework)) para gerenciar o banco de dados. Passo o a classe do banco de dados, para ele manipular
-                 * Passando a connectionstring
-                 */
-                var dbContext = new IdentityDbContext<UserAplication>("DefaultConnection");
-                /*
-                 * O UserStore(); gera a interface entre o identity e o entityframework. Neste caso, estou criando a interface para os modelos do Usuário
-                 * 
-                 */
-                var userStore = new UserStore<UserAplication>(dbContext);
-                /*
-                 * Uso o userManager para desacoplar o identity do entityframework e gerenciar as persistencia do usuário
-                 */
-                var userManager = new UserManager<UserAplication>(userStore);
-                /*
-                 * Criando um objeto para armazenar os dados do usuario
-                 */
+                // isso tudo, entre o //, está no Startup.cs
+                ///*
+                // * O contexto (DbContext (EntityFramework)) para gerenciar o banco de dados. Passo o a classe do banco de dados, para ele manipular
+                // * Passando a connectionstring
+                // */
+                //var dbContext = new IdentityDbContext<UserAplication>("DefaultConnection");
+                ///*
+                // * O UserStore(); gera a interface entre o identity e o entityframework. Neste caso, estou criando a interface para os modelos do Usuário
+                // * 
+                // */
+                //var userStore = new UserStore<UserAplication>(dbContext);
+                ///*
+                // * Uso o userManager para desacoplar o identity do entityframework e gerenciar as persistencia do usuário
+                // */
+                //var userManager = new UserManager<UserAplication>(userStore);
+                ///*
+                // * Criando um objeto para armazenar os dados do usuario
+                // */
                 var novoUsuario = new UserAplication();
 
                 novoUsuario.Email = modelo.Email;
                 novoUsuario.UserName = modelo.UserName;
                 novoUsuario.FullName = modelo.NomeCompleto;
 
+                await UserManager.CreateAsync(novoUsuario, modelo.Senha);
                 // a senha vai como argumento pois ela fica armazenada em outro lugar. Por padrão, o UserManager salva alterações automáticamente
-                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+                //await userManager.CreateAsync(novoUsuario, modelo.Senha);
                 // não preciso mais do dbContext, pois irei usar algo menos desacoplado
                 //dbContext.Users.Add(novoUsuario);
                 //dbContext.SaveChanges();
---
+++ Extras/ASP-NET-Identity/Parte-01/ASP-NET-Identity-Parte-1/Sartup.cs
@@ -0,0 +1,52 @@
+﻿using ASP_NET_Identity_Parte_1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+// este assembly defini para o Owin a classe/tipo de inicialização
+[assembly: OwinStartup(typeof(ASP_NET_Identity_Parte_1.Sartup))]
+
+namespace ASP_NET_Identity_Parte_1
+{
+    /*
+     * Classe para configurar o Owin. O Owin é responsável por gerenciar o pipelina entre a aplicação e o servidor
+     */
+    public class Sartup
+    {
+        /* sou obrigado a colocar o método Coniguration
+         * ele recebe como argumento o pipeline da aplicação
+         */
+        public void Configuration(IAppBuilder builder)
+        {
+            // a cada contexto do Owin, crie o tipo DbContext. Falando para o Owin qual o dbcontext
+            builder.CreatePerOwinContext<DbContext>(() =>
+                new IdentityDbContext<UserAplication>("DefaultConnection")
+                );
+            // a cada contexto do Owin, crie um UserStore
+            builder.CreatePerOwinContext<IUserStore<UserAplication>>(
+                (opcoes, contextOwin) =>
+                {
+                    // método Get vem do pacote using Microsoft.AspNet.Identity.Owin;
+                    var dbContext = contextOwin.Get<DbContext>();
+                    return new UserStore<UserAplication>(dbContext);
+                });
+            // a cada contexto do Owin, crie o UserManager
+            builder.CreatePerOwinContext<UserManager<UserAplication>>(
+                (opcoes, contextOwin) =>
+                {
+                    var userStore = contextOwin.Get<IUserStore<UserAplication>>();
+                    return new UserManager<UserAplication>(userStore);
+                });
+        }
+    }
+}
+/*
+ * Um contexto do Owin ocorre a cada requisição (GET, POST, PUT, DELETE)
+ */
