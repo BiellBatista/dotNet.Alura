ee68107379a81d50cd3acf654280e5dcad750e9c
Gabriel Batista <gabriel.texteis@gmail.com>
2019-04-25 07:24:22 -0300

    conclusão do módulo 08 da aula 01 da seção "ASP.NET Core parte 1: Um e-Commerce com MVC e EF Core"

--- Formação/Desenvolvedor.NET/02-01-XX-CasaDoCodigo/Startup.cs
+++ Formação/Desenvolvedor.NET/02-01-XX-CasaDoCodigo/Startup.cs
@@ -11,7 +11,15 @@ namespace _02_01_XX_CasaDoCodigo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ICatalogo, Catalogo>(); //criando um cerviço temporário (transidório). Passo dois parametros, a interface e o objeto concreto
+            //services.AddTransient<ICatalogo, Catalogo>(); //criando um cerviço temporário (transidório), ou seja, toda fez que for solicitado um serviceProvider.GetService<>(), será instanciado um novo objeto. Passo dois parametros, a interface e o objeto concreto
+            //services.AddTransient<IRelatorio, Relatorio>();
+
+            //services.AddScoped<ICatalogo, Catalogo>(); //criando um serviço de escopo, ou seja, toda fez que for realizado um request, o objeto será instanciado apenas uma vez. Passo dois parametros, a interface e o objeto concreto
+            //services.AddScoped<IRelatorio, Relatorio>();
+
+            var catalogo = new Catalogo();
+            services.AddSingleton<ICatalogo>(catalogo); //criando um serviço único, ou seja, enquanto a aplicação estiver rodando, existirá apenas uma instancia de catalogo. Passo dois parametros, a interface e o objeto concreto
+            services.AddSingleton<IRelatorio>(new Relatorio(catalogo));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +36,7 @@ namespace _02_01_XX_CasaDoCodigo
              * Além disos tudo, é necessário soliciar o Catalago como serviço
              */
             ICatalogo catalogo = serviceProvider.GetService<ICatalogo>(); // solicitando o catalago como serviço. ASsim, não mexo na classe concreta
-            IRelatorio relatorio = new Relatorio(catalogo);
+            IRelatorio relatorio = serviceProvider.GetService<IRelatorio>(); // solicitando o relatorio como serviço. ASsim, não mexo na classe concreta
 
             app.Run(async (context) =>
             {
