fd8a3f348d6d4bbed96ebef71676ab6b2dfcb183
Gabriel Batista <gabriel.texteis@gmail.com>
2019-04-28 17:20:56 -0300

    conclusão do módulo 05 da aula 03 da seção "ASP.NET Core parte 1: Um e-Commerce com MVC e EF Core"

--- Formação/Desenvolvedor.NET/02-03-XX-CasaDoCodigo/ApplicationContext.cs
+++ Formação/Desenvolvedor.NET/02-03-XX-CasaDoCodigo/ApplicationContext.cs
@@ -20,6 +20,22 @@ namespace _02_03_XX_CasaDoCodigo
 
             // este método registra uma classe no modelo, para saber qual classe será usada para gerar o banco de dados
             modelBuilder.Entity<Produto>().HasKey(t => t.Id); //o método HashKey() indica a propriedade que representará a chave primária
+
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id); //o método HashKey() indica a propriedade que representará a chave primária
+            //Relacionamento 1:N (um pedido tem muitos itens).RelacionametoDeVolta cada item terá pedido
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido);
+            //Relacionamento 1:1 (um pedido tem um cadastro).RelacionamentoDeVolta cada cadastro terá pedido.ÉObrigatório
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).HasForeignKey<Cadastro>(t => t.Id).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(t => t.Id); //o método HashKey() indica a propriedade que representará a chave primária
+            //Relacionamento 1:1 (um itemPedido tem um pedido)
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            //Relacionamento 1:1 (um itemPedido tem um produto)
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(t => t.Id); //o método HashKey() indica a propriedade que representará a chave primária
+            //Relacionamento 1:1 (um cadastro tem um pedido)
+            modelBuilder.Entity<Cadastro>().HasOne(t => t.Pedido);
         }
     }
 }
