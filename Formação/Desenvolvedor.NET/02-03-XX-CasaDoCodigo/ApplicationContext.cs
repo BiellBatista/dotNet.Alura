a86943c760aca400b7a8251ef9dae723a0a2d850
Gabriel Batista <gabriel.texteis@gmail.com>
2019-04-28 16:14:03 -0300

    conclusão do módulo 01 da aula 03 da seção "ASP.NET Core parte 1: Um e-Commerce com MVC e EF Core"

---
+++ Formação/Desenvolvedor.NET/02-03-XX-CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using _02_03_XX_CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_03_XX_CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // este método registra uma classe no modelo, para saber qual classe será usada para gerar o banco de dados
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id); //o método HashKey() indica a propriedade que representará a chave primária
+        }
+    }
+}
