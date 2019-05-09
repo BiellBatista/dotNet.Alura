9ccaaa43fb9164544df4fe4941ca77c71bc890dd
Gabriel Batista <gabriel.texteis@gmail.com>
2019-05-09 06:39:19 -0300

    conclusão do módulo 01 da aula 02 da seção "ASP.NET Core parte 2: Um e-Commerce com MVC e EF Core"

--- Formação/Desenvolvedor.NET/03-02-XX-CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Formação/Desenvolvedor.NET/03-02-XX-CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -4,13 +4,18 @@ namespace _03_02_XX_CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+
         }
     }
 }
