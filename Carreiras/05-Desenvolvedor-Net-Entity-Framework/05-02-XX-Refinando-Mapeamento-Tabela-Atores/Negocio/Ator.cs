6cf963c66a148a98a19e3ded7479aa10ad195ad0
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 20:48:07 -0300

    conclusão da aula 01 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Negocio/Ator.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Alura.Filmes.App.Negocio
+{
+    [Table("Actor")] //falando que quero usar a tabela Actor. Com isso, posso usar outro nome  de propriedade no contexto e quebro a regra de nome de tabela
+    class Ator
+    {
+        [Column("actor_id")]
+        public int Id { get; set; }
+        [Column("first_name")] //quebrando a regra de nome de coluna. Com isso, falo qual a coluna que esta propriedade referencia
+        public string PrimeiroNome { get; set; } //Geralmente, a propriedade possui o mesmo nome que a coluna
+        [Column("last_name")]
+        public string UltimoNome { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ator ({Id}): {PrimeiroNome} {UltimoNome}";
+        }
+    }
+}
