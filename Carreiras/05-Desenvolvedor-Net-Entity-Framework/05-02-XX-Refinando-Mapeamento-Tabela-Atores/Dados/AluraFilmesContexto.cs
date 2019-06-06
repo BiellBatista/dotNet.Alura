2630af6645f9d2dad5de1ab3cd8edba8799bc993
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 21:36:15 -0300

    conclusão do módulo 07 da aula 02 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Dados/AluraFilmesContexto.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Dados/AluraFilmesContexto.cs
@@ -11,5 +11,29 @@ namespace Alura.Filmes.App.Dados
         {
             optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
+
+        //este método representa o evento de criação do modelo de mapeamento
+        //configurando por aqui, chamo o código de Fluent API.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //falando que o contexto irá mapear a entidade Ator para a tabela "actor"
+            modelBuilder.Entity<Ator>().ToTable("actor");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id) // pegando a propriedade Id
+                .HasColumnName("actor_id"); //falando que a propriedade Id referencia a coluna actor_id
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome) // pegando a propriedade PrimeiroNome
+                .HasColumnName("first_name") //falando que a propriedade PrimeiroNome referencia a coluna first_name
+                .HasColumnType("varchar(45)") //falando que a propriedade PrimeiroNome referencia uma coluna do tipo varchar64
+                .IsRequired(); //falando que a prorpeidade PrimeiroNome referencia uma coluna não nula
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome) // pegando a propriedade UltimoNome
+                .HasColumnName("last_name") //falando que a propriedade UltimoNome referencia a coluna first_name
+                .HasColumnType("varchar(45)") //falando que a propriedade UltimoNome referencia uma coluna do tipo varchar64
+                .IsRequired(); //falando que a prorpeidade UltimoNome referencia uma coluna não nula
+        }
     }
 }
--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Negocio/Ator.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Negocio/Ator.cs
@@ -1,19 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Alura.Filmes.App.Negocio
+﻿namespace Alura.Filmes.App.Negocio
 {
-    [Table("Actor")]
+    //[Table("Actor")]
     class Ator
     {
-        [Column("actor_id")]
+        //[Column("actor_id")]
         public int Id { get; set; }
-        [Required]
-        [Column("first_name", TypeName = "varchar(45)")]
+        //[Required]
+        //[Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
-        [Required]
+        //[Required]
         //falando que esta coluna é do tipo varchar(45)
-        [Column("last_name", TypeName = "varchar(45)")]
+        //[Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
 
         public override string ToString()
