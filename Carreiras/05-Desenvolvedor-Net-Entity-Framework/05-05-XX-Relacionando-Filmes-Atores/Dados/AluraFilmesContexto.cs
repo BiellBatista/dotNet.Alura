19174abc0f5ad15368a118832b324539d2fede00
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 23:10:08 -0300

    conclusão da aula 04 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-05-XX-Relacionando-Filmes-Atores/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,23 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
+        }
+    }
+}
