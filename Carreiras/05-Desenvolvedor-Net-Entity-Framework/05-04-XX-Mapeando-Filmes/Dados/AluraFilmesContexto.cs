f58c9f38d401570dcb3b6c8d35f3b5d1fbe4528f
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 23:07:04 -0300

    conclusão do módulo 07 da aula 04 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-04-XX-Mapeando-Filmes/Dados/AluraFilmesContexto.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-04-XX-Mapeando-Filmes/Dados/AluraFilmesContexto.cs
@@ -16,61 +16,68 @@ namespace Alura.Filmes.App.Dados
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Ator>()
-                .ToTable("actor");
+            //não preciso mais disso, porque separei para uma classe de configuração
+            //modelBuilder.Entity<Ator>()
+            //.ToTable("actor");
+            //modelBuilder.Entity<Ator>()
+            //    .Property(a => a.Id)
+            //    .HasColumnName("actor_id");
 
-            modelBuilder.Entity<Ator>()
-                .Property(a => a.Id)
-                .HasColumnName("actor_id");
+            //modelBuilder.Entity<Ator>()
+            //    .Property(a => a.PrimeiroNome)
+            //    .HasColumnName("first_name")
+            //    .HasColumnType("varchar(45)")
+            //    .IsRequired();
 
-            modelBuilder.Entity<Ator>()
-                .Property(a => a.PrimeiroNome)
-                .HasColumnName("first_name")
-                .HasColumnType("varchar(45)")
-                .IsRequired();
+            //modelBuilder.Entity<Ator>()
+            //    .Property(a => a.UltimoNome)
+            //    .HasColumnName("last_name")
+            //    .HasColumnType("varchar(45)")
+            //    .IsRequired();
 
-            modelBuilder.Entity<Ator>()
-                .Property(a => a.UltimoNome)
-                .HasColumnName("last_name")
-                .HasColumnType("varchar(45)")
-                .IsRequired();
+            //modelBuilder.Entity<Ator>()
+            //    .Property<DateTime>("last_update")
+            //    .HasColumnType("datetime")
+            //    .HasDefaultValueSql("getdate()");
 
-            modelBuilder.Entity<Ator>()
-                .Property<DateTime>("last_update")
-                .HasColumnType("datetime")
-                .HasDefaultValueSql("getdate()");
+            //não preciso dos códigos de cima, porque já separei para uma classe especializada
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
 
-            modelBuilder.Entity<Filme>()
-                .ToTable("film");
+            //não preciso mais disso, porque separei para uma classe de configuração
+            //modelBuilder.Entity<Filme>()
+            //    .ToTable("film");
 
-            modelBuilder.Entity<Filme>()
-                .Property(f => f.Id)
-                .HasColumnName("film_id");
+            //modelBuilder.Entity<Filme>()
+            //    .Property(f => f.Id)
+            //    .HasColumnName("film_id");
 
-            modelBuilder.Entity<Filme>()
-                .Property(f => f.Titulo)
-                .HasColumnName("title")
-                .HasColumnType("varchar(255)")
-                .IsRequired();
+            //modelBuilder.Entity<Filme>()
+            //    .Property(f => f.Titulo)
+            //    .HasColumnName("title")
+            //    .HasColumnType("varchar(255)")
+            //    .IsRequired();
 
-            modelBuilder.Entity<Filme>()
-                .Property(f => f.Descricao)
-                .HasColumnName("description")
-                .HasColumnType("text");
+            //modelBuilder.Entity<Filme>()
+            //    .Property(f => f.Descricao)
+            //    .HasColumnName("description")
+            //    .HasColumnType("text");
 
-            modelBuilder.Entity<Filme>()
-                .Property(f => f.AnoLancamento)
-                .HasColumnName("release_year")
-                .HasColumnType("varchar(4)");
+            //modelBuilder.Entity<Filme>()
+            //    .Property(f => f.AnoLancamento)
+            //    .HasColumnName("release_year")
+            //    .HasColumnType("varchar(4)");
 
-            modelBuilder.Entity<Filme>()
-                .Property(f => f.Duracao)
-                .HasColumnName("length");
+            //modelBuilder.Entity<Filme>()
+            //    .Property(f => f.Duracao)
+            //    .HasColumnName("length");
 
-            modelBuilder.Entity<Filme>()
-                .Property<DateTime>("last_update")
-                .HasColumnType("datetime")
-                .HasDefaultValueSql("getdate()");
+            //modelBuilder.Entity<Filme>()
+            //    .Property<DateTime>("last_update")
+            //    .HasColumnType("datetime")
+            //    .HasDefaultValueSql("getdate()");
+
+            //não preciso dos códigos de cima, porque já separei para uma classe especializada
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
         }
     }
 }
---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-04-XX-Mapeando-Filmes/Dados/AtorConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AtorConfiguration : IEntityTypeConfiguration<Ator>
+    {
+        public void Configure(EntityTypeBuilder<Ator> builder)
+        {
+            //modelBuilder.Entity<Ator>() não precisso mais disso, porque o builder já uma entidade Ator
+            builder
+                .ToTable("actor");
+
+            builder
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+
+            builder
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            builder
+                 .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            builder
+                .Property<DateTime>("last_update")
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
