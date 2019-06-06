61a253f8cfcb82de1518c4a2adaacac8b65c9781
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 22:27:02 -0300

    conclusão da aula 03 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-03-XX-Colunas-Existem-Camada-Negocio/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-03-XX-Colunas-Existem-Camada-Negocio/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace Alura.Filmes.App
 {
@@ -11,12 +14,25 @@ namespace Alura.Filmes.App
             using (var contexto = new AluraFilmesContexto())
             {
                 contexto.LogSQLToConsole();
-                foreach (var ator in contexto.Atores)
-                {
-                    System.Console.WriteLine(ator);
-                    //contexto.Entry(ator).Property("las_update").CurrentValue = DateTime.Now; //adicionado um valor para uma propriedade shadow. Tenho que fazer isso porque ela não pode, pela regra de negócio, ser declada na entidade Ator e, consequentemente, não consigo acessar a propriedade pelo objeto ator
-                }
-                System.Console.ReadLine();
+                //var ator1 = new Ator();
+                //ator1.PrimeiroNome = "Tom";
+                //ator1.UltimoNome = "Hanks";
+                //contexto.Entry(ator1).Property("las_update").CurrentValue = DateTime.Now; //adicionado um valor para uma propriedade shadow. Tenho que fazer isso porque ela não pode, pela regra de negócio, ser declada na entidade Ator e, consequentemente, não consigo acessar a propriedade pelo objeto ator1
+
+                //foreach (var ator in contexto.Atores)
+                //{
+                //    //Console.WriteLine(ator);
+                //    //Console.WriteLine(contexto.Entry(ator).Property("last_update").CurrentValue); //mostrando o valor da propriedade shadow
+                //}
+
+                //listar os 10 atores  modificados recentemente
+                var atores = contexto.Atores
+                    .OrderByDescending(a => EF.Property<DateTime>(a, "last_update")) //pegando o valor da propriedade shadow e usando como parametro para executar um order by desc
+                    .Take(10); //pegando apenas os 10 primeiro
+
+                foreach (var ator in atores)
+                    Console.WriteLine($"{ator} - {contexto.Entry(ator).Property("last_update").CurrentValue}");
+                Console.ReadLine();
             }
         }
     }
