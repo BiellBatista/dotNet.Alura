0fe64af13ca433b1295df653dfab851ae48437e7
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 21:52:26 -0300

    conclusão do módulo 01 da aula 03 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-03-XX-Colunas-Existem-Camada-Negocio/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,41 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ator>().ToTable("actor");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            modelBuilder.Entity<Ator>() //para a entidade ator
+                .Property<DateTime>("last_update") //crie uma propriedade shadow do tipo datetime que faz referencia a coluna last_update
+                .HasColumnType("datetime"); //a propriedade shadow faz referencia a uma coluna do tipo datetime
+        }
+    }
+}
