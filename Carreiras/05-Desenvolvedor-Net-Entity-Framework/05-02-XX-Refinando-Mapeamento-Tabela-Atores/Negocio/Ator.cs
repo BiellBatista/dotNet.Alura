753d1d82e4e5e2a4ef3e6d4fc7689ffa8857d165
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-05 21:16:28 -0300

    conclusão do módulo 05 da aula 02 da seção "Entity Framework Core parte 1: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Negocio/Ator.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/05-02-XX-Refinando-Mapeamento-Tabela-Atores/Negocio/Ator.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Alura.Filmes.App.Negocio
 {
@@ -7,8 +8,10 @@ namespace Alura.Filmes.App.Negocio
     {
         [Column("actor_id")]
         public int Id { get; set; }
+        [Required]
         [Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
+        [Required]
         //falando que esta coluna é do tipo varchar(45)
         [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
