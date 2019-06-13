fae62c77889a6d0eafa5bf2c49fe71b182a00d46
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-13 08:25:40 -0300

    conclusão do módulo 05 da aula 01 da seção "Entity Framework Core parte 2: Mapeando um banco pré-existente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-02-XX-Classificacao-Indicativa-Filmes/Negocio/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Alura.Filmes.App.Negocio
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string AnoLancamento { get; set; }
+        public short Duracao { get; set; }
+        public IList<FilmeAtor> Atores { get; set; }
+        public Idioma IdiomaFalado { get; set; }
+        public Idioma IdiomaOriginal { get; set; }
+        public IEnumerable<FilmeCategoria> Categorias { get; internal set; }
+
+        public Filme()
+        {
+            Atores = new List<FilmeAtor>();
+        }
+
+        public override string ToString()
+        {
+            return $"Filme ({Id}): {Titulo} - {Descricao} - {AnoLancamento}";
+        }
+    }
+}
+
+/**
+ * O EF não consegue descobrir o relacionamento para duas propriedades da mesma classe
+ */
