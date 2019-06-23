b6b3b2a7b516f2813b3c7a45a6f5b46e0cb674e3
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-23 01:51:09 -0300

    conclusão do módulo 04 da aula 06 da seção "Entity Framework Core parte 2: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-06-XX-Stored-Procedures/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-06-XX-Stored-Procedures/Program.cs
@@ -13,28 +13,40 @@ namespace Alura.Filmes.App
             using (var contexto = new AluraFilmesContexto())
             {
                 contexto.LogSQLToConsole();
+                //poso executar comandos de INSERT, DELE e UPDATE no método ExecuteSqlCommand
+                var insert = "INSERT INTO language(name) VALUES('Teste 1'), ('Teste 2'), ('Teste 3')";
+                var delete = "DELETE FROM language WHERE name LIKE 'Teste%'";
 
-                var categoria = "Action"; //36
+                //executando os SQL
+                var registrosInsert = contexto.Database.ExecuteSqlCommand(insert);
+                var registrosDelete = contexto.Database.ExecuteSqlCommand(delete);
 
-                var paramCategoria = new SqlParameter("category_name", categoria); //Criando um parametro para minhas procedures
-                //o primeiro argumento é nome do parametro que está definido na procedure/function e o segundo argumento é o valor
-                var paramTotal = new SqlParameter
-                {
-                    ParameterName = "@total_actors",
-                    Size = 4,
-                    Direction = System.Data.ParameterDirection.Output
-                };
-
-                contexto.Database
-                    //a assinatura da procedure deve ser exatamente a mesma que está no banco de dados
-                    .ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT",
-                                        paramCategoria,
-                                        paramTotal); //executando um comando SQL (Procedure/Function)
-                //o último argumento é o valor de saída do ExecuteSqlCommand
-                Console.WriteLine($"O total de atores na categoria {categoria} é de {paramTotal.Value}.");
+                Console.WriteLine($"O total de registros afetados é {registrosInsert}, {registrosDelete}");
 
                 Console.ReadLine();
             }
+        }
+
+        static void StoredProcedure(DbContext contexto)
+        {
+            var categoria = "Action"; //36
+
+            var paramCategoria = new SqlParameter("category_name", categoria); //Criando um parametro para minhas procedures
+                                                                               //o primeiro argumento é nome do parametro que está definido na procedure/function e o segundo argumento é o valor
+            var paramTotal = new SqlParameter
+            {
+                ParameterName = "@total_actors",
+                Size = 4,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            contexto.Database
+                //a assinatura da procedure deve ser exatamente a mesma que está no banco de dados
+                .ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT",
+                                    paramCategoria,
+                                    paramTotal); //executando um comando SQL (Procedure/Function)
+                                                 //o último argumento é o valor de saída do ExecuteSqlCommand
+            Console.WriteLine($"O total de atores na categoria {categoria} é de {paramTotal.Value}.");
         }
     }
 }
