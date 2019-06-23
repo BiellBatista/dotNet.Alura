4cf23824d7be46439d1c9af4d436cca2fb059a44
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-23 01:20:49 -0300

    conclusão da aula 05 da seção "Entity Framework Core parte 2: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-05-XX-Assumindo-Controle-Geracao-SQL/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-05-XX-Assumindo-Controle-Geracao-SQL/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
-using Alura.Filmes.App.Negocio;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Linq;
 
 namespace Alura.Filmes.App
 {
@@ -24,12 +22,22 @@ namespace Alura.Filmes.App
                             GROUP BY a.actor_id
                             ORDER BY total DESC)
                             filmes ON filmes.actor_id = a.actor_id";
+                //usando uma view
+                /*
+                 * Quando um SQL for muito usado, é bom passar ele para VIEW
+                 */
+                var sql2 = @"SELECT a.*
+                             FROM actor a
+                                INNER JOIN top5_most_starred_actors filmes
+                                ON filmes.actor_id = a.actor_id";
 
                 //falando para o EF usar o meu SQL e não gerar um novo
                 var atoresMaisAtuantes = contexto.Atores.FromSql(sql)
                                             .Include(a => a.Filmografia);
+                var atoresMaisAtuantes2 = contexto.Atores.FromSql(sql2)
+                                            .Include(a => a.Filmografia);
 
-                foreach (var ator in atoresMaisAtuantes)
+                foreach (var ator in atoresMaisAtuantes2)
                 {
                     Console.WriteLine($"O autor {ator.PrimeiroNome} {ator.UltimoNome} atuou em {ator.Filmografia.Count} filmes.");
                 }
