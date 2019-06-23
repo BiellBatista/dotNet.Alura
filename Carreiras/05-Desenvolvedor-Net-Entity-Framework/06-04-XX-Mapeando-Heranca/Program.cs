bf3361fa658271483b810f3f76d0504796f079f6
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-23 00:24:53 -0300

    conclusão do módulo 03 da aula 04 da seção "Entity Framework Core parte 2: Mapeando um banco pré-existente"

--- Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-04-XX-Mapeando-Heranca/Program.cs
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-04-XX-Mapeando-Heranca/Program.cs
@@ -31,3 +31,11 @@ namespace Alura.Filmes.App
         }
     }
 }
+
+/*
+ * Herança - Padrões de Mapeamento
+ * 
+ * TPH - Table Per Hierarchy: ele cria uma única tabela para a hierarquia.
+ * TPC - Table Per Concrete Type: ele cria uma tabela por classes concretas. Ele vai lá na classe filha e vai criar uma tabela por cada classe (esse seria o padrão utilizado na aula 04, se eu tivesse colocado a tabela Pessoa no contexto para ser rastreada)
+ * TPT - Table Per Type: ele cria uma tabela para todos os tipos que estão na hierarquia.
+ */
