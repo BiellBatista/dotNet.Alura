d4790c616e9ab1e14e619da919c185dc39ff6c61
Gabriel Batista <gabriel.texteis@gmail.com>
2019-06-23 01:26:10 -0300

    preparando o ambiente para aula 06 da seção "Entity Framework Core parte 2: Mapeando um banco pré-existente"

---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-06-XX-Stored-Procedures/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,33 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Filmes.App.Dados
+{
+    class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<FilmeAtor> Elenco { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AluraFilmes;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoriaConfiguration());
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeAtorConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeCategoriaConfiguration());
+            modelBuilder.ApplyConfiguration(new FuncionarioConfiguration());
+            modelBuilder.ApplyConfiguration(new IdiomaConfiguration());
+        }
+    }
+}
---
+++ Carreiras/05-Desenvolvedor-Net-Entity-Framework/06-06-XX-Stored-Procedures/Program.cs
@@ -0,0 +1,20 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
