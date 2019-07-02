4ad005300490f85f86f8c77e212bb37366a79aa7
Gabriel Batista <gabriel.batista@kognita.com>
2019-07-02 17:46:24 -0300

    conclusão do módulo 03 da aula 02 da seção "APIs Rest com Asp.NET Core 2.1 Parte 1: Da app MVC para API"

--- Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/01-Introducao-Web-APIs/Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/01-Introducao-Web-APIs/Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -25,5 +25,62 @@ namespace Alura.WebAPI.WebApp.Api
                 return NotFound();
             return Json(model.ToModel());
         }
+
+        [HttpPost]
+        public IActionResult Incluir(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+
+                var uri = Url.Action("Recuperar", new { id = livro.Id }); //criando uma URL a partir de uma action de um controler. O primeiro argumento é a action e o segundo é o identificador do objeto na base de dados
+
+                return Created(uri, livro); //código 201
+                /**
+                 * Retornando um objeto, após a conclusão de sua operação
+                 */
+            }
+
+            return BadRequest(); //requisição inválida (error 400)
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok(); //código 200
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent(); //OK, mas não tem nenhum conteúdo (código 204)
+        }
     }
 }
+
+/*
+ * Created() --> 201
+ * Ok() --> 200
+ * BadRequest() --> 400
+ * NoContent() --> 204
+ */
