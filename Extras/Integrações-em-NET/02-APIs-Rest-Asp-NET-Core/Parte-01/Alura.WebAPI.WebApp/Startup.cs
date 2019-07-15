dc9aac39f5372f919cd0ac6de2099466a162ad85
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-15 00:25:23 -0300

    conclusão da aula 06 da seção "APIs Rest com Asp.NET Core 2.1 Parte 1: Da app MVC para API"

--- Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/Parte-01/Alura.WebAPI.WebApp/Startup.cs
+++ Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/Parte-01/Alura.WebAPI.WebApp/Startup.cs
@@ -59,18 +59,18 @@ namespace Alura.ListaLeitura.WebApp
             {
                 options.DefaultAuthenticateScheme = "JwtBearer";
                 options.DefaultChallengeScheme = "JwtBearer";
-            }).AddJwtBearer("JwtBearer", options =>
-            {
+            }).AddJwtBearer("JwtBearer", options => {
                 //configurando a criação do token
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true, //será validado o token
-                    ValidateAudience = true, //o juiz
-                    ValidateLifetime = true, //e a expiração
+                    ValidateAudience = true, //terá um juiz
+                    ValidateLifetime = true, //terá uma expiração
+                    ValidateIssuerSigningKey = true, //irei validar a signature (LoginController)
                     IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")), //chave que será usada para validar
                     ClockSkew = TimeSpan.FromMinutes(5), //a formula que será usada no teste de expiração (tempo de vida do token)
                     ValidIssuer = "Alura.WebApp", //dizendo o cara (aplicação) que é valido
-                    ValidAudience = "Postman" //cliente/audiencia o postman
+                    ValidAudience = "Postman", //cliente/audiencia o postman
                 };
             });
         }
