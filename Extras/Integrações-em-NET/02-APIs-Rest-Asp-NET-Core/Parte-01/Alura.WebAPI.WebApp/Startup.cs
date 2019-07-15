1c59b2b4b166c78c36ddcf7e396d0723f4d61675
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-14 23:45:00 -0300

    conclusão do módulo 03 da aula 06 da seção "APIs Rest com Asp.NET Core 2.1 Parte 1: Da app MVC para API"

--- Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/Parte-01/Alura.WebAPI.WebApp/Startup.cs
+++ Extras/Integrações-em-NET/02-APIs-Rest-Asp-NET-Core/Parte-01/Alura.WebAPI.WebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Alura.WebAPI.WebApp.Formatters;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace Alura.ListaLeitura.WebApp
 {
@@ -50,8 +52,27 @@ namespace Alura.ListaLeitura.WebApp
             services.AddMvc(options =>
             {
                 options.OutputFormatters.Add(new LivroCsvFormatter());
-            })
-                    .AddXmlSerializerFormatters(); //coloque o formatado XML (a depedencia já está baixada). Com isso, o usuário pode solicitar dados em XML, em vez de receber apenas JSON
+            }).AddXmlSerializerFormatters(); //coloque o formatado XML (a depedencia já está baixada). Com isso, o usuário pode solicitar dados em XML, em vez de receber apenas JSON
+
+            //criando autenticação via token (JWT Bearer)
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                //configurando a criação do token
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true, //será validado o token
+                    ValidateAudience = true, //o juiz
+                    ValidateLifetime = true, //e a expiração
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid")), //chave que será usada para validar
+                    ClockSkew = TimeSpan.FromMinutes(5), //a formula que será usada no teste de expiração (tempo de vida do token)
+                    ValidIssuer = "Alura.WebApp", //dizendo o cara (aplicação) que é valido
+                    ValidAudience = "Postman" //cliente/audiencia o postman
+                };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
