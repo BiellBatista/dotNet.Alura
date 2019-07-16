ece96a4cbe3a20a1a87ec805fcb9e884b6cc4b1d
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-16 10:55:19 -0300

    ambiente preparado para a aula 03 da seção "APIs Rest com Asp.NET Core 2.1 Parte 2: Consumindo nossa API"

---
+++ Carreiras/03-Desenvolvedor-ASP.NET-Core/02-APIs-Rest-Asp-NET-Core/03-Requisicoes-Upload/Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -0,0 +1,46 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.HttpClients
+{
+    public class LivroApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public LivroApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<LivroApi> GetLivroAsync(int id)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}");
+            resposta.EnsureSuccessStatusCode();
+
+            return await resposta.Content.ReadAsAsync<LivroApi>();
+        }
+
+        public async Task<byte[]> GetCapaLivroAsync(int id)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}/capa");
+            resposta.EnsureSuccessStatusCode();
+
+           return await resposta.Content.ReadAsByteArrayAsync();
+        }
+
+        public async Task DeleteLivroAsync(int id)
+        {
+            var resposta = await _httpClient.DeleteAsync($"livros/{id}");
+            resposta.EnsureSuccessStatusCode();
+        }
+
+        public async Task<Lista> GetListaLeituraAsync(TipoListaLeitura tipo)
+        {
+            var resposta = await _httpClient.GetAsync($"listasleitura/{tipo}");
+            resposta.EnsureSuccessStatusCode(); //verificando se houve um status code da família 200
+            return await resposta.Content.ReadAsAsync<Lista>();
+        }
+    }
+}
