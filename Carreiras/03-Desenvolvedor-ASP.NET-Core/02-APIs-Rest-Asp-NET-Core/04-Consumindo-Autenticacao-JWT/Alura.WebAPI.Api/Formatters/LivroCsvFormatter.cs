0580610750c1f911174667df9918e8c9ff77f568
Gabriel Batista <gabriel.texteis@gmail.com>
2019-07-27 03:52:55 -0300

    ambiente preparado para a aula 04 da seção "APIs Rest com Asp.NET Core 2.1 Parte 2: Consumindo nossa API"

---
+++ Carreiras/03-Desenvolvedor-ASP.NET-Core/02-APIs-Rest-Asp-NET-Core/04-Consumindo-Autenticacao-JWT/Alura.WebAPI.Api/Formatters/LivroCsvFormatter.cs
@@ -0,0 +1,44 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.Api.Formatters
+{
+    public class LivroCsvFormatter : TextOutputFormatter
+    {
+        public LivroCsvFormatter()
+        {
+            var textMediaType = MediaTypeHeaderValue.Parse("text/csv");
+            var apptMediaType = MediaTypeHeaderValue.Parse("application/csv");
+
+            SupportedMediaTypes.Add(textMediaType);
+            SupportedMediaTypes.Add(apptMediaType);
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return type == typeof(LivroApi);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var livroEmCsv = "";
+
+            if (context.Object is LivroApi)
+            {
+                var livro = context.Object as LivroApi;
+
+                livroEmCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista};";
+            }
+
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroEmCsv);
+            }
+        }
+    }
+}
---
+++ Carreiras/03-Desenvolvedor-ASP.NET-Core/02-APIs-Rest-Asp-NET-Core/04-Consumindo-Autenticacao-JWT/Alura.WebAPI.WebApp/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Alura.ListaLeitura.Seguranca;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Alura.WebAPI.WebApp.Formatters;
+using Alura.WebAPI.WebApp.HttpClients;
+
+namespace Alura.ListaLeitura.WebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("AuthDB"));
+            });
+
+            services.AddIdentity<Usuario, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<AuthDbContext>();
+
+            services.AddHttpClient<LivroApiClient>(client =>
+            {
+                client.BaseAddress = new System.Uri("http://localhost:6000/api/");
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Usuario/Login";
+            });
+
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Add(new LivroCsvFormatter());
+            }).AddXmlSerializerFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
