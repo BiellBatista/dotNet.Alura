5d67d0d5a4cbbf674b1577f5b5dc853a353cc954
Gabriel Batista <gabriel.texteis@gmail.com>
2019-08-08 00:50:02 -0300

    conclusão do módulo 04 da aula 02 da seção "APIs Rest com Asp.NET Core 2.1 Parte 3: padronizando a API"

---
+++ Carreiras/03-Desenvolvedor-ASP.NET-Core/03-APIs-Rest-Asp-NET-Core/02-Lidando-Grandes-Colecoes/Alura.WebAPI.Api/Models/LivroOrdem.cs
@@ -0,0 +1,24 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace Alura.WebAPI.Api.Models
+{
+    public static class LivroOdemExtensions
+    {
+        public static IQueryable<Livro> AplicaOrdem(this IQueryable<Livro> query, LivroOrdem ordem)
+        {
+            if(!(ordem is null))
+            {
+                query = query.OrderBy(ordem.OrdenarPor);
+            }
+
+            return query;
+        }
+    }
+
+    public class LivroOrdem
+    {
+        public string OrdenarPor { get; set; }
+    }
+}
