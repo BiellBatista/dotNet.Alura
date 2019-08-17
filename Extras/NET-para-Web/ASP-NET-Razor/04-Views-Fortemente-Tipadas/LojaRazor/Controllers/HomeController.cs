0b0003537ba3dd92d765b0b592a8235e949cb54a
Gabriel Batista <gabriel.texteis@gmail.com>
2019-08-17 20:44:29 -0300

    ambiente preparado para a aula 04 da seção "ASP.NET: Razor"

---
+++ Extras/NET-para-Web/ASP-NET-Razor/04-Views-Fortemente-Tipadas/LojaRazor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using LojaRazor.DAO;
+using System.Web.Mvc;
+
+namespace LojaRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ProdutosDAO produtosDAO = new ProdutosDAO();
+            ViewBag.Produtos = produtosDAO.Ofertas();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
