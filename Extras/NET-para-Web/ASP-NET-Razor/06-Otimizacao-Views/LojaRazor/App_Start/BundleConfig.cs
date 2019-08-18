c976d3260600409f1e20b076e8df8584dc634b25
Gabriel Batista <gabriel.texteis@gmail.com>
2019-08-17 23:33:40 -0300

    conclusão do módulo 01 da aula 06 da seção "ASP.NET: Razor"

--- Extras/NET-para-Web/ASP-NET-Razor/06-Otimizacao-Views/LojaRazor/App_Start/BundleConfig.cs
+++ Extras/NET-para-Web/ASP-NET-Razor/06-Otimizacao-Views/LojaRazor/App_Start/BundleConfig.cs
@@ -6,7 +6,18 @@ namespace LojaRazor
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //juntando os estilos em um único arquivo
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
 
+            ScriptBundle scripts = new ScriptBundle("~/bundles/scripts");
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jquery.validate.js");
+            scripts.Include("~/Scripts/jquery.validate.unobtrusive.js");
+
+            bundles.Add(estilos);
+            bundles.Add(scripts);
         }
     }
 }
