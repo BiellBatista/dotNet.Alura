2be621707d119deba12a76d2bb2e718f27d9f202
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-19 22:22:57 -0300

    preparando ambiente para a aula 05 da seção "Projeto Completo: Controle Financeiro com Asp.NET MVC 5"

---
+++ Extras/NET-para-Web/Controle-Financeiro-ASP-NET-MVC-5/05-Seguranca/Financas/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using Financas.DAO;
+using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuarioDao;
+
+        public UsuarioController(UsuarioDAO usuarioDao)
+        {
+            this.usuarioDao = usuarioDao;
+        }
+
+        // GET: Usuario
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuarioDao.Adiciona(usuario);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", usuario);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDao.Lista();
+
+            return View(usuarios);
+        }
+    }
+}
---
+++ Extras/NET-para-Web/Controle-Financeiro-ASP-NET-MVC-5/05-Seguranca/Financas/Entidades/Movimentacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Financas.Entidades
+{
+    public class Movimentacao
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public Tipo Tipo { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
