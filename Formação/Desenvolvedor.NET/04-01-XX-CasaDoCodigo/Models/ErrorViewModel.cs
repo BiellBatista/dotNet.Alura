0fe9596f6625a28b8dc2e9767559a2c9100a0d06
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-21 18:55:58 -0300

    conclusão do módulo 05 da aula 01 da seção "ASP.NET Core Parte 3: Identity"

---
+++ Formação/Desenvolvedor.NET/04-01-XX-CasaDoCodigo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _04_01_XX_CasaDoCodigo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
