c00669a183f2238dff1d0c3b34789e728f549ac1
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-22 21:39:22 -0300

    preparando ambiente para a aula 04 da seção "ASP.NET Core Parte 3: Identity"

---
+++ Formação/Desenvolvedor.NET/04-04-XX-CasaDoCodigo/Models/UpdateQuantidadeResponse.cs
@@ -0,0 +1,16 @@
+﻿using _04_03_XX_CasaDoCodigo.Models.ViewModels;
+
+namespace _04_03_XX_CasaDoCodigo.Models
+{
+    public class UpdateQuantidadeResponse
+    {
+        public UpdateQuantidadeResponse(ItemPedido itemPedido, CarrinhoViewModel carrinhoViewModel)
+        {
+            ItemPedido = itemPedido;
+            CarrinhoViewModel = carrinhoViewModel;
+        }
+
+        public ItemPedido ItemPedido { get; }
+        public CarrinhoViewModel CarrinhoViewModel { get; }
+    }
+}
