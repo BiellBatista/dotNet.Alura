9964bcc7394e995ddc185f50e3ca762e890db17c
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-23 01:02:26 -0300

    conclusão do módulo 04 da aula 04 da seção "ASP.NET Core Parte 3: Identity"

--- Formação/Desenvolvedor.NET/04-04-XX-CasaDoCodigo/Migrations/AppIdentity/20200323035910_UsuarioCustomizado.Designer.cs
+++ Formação/Desenvolvedor.NET/04-04-XX-CasaDoCodigo/Migrations/AppIdentity/20200323035910_UsuarioCustomizado.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _04_03_XX_CasaDoCodigo.Models;
 
 namespace _04_03_XX_CasaDoCodigo.Migrations.AppIdentity
 {
     [DbContext(typeof(AppIdentityContext))]
-    partial class AppIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20200323035910_UsuarioCustomizado")]
+    partial class UsuarioCustomizado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
