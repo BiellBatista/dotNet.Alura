c6c82f993d07c66a6eb37c905302096738da62ea
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-23 01:45:31 -0300

    preparando ambiente para a aula 05 da seção "ASP.NET Core Parte 3: Identity"

---
+++ Formação/Desenvolvedor.NET/04-05-XX-CasaDoCodigo/Repositories/IHttpHelper.cs
@@ -0,0 +1,15 @@
+﻿using _04_05_XX_CasaDoCodigo.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace _04_05_XX_CasaDoCodigo
+{
+    public interface IHttpHelper
+    {
+        IConfiguration Configuration { get; }
+        int? GetPedidoId();
+        void SetPedidoId(int pedidoId);
+        void ResetPedidoId();
+        void SetCadastro(Cadastro cadastro);
+        Cadastro GetCadastro();
+    }
+}
