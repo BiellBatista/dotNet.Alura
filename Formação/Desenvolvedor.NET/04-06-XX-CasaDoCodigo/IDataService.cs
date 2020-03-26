22a4feebd6e0dcec101f69c8d9ccbf5145671a16
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-25 22:46:11 -0300

    preparando ambiente para a aula 06 da seção "ASP.NET Core Parte 3: Identity"

---
+++ Formação/Desenvolvedor.NET/04-06-XX-CasaDoCodigo/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _04_06_XX_CasaDoCodigo
+{
+    public interface IDataService
+    {
+        Task InicializaDBAsync(IServiceProvider provider);
+    }
+}
---
+++ Formação/Desenvolvedor.NET/04-06-XX-CasaDoCodigo/Repositories/HttpHelper.cs
@@ -0,0 +1,63 @@
+﻿using _04_06_XX_CasaDoCodigo.Areas.Identity.Data;
+using _04_06_XX_CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace _04_06_XX_CasaDoCodigo
+{
+    //MELHORIA: 8) dados do cadastro gravados na sessão
+    public class HttpHelper : IHttpHelper
+    {
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly UserManager<AppIdentityUser> userManager;
+
+        public IConfiguration Configuration { get; }
+
+        public HttpHelper(IHttpContextAccessor contextAccessor,
+            IConfiguration configuration,
+            UserManager<AppIdentityUser> userManager)
+        {
+            this.contextAccessor = contextAccessor;
+            this.userManager = userManager;
+            Configuration = configuration;
+        }
+
+        public int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32($"pedidoId_{GetClienteId()}");
+        }
+
+        private string GetClienteId()
+        {
+            return userManager.GetUserId(contextAccessor.HttpContext.User);
+        }
+
+        public void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32($"pedidoId_{GetClienteId()}", pedidoId);
+        }
+
+        public void ResetPedidoId()
+        {
+            contextAccessor.HttpContext.Session.Remove($"pedidoId_{GetClienteId()}");
+        }
+
+        public void SetCadastro(Cadastro cadastro)
+        {
+            string json = JsonConvert.SerializeObject(cadastro.GetClone());
+            contextAccessor.HttpContext.Session.SetString("cadastro", json);
+        }
+
+        public Cadastro GetCadastro()
+        {
+            string json = contextAccessor.HttpContext.Session.GetString("cadastro");
+            if (string.IsNullOrWhiteSpace(json))
+                return new Cadastro();
+
+            return JsonConvert.DeserializeObject<Cadastro>(json);
+        }
+    }
+
+}
