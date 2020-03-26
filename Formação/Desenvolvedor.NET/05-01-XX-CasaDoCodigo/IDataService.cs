2857394ab904813d9d1b2c98c6698d3f1a1f071e
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-26 00:35:39 -0300

    preparando ambiente para a aula 01 da seção "ASP.NET Core Parte 4: IdentityServer"

---
+++ Formação/Desenvolvedor.NET/05-01-XX-CasaDoCodigo/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _05_01_XX_CasaDoCodigo
+{
+    public interface IDataService
+    {
+        Task InicializaDBAsync(IServiceProvider provider);
+    }
+}
---
+++ Formação/Desenvolvedor.NET/05-01-XX-CasaDoCodigo/Models/Pedido.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _05_01_XX_CasaDoCodigo.Models
+{
+    //MELHORIA: 5) arquivo modelo.cs foi quebrado em vários arquivos
+    //veja o diagrama
+    public class Pedido : BaseModel
+    {
+        public Pedido(string clienteId)
+        {
+            ClienteId = clienteId;
+            Cadastro = new Cadastro();
+        }
+
+        public Pedido(Cadastro cadastro, string clienteId)
+        {
+            ClienteId = clienteId;
+            Cadastro = cadastro;
+        }
+
+        public List<ItemPedido> Itens { get; private set; } = new List<ItemPedido>();
+
+        [ForeignKey("CadastroId")]
+        public int CadastroId { get; set; }
+        [Required]
+        public virtual Cadastro Cadastro { get; private set; }
+        [Required]
+        public string ClienteId { get; set; }
+    }
+}
