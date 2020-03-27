77c0b3cc80f822a76f9208f05ef1854e77e6a04f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-03-26 23:17:57 -0300

    preparando ambiente para a aula 02 da seção "ASP.NET Core Parte 4: IdentityServer"

---
+++ Formação/Desenvolvedor.NET/05-02-XX-CasaDoCodigo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace _05_02_XX_CasaDoCodigo
+{
+    //MELHORIA: 7) Projeto atualizado para ASP.NET Core 2.2
+    //Veja propriedades do projeto
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost webHost = BuildWebHost(args);
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Formação/Desenvolvedor.NET/05-02-XX-CasaDoCodigo/Repositories/IHttpHelper.cs
@@ -0,0 +1,15 @@
+﻿using _05_02_XX_CasaDoCodigo.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace _05_02_XX_CasaDoCodigo
+{
+    public interface IHttpHelper
+    {
+        IConfiguration Configuration { get; }
+        int? GetPedidoId();
+        void SetPedidoId(int pedidoId);
+        void ResetPedidoId();
+        void SetCadastro(Cadastro cadastro);
+        Cadastro GetCadastro();
+    }
+}
