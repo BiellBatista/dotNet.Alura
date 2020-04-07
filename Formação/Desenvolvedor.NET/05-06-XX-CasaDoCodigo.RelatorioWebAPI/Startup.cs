67b34646d0be97d036185f728a8657c178438a03
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-06 22:41:38 -0300

    conclusão do módulo 04 da aula 06 da seção "ASP.NET Core Parte 4: IdentityServer"

--- Formação/Desenvolvedor.NET/05-06-XX-CasaDoCodigo.RelatorioWebAPI/Startup.cs
+++ Formação/Desenvolvedor.NET/05-06-XX-CasaDoCodigo.RelatorioWebAPI/Startup.cs
@@ -25,6 +25,20 @@ namespace _05_06_XX_CasaDoCodigo.RelatorioWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // a autenticação irá validar o token vindo do IdentityServer
+            // além disso, irá garantir que é valido
+            // autenticação básica é aquela que o usuário e a senha trafegam pela rede
+            // iremos usar a autenticação ao portador: somente o token trafega
+            services
+                .AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.ApiName = "CasaDoCodigo.Relatorio";
+                    options.ApiSecret = "511536EF-F270-4058-80CA-1C89C192F69A";
+                    options.Authority = Configuration["CasaDoCodigoIdentityServerUrl"];
+                    options.RequireHttpsMetadata = false;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
