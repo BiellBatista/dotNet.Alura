adf6827a469a3ef352ab01a50521ad38c7d9d562
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-07 22:39:28 -0300

    preparando ambiente para a aula 04 da seção "ASP.NET Core Parte 5: Modularização e Componentização"

---
+++ Formação/Desenvolvedor.NET/06-01-XX-CasaDoCodigo/Areas/Catalago/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _06_01_XX_CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06_01_XX_CasaDoCodigo.Areas.Catalago.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+
+        public HomeController(IProdutoRepository produtoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+        }
+
+        public async Task<IActionResult> Index(string pesquisa)
+        {
+            return View(await produtoRepository.GetProdutosAsync(pesquisa));
+        }
+    }
+}
