a159eb6bca24745b6c2f8667fbdb97d164065ea3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-10 12:33:41 -0300

    preparando ambiente para a aula 02 da seção "ASP.NET Core Parte 5: Modularização e Componentização"

---
+++ Formação/Desenvolvedor.NET/06-02-XX-CasaDoCodigo/Areas/Catalogo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _06_02_XX_CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06_02_XX_CasaDoCodigo.Areas.Catalago.Controllers
+{
+    [Area("Catalogo")]
+    public class HomeController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+
+        public HomeController(IProdutoRepository produtoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+        }
+
+        public async Task<IActionResult> Index(string pesquisa)
+        {
+            return View(await produtoRepository.GetProdutosAsync(pesquisa));
+        }
+    }
+}
---
+++ Formação/Desenvolvedor.NET/06-02-XX-CasaDoCodigo/Repositories/IHttpHelper.cs
@@ -0,0 +1,15 @@
+﻿using _06_02_XX_CasaDoCodigo.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace _06_02_XX_CasaDoCodigo
+{
+    public interface IHttpHelper
+    {
+        IConfiguration Configuration { get; }
+        int? GetPedidoId();
+        void SetPedidoId(int pedidoId);
+        void ResetPedidoId();
+        void SetCadastro(Cadastro cadastro);
+        Cadastro GetCadastro();
+    }
+}
