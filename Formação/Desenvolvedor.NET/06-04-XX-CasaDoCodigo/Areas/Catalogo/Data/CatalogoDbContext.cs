02aeb7eff32c9c58bbbd60fbd8cedea693575e48
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-11 21:04:17 -0300

    conclusão do módulo 01 da aula 04 da seção "ASP.NET Core Parte 5: Modularização e Componentização"

---
+++ Formação/Desenvolvedor.NET/06-04-XX-CasaDoCodigo/Areas/Catalogo/Data/CatalogoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using _06_04_XX_CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _06_04_XX_CasaDoCodigo.Areas.Catalogo.Data
+{
+    public class CatalogoDbContext : DbContext
+    {
+        // chamando o construtor pai (base) e passando o parametro
+        public CatalogoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Categoria>(b =>
+            {
+                b.HasKey(t => t.Id);
+            });
+
+            modelBuilder.Entity<Produto>(b =>
+            {
+                b.HasKey(t => t.Id);
+            });
+        }
+    }
+}
