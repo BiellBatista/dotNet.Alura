5a0a20157144b0e17656201d3ab3eba320ead7f4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-12 18:29:24 -0300

    preparando ambiente para a aula 02 da seção "Curso de C#: Paralelismo no mundo real"

---
+++ Extras/Cursos/Paralelismo-Mundo-Real/02-XX-ByteBank.Core/Model/Movimento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02_XX_ByteBank.Core.Model
+{
+    public enum TipoMovimento
+    {
+        Saque,
+        Deposito,
+        Transferencia,
+        RecargaCelular,
+        PagamentoDebito
+    }
+
+    public class Movimento
+    {
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public TipoMovimento Tipo { get; set; }
+    }
+}
---
+++ Extras/Cursos/Paralelismo-Mundo-Real/02-XX-ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using _02_XX_ByteBank.Core.Repository;
+using _02_XX_ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace _02_XX_ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            var contas = r_Repositorio.GetContaClientes();
+            var contasQuantiadePorThread = contas.Count() / 4;
+
+            var contas_parte1 = contas.Take(contasQuantiadePorThread);
+            var contas_parte2 = contas.Skip(contasQuantiadePorThread).Take(contasQuantiadePorThread);
+            var contas_parte3 = contas.Skip(contasQuantiadePorThread * 2).Take(contasQuantiadePorThread);
+            var contas_parte4 = contas.Skip(contasQuantiadePorThread * 3);
+
+            var resultado = new List<string>();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+
+            Thread thread_part1 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte1)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            Thread thread_part2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            Thread thread_part3 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            Thread thread_part4 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            thread_part1.Start();
+            thread_part2.Start();
+            thread_part3.Start();
+            thread_part4.Start();
+
+            while (thread_part1.IsAlive || thread_part2.IsAlive || thread_part3.IsAlive || thread_part4.IsAlive)
+            {
+                Thread.Sleep(250);
+            }
+
+            var fim = DateTime.Now;
+
+            AtualizarView(resultado, fim - inicio);
+        }
+
+        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
