5b1086f61a782913f474dd1d2a418853e5b1bea9
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-25 16:54:11 -0300

    conclusão do módulo 01 da aula 04 da seção "Curso de C#: Paralelismo no mundo real"

--- Extras/Cursos/Paralelismo-Mundo-Real/04-XX-ByteBank.View/MainWindow.xaml.cs
+++ Extras/Cursos/Paralelismo-Mundo-Real/04-XX-ByteBank.View/MainWindow.xaml.cs
@@ -26,55 +26,46 @@ namespace _04_XX_ByteBank.View
         private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
             BtnProcessar.IsEnabled = false;
-            //não preciso armazenar o contexto da thread, pois estou usando "async await"
-            //var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
             var contas = r_Repositorio.GetContaClientes();
 
-            AtualizarView(new List<string>(), TimeSpan.Zero);
+            PgsProgresso.Maximum = contas.Count();
+
+            LimparView();
 
             var inicio = DateTime.Now;
-            // este é o mesmo esquema do callback em JS
-            //ConsolidarContas(contas)
-            //    .ContinueWith(t =>
-            //    {
-            //        var fim = DateTime.Now;
-            //        var resultado = t.Result;
-            //        AtualizarView(resultado, fim - inicio);
-            //    }, taskSchedulerUI)
-            //    .ContinueWith(t =>
-            //    {
-            //        BtnProcessar.IsEnabled = true;
-            //    }, taskSchedulerUI);
-
             var resultado = await ConsolidarContas(contas);
             var fim = DateTime.Now;
+
             AtualizarView(resultado, fim - inicio);
             BtnProcessar.IsEnabled = true;
         }
 
-        //método antigo
-        //private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
-        //{
-        //    var resultado = new List<string>();
-        //    var tasks = contas.Select(c =>
-        //    {
-        //        return Task.Factory.StartNew(() =>
-        //        {
-        //            var resultadoConta = r_Servico.ConsolidarMovimentacao(c);
-        //            resultado.Add(resultadoConta);
-        //        });
-        //    });
-
-        //    return Task.WhenAll(tasks).ContinueWith(t =>
-        //    {
-        //        return resultado;
-        //    });
-        //}
-
         private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
-            var tasks = contas.Select(c => Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(c)));
+            var taskSchedulerGui = TaskScheduler.FromCurrentSynchronizationContext();
+            var tasks = contas.Select(c => Task.Factory.StartNew(() =>
+            {
+                var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(c);
+
+                Task.Factory.StartNew(
+                    () => PgsProgresso.Value++,
+                    CancellationToken.None,
+                    TaskCreationOptions.None,
+                    taskSchedulerGui
+                    );
+
+                return resultadoConsolidacao;
+            })
+            );
+
             return await Task.WhenAll(tasks);
+        }
+
+        private void LimparView()
+        {
+            LstResultados.ItemsSource = null;
+            TxtTempo.Text = null;
+            PgsProgresso.Value = 0;
         }
 
         private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
