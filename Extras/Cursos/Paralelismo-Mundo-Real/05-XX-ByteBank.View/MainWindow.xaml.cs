a0ddf80e840ce9941e46d657da7ea785dc0923c5
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-25 17:26:13 -0300

    preparando ambiente para a aula 05 da seção "Curso de C#: Paralelismo no mundo real"

---
+++ Extras/Cursos/Paralelismo-Mundo-Real/05-XX-ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using _05_XX_ByteBank.Core.Model;
+using _05_XX_ByteBank.Core.Repository;
+using _05_XX_ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _05_XX_ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            BtnProcessar.IsEnabled = false;
+            var contas = r_Repositorio.GetContaClientes();
+
+            PgsProgresso.Maximum = contas.Count();
+
+            LimparView();
+
+            var inicio = DateTime.Now;
+
+            var progress = new Progress<string>(str => PgsProgresso.Value++);
+            var resultado = await ConsolidarContas(contas, progress);
+            var fim = DateTime.Now;
+
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
+        }
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso)
+        {
+            var tasks = contas.Select(c => Task.Factory.StartNew(() =>
+            {
+                var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(c);
+                reportadorDeProgresso.Report(resultadoConsolidacao);
+
+                return resultadoConsolidacao;
+            })
+            );
+
+            return await Task.WhenAll(tasks);
+        }
+
+        private void LimparView()
+        {
+            LstResultados.ItemsSource = null;
+            TxtTempo.Text = null;
+            PgsProgresso.Value = 0;
+        }
+
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
