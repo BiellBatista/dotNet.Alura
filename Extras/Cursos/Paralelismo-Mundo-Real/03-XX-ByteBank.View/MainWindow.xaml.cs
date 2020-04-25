00695798fa8cab798b1334a3df5792970deba7c4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-24 22:44:23 -0300

    conclusão do módulo 02 da aula 03 da seção "Curso de C#: Paralelismo no mundo real"

--- Extras/Cursos/Paralelismo-Mundo-Real/03-XX-ByteBank.View/MainWindow.xaml.cs
+++ Extras/Cursos/Paralelismo-Mundo-Real/03-XX-ByteBank.View/MainWindow.xaml.cs
@@ -23,52 +23,64 @@ namespace _03_XX_ByteBank.View
             r_Servico = new ContaClienteService();
         }
 
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
             BtnProcessar.IsEnabled = false;
-
-            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+            //não preciso armazenar o contexto da thread, pois estou usando "async await"
+            //var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
             var contas = r_Repositorio.GetContaClientes();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
             // este é o mesmo esquema do callback em JS
-            ConsolidarContas(contas)
-                .ContinueWith(t =>
-                {
-                    var fim = DateTime.Now;
-                    var resultado = t.Result;
-                    AtualizarView(resultado, fim - inicio);
-                }, taskSchedulerUI)
-                .ContinueWith(t =>
-                {
-                    BtnProcessar.IsEnabled = true;
-                }, taskSchedulerUI);
+            //ConsolidarContas(contas)
+            //    .ContinueWith(t =>
+            //    {
+            //        var fim = DateTime.Now;
+            //        var resultado = t.Result;
+            //        AtualizarView(resultado, fim - inicio);
+            //    }, taskSchedulerUI)
+            //    .ContinueWith(t =>
+            //    {
+            //        BtnProcessar.IsEnabled = true;
+            //    }, taskSchedulerUI);
+
+            var resultado = await ConsolidarContas(contas);
+            var fim = DateTime.Now;
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
         }
 
-        private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        //método antigo
+        //private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        //{
+        //    var resultado = new List<string>();
+        //    var tasks = contas.Select(c =>
+        //    {
+        //        return Task.Factory.StartNew(() =>
+        //        {
+        //            var resultadoConta = r_Servico.ConsolidarMovimentacao(c);
+        //            resultado.Add(resultadoConta);
+        //        });
+        //    });
+
+        //    return Task.WhenAll(tasks).ContinueWith(t =>
+        //    {
+        //        return resultado;
+        //    });
+        //}
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
-            var resultado = new List<string>();
-            var tasks = contas.Select(c =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(c);
-                    resultado.Add(resultadoConta);
-                });
-            });
-
-            return Task.WhenAll(tasks).ContinueWith(t =>
-            {
-                return resultado;
-            });
+            var tasks = contas.Select(c => Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(c)));
+            return await Task.WhenAll(tasks);
         }
 
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
