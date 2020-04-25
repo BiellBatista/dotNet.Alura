f83574720862cdcb43d4cee4105e34a52b6cc9ca
Gabriel Batista <gabriel.texteis@gmail.com>
2020-04-24 23:01:28 -0300

    preparando ambiente para a aula 04 da seção "Curso de C#: Paralelismo no mundo real"

---
+++ Extras/Cursos/Paralelismo-Mundo-Real/04-XX-ByteBank.Core/Model/ContaCliente.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace _04_XX_ByteBank.Core.Model
+{
+    public class ContaCliente
+    {
+        public string NomeCliente { get; set; }
+        public List<Movimento> Movimentacoes { get; set; }
+        public decimal Investimento { get; set; }
+    }
+}
---
+++ Extras/Cursos/Paralelismo-Mundo-Real/04-XX-ByteBank.View/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using _04_XX_ByteBank.Core.Model;
+using _04_XX_ByteBank.Core.Repository;
+using _04_XX_ByteBank.Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _04_XX_ByteBank.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ContaClienteRepository r_Repositorio;
+        private readonly ContaClienteService r_Servico;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            r_Repositorio = new ContaClienteRepository();
+            r_Servico = new ContaClienteService();
+        }
+
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        {
+            BtnProcessar.IsEnabled = false;
+            //não preciso armazenar o contexto da thread, pois estou usando "async await"
+            //var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
+            var contas = r_Repositorio.GetContaClientes();
+
+            AtualizarView(new List<string>(), TimeSpan.Zero);
+
+            var inicio = DateTime.Now;
+            // este é o mesmo esquema do callback em JS
+            //ConsolidarContas(contas)
+            //    .ContinueWith(t =>
+            //    {
+            //        var fim = DateTime.Now;
+            //        var resultado = t.Result;
+            //        AtualizarView(resultado, fim - inicio);
+            //    }, taskSchedulerUI)
+            //    .ContinueWith(t =>
+            //    {
+            //        BtnProcessar.IsEnabled = true;
+            //    }, taskSchedulerUI);
+
+            var resultado = await ConsolidarContas(contas);
+            var fim = DateTime.Now;
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
+        }
+
+        //método antigo
+        //private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        //{
+        //    var resultado = new List<string>();
+        //    var tasks = contas.Select(c =>
+        //    {
+        //        return Task.Factory.StartNew(() =>
+        //        {
+        //            var resultadoConta = r_Servico.ConsolidarMovimentacao(c);
+        //            resultado.Add(resultadoConta);
+        //        });
+        //    });
+
+        //    return Task.WhenAll(tasks).ContinueWith(t =>
+        //    {
+        //        return resultado;
+        //    });
+        //}
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        {
+            var tasks = contas.Select(c => Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(c)));
+            return await Task.WhenAll(tasks);
+        }
+
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
+        {
+            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
+
+            LstResultados.ItemsSource = result;
+            TxtTempo.Text = mensagem;
+        }
+    }
+}
