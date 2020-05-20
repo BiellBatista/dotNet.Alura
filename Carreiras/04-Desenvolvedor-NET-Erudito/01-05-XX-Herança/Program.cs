587561da2d8493c393eba70c7343a4528ff10be1
Gabriel Batista <gabriel.texteis@gmail.com>
2020-05-20 00:51:21 -0300

    preparando ambiente para a aula 05 da seção "SOLID com C#: Orientação a Objetos com C#"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/01-05-XX-Herança/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _01_05_XX_Heranca
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
