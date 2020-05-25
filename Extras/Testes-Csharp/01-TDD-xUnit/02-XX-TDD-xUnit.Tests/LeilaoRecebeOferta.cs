dc9669f4d1e5e33b35144605af6885313a0d3bea
Gabriel Batista <gabriel.texteis@gmail.com>
2020-05-25 00:39:38 -0300

    conclusão do módulo 08 da aula 02 da seção "C#: Testes de unidade e TDD com xUnit"

---
+++ Extras/Testes-Csharp/01-TDD-xUnit/02-XX-TDD-xUnit.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using _01_XX_TDD_xUnit.Core;
+using System.Linq;
+
+namespace _02_XX_TDD_xUnit.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Theory]
+        [InlineData(2, new double[] { 800, 900 })]
+        [InlineData(4, new double[] { 1000, 1200, 1400, 100 })]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(int qtdeEsperada, double[] ofertas)
+        {
+            //Arranje - Cenário de entrada.
+            //Given - Dado leilão com três clientes e lances realizados por eles
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+            leilao.TerminaPregao();
+
+            //Act - Método que está sendo testado
+            //When - Quando o pregão/leilão termina
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert - Seção de verificação
+            //Then - Então o cliente ganhador é o que deu o maior o lance
+            var qtdeObtida = leilao.Lances.Count();
+
+            Assert.Equal(qtdeEsperada, qtdeObtida);
+        }
+    }
+}
