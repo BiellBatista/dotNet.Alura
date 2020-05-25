03125c5997422b366d9d4f548d2a57a6a41a6087
Gabriel Batista <gabriel.texteis@gmail.com>
2020-05-25 00:16:38 -0300

    conclusão do módulo 05 da aula 02 da seção "C#: Testes de unidade e TDD com xUnit"

--- Extras/Testes-Csharp/01-TDD-xUnit/02-XX-TDD-xUnit.Tests/LeilaoTestes.cs
+++ Extras/Testes-Csharp/01-TDD-xUnit/02-XX-TDD-xUnit.Tests/LeilaoTestes.cs
@@ -5,101 +5,125 @@ namespace _02_XX_TDD_xUnit.Tests
 {
     public class LeilaoTestes
     {
-        [Fact] //Annotation diz que este método é um teste
-        public void LeilaoComVariosLances()
+        //[Fact] //Annotation diz que este método é um teste
+        //public void LeilaoComVariosLances()
+        //{
+        //    //Arranje - Cenário de entrada.
+        //    var leilao = new Leilao("Van Gogh");
+        //    var fulano = new Interessada("Fulano", leilao);
+        //    var maria = new Interessada("Maria", leilao);
+
+        //    leilao.RecebeLance(fulano, 800);
+        //    leilao.RecebeLance(maria, 900);
+        //    leilao.RecebeLance(fulano, 1000);
+        //    leilao.RecebeLance(maria, 990);
+
+        //    //Act - Método que está sendo testado
+        //    leilao.TerminaPregao();
+
+        //    //Assert - Seção de verificação
+        //    var valorEsperado = 1000;
+        //    var valorObtido = leilao.Ganhador.Valor;
+
+        //    Assert.Equal(valorEsperado, valorObtido);
+        //}
+
+        [Theory] //Transforma este método em uma classe de equivalência
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })] //Indica os dados que esta classe receberá
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })] //Indica os dados que esta classe receberá
+        [InlineData(800, new double[] { 800 })] //Indica os dados que esta classe receberá
+        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
         {
             //Arranje - Cenário de entrada.
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
 
             //Act - Método que está sendo testado
             leilao.TerminaPregao();
 
             //Assert - Seção de verificação
-            var valorEsperado = 1000;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
         }
 
-        [Fact]
-        public void LeilaoComApenasUmLance()
-        {
-            //Arranje - Cenário de entrada.
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
+        //[Fact]
+        //public void LeilaoComApenasUmLance()
+        //{
+        //    //Arranje - Cenário de entrada.
+        //    var leilao = new Leilao("Van Gogh");
+        //    var fulano = new Interessada("Fulano", leilao);
 
-            leilao.RecebeLance(fulano, 800);
+        //    leilao.RecebeLance(fulano, 800);
 
-            //Act - Método que está sendo testado
-            leilao.TerminaPregao();
+        //    //Act - Método que está sendo testado
+        //    leilao.TerminaPregao();
 
-            //Assert - Seção de verificação
-            var valorEsperado = 800;
-            var valorObtido = leilao.Ganhador.Valor;
+        //    //Assert - Seção de verificação
+        //    var valorEsperado = 800;
+        //    var valorObtido = leilao.Ganhador.Valor;
 
-            Assert.Equal(valorEsperado, valorObtido);
-        }
+        //    Assert.Equal(valorEsperado, valorObtido);
+        //}
 
-        [Fact]
-        public void LeilaoComLancesOrdenadosPorValor()
-        {
-            //Arranje - Cenário de entrada.
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
+        //[Fact]
+        //public void LeilaoComLancesOrdenadosPorValor()
+        //{
+        //    //Arranje - Cenário de entrada.
+        //    var leilao = new Leilao("Van Gogh");
+        //    var fulano = new Interessada("Fulano", leilao);
+        //    var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
+        //    leilao.RecebeLance(fulano, 800);
+        //    leilao.RecebeLance(maria, 900);
+        //    leilao.RecebeLance(maria, 990);
+        //    leilao.RecebeLance(fulano, 1000);
 
-            //Act - Método que está sendo testado
-            leilao.TerminaPregao();
+        //    //Act - Método que está sendo testado
+        //    leilao.TerminaPregao();
 
-            //Assert - Seção de verificação
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
+        //    //Assert - Seção de verificação
+        //    var valorEsperado = 1000;
+        //    var valorObtido = leilao.Ganhador.Valor;
 
-            Assert.Equal(valorEsperado, valorObtido);
-        }
+        //    Assert.Equal(valorEsperado, valorObtido);
+        //}
 
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            //Arranje - Cenário de entrada.
-            //Given - Dado leilão com três clientes e lances realizados por eles
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
+        //[Fact]
+        //public void LeilaoComTresClientes()
+        //{
+        //    //Arranje - Cenário de entrada.
+        //    //Given - Dado leilão com três clientes e lances realizados por eles
+        //    var leilao = new Leilao("Van Gogh");
+        //    var fulano = new Interessada("Fulano", leilao);
+        //    var maria = new Interessada("Maria", leilao);
+        //    var beltrano = new Interessada("Beltrano", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(beltrano, 1400);
+        //    leilao.RecebeLance(fulano, 800);
+        //    leilao.RecebeLance(maria, 900);
+        //    leilao.RecebeLance(fulano, 1000);
+        //    leilao.RecebeLance(maria, 990);
+        //    leilao.RecebeLance(beltrano, 1400);
 
-            //Act - Método que está sendo testado
-            //When - Quando o pregão/leilão termina
-            leilao.TerminaPregao();
+        //    //Act - Método que está sendo testado
+        //    //When - Quando o pregão/leilão termina
+        //    leilao.TerminaPregao();
 
-            //Assert - Seção de verificação
-            //Then - Então o valor esperado é o maior lance dado pelo cliente
-            var valorEsperado = 1400;
-            //Assert - Seção de verificação
-            //Then - Então o cliente ganhador é o que deu o maior o lance
-            var valorObtido = leilao.Ganhador.Valor;
+        //    //Assert - Seção de verificação
+        //    //Then - Então o valor esperado é o maior lance dado pelo cliente
+        //    var valorEsperado = 1400;
+        //    //Assert - Seção de verificação
+        //    //Then - Então o cliente ganhador é o que deu o maior o lance
+        //    var valorObtido = leilao.Ganhador.Valor;
 
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-        }
+        //    Assert.Equal(valorEsperado, valorObtido);
+        //    Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        //}
 
         [Fact]
         public void LeilaoSemLances()
@@ -118,3 +142,8 @@ namespace _02_XX_TDD_xUnit.Tests
         }
     }
 }
+
+/**
+ * Portanto, se queremos passar dados de entrada para orientar os testes através de [InlineData], usamos a anotação [Theory].
+ * Se não importar quais dados são passados e a expectativa permanecer a mesma, usamos [Fact].
+ */
