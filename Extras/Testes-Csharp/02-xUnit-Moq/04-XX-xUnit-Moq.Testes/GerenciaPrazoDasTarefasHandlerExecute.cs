96426668d106366532ef1dbc72091a72a099d4cf
Gabriel Batista <gabriel.texteis@gmail.com>
2020-05-31 23:48:53 -0300

    conclusão do módulo 02 da aula 04 da seção "Mocks em C#: Testes de Integração com xUnit e Moq"

--- Extras/Testes-Csharp/02-xUnit-Moq/04-XX-xUnit-Moq.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
+++ Extras/Testes-Csharp/02-xUnit-Moq/04-XX-xUnit-Moq.Testes/GerenciaPrazoDasTarefasHandlerExecute.cs
@@ -3,6 +3,7 @@ using _04_XX_xUnit_Moq.Core.Models;
 using _04_XX_xUnit_Moq.Infrastructure;
 using _04_XX_xUnit_Moq.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,6 +55,37 @@ namespace _04_XX_xUnit_Moq.Testes
             var tarefasEmAtraso = repo.ObtemTarefas(t => t.Status == StatusTarefa.EmAtraso);
             Assert.Equal(5, tarefasEmAtraso.Count());
         }
+
+        [Fact]
+        public void QuandoInvocadoDeveChamarAtualizarTarefasNaQtDeVezesDoTotalDeTarefasAtrsadas()
+        {
+            //arrange
+            var categ = new Categoria("Dummy");
+            var tarefas = new List<Tarefa>
+            {
+                new Tarefa(1, "Tirar lixo", categ, new DateTime(2018, 12, 31), null, StatusTarefa.Criada),
+                new Tarefa(4, "Fazer o almoço", categ, new DateTime(2017, 12, 1), null, StatusTarefa.Criada),
+                new Tarefa(9, "Ir à academia", categ, new DateTime(2018, 12, 31), null, StatusTarefa.Criada),
+            };
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            //quando "ObtemTarefas" for chamado para qualquer tarefa, retorne um enumerador
+            mock.Setup(r => r.ObtemTarefas(It.IsAny<Func<Tarefa, bool>>()))
+                .Returns(tarefas);
+
+            var repo = mock.Object;
+
+            var comando = new GerenciaPrazoDasTarefas(new DateTime(2019, 1, 1));
+            var handler = new GerenciaPrazoDasTarefasHandler(repo);
+
+            //act
+            handler.Execute(comando);
+
+            //assert
+            // verificando se o método atualizar tarefas foi executado 3 vezes
+            mock.Verify(r => r.AtualizarTarefas(It.IsAny<Tarefa[]>()), Times.Once());
+        }
     }
 }
 
@@ -62,5 +94,7 @@ namespace _04_XX_xUnit_Moq.Testes
  * Dublês para Testes
  * 
  * Dummy Object: são objetos que eu tenho que criar, mas que não são utilizados no assert. Neste teste, tenho como exemplos as categorias.
- * Fake Object: são classes que eu crio/uso para simular um recurso, de forma leve. Por exemplo, o repositório fake e o inMemoryDatabase
+ * Fake Object: são classes que eu crio/uso para simular um recurso, de forma leve. Por exemplo, o repositório fake e o inMemoryDatabase.
+ * Stubs: é um objeto do qual eu preciso fornecer alguma informação de entrada para o teste. Por exemplo, o lançamento de uma exceção, porque foi preciso
+ *  mockar um objeto (simular). Em resumo, ele simula um objeto, do qual é necessário fornecer informação
  */
