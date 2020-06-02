ef3cedf1639945a60258bfe3c0ae4cc5d4af9470
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-02 08:44:28 -0300

    conclusão do módulo 02 da aula 06 da seção "Mocks em C#: Testes de Integração com xUnit e Moq"

---
+++ Extras/Testes-Csharp/02-xUnit-Moq/06-XX-xUnit-Moq.Testes/TarefasControllerEndPointCadastraTarefa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xunit;
+using Moq;
+using _06_XX_xUnit_Moq.WebApp.Controllers;
+using _06_XX_xUnit_Moq.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _06_XX_xUnit_Moq.Services.Handlers;
+using _06_XX_xUnit_Moq.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace _06_XX_xUnit_Moq.Testes
+{
+    public class TarefasControllerEndPointCadastraTarefa
+    {
+        [Fact]
+        public void DadaTarefaComInformacoesValidasDeveRetornar200()
+        {
+            //arrange
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                .UseInMemoryDatabase("DbTarefasContext")
+                .Options;
+            var contexto = new DbTarefasContext(options);
+
+            contexto.Categorias.Add(new Core.Models.Categoria(20, "Estudo"));
+            contexto.SaveChanges();
+
+            var repo = new RepositorioTarefa(contexto);
+
+            var controlador = new TarefasController(repo, mockLogger.Object);
+            var model = new CadastraTarefaVM();
+
+            model.IdCategoria = 20;
+            model.Titulo = "Estudar xUnit";
+            model.Prazo = new DateTime(2020, 12, 31);
+
+            //act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            //assert
+            Assert.IsType<OkResult>(retorno);
+        }
+    }
+}
--- Extras/Testes-Csharp/02-xUnit-Moq/06-XX-xUnit-Moq.WebApp/Controllers/TarefasController.cs
+++ Extras/Testes-Csharp/02-xUnit-Moq/06-XX-xUnit-Moq.WebApp/Controllers/TarefasController.cs
@@ -2,6 +2,8 @@
 using _06_XX_xUnit_Moq.WebApp.Models;
 using _06_XX_xUnit_Moq.Core.Commands;
 using _06_XX_xUnit_Moq.Services.Handlers;
+using _06_XX_xUnit_Moq.Infrastructure;
+using Microsoft.Extensions.Logging;
 
 namespace _06_XX_xUnit_Moq.WebApp.Controllers
 {
@@ -9,19 +11,28 @@ namespace _06_XX_xUnit_Moq.WebApp.Controllers
     [ApiController]
     public class TarefasController : ControllerBase
     {
+        IRepositorioTarefas _repo;
+        ILogger<CadastraTarefaHandler> _logger;
+
+        public TarefasController(IRepositorioTarefas repo, ILogger<CadastraTarefaHandler> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
         [HttpPost]
         public IActionResult EndpointCadastraTarefa(CadastraTarefaVM model)
         {
             var cmdObtemCateg = new ObtemCategoriaPorId(model.IdCategoria);
-            var categoria = new ObtemCategoriaPorIdHandler().Execute(cmdObtemCateg);
+            var categoria = new ObtemCategoriaPorIdHandler(_repo).Execute(cmdObtemCateg);
             if (categoria == null)
             {
                 return NotFound("Categoria não encontrada");
             }
 
             var comando = new CadastraTarefa(model.Titulo, categoria, model.Prazo);
-            //var handler = new CadastraTarefaHandler();
-            //handler.Execute(comando);
+            var handler = new CadastraTarefaHandler(_repo, _logger);
+            handler.Execute(comando);
             return Ok();
         }
     }
