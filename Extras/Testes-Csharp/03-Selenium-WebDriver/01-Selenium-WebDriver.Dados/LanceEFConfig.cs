53db73dd9bea5477c82b4188ea0661056ad5ea18
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-02 23:07:41 -0300

    preparando ambiente para a aula 01 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

---
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/01-Selenium-WebDriver.Dados/LanceEFConfig.cs
@@ -0,0 +1,17 @@
+﻿using _01_Selenium_WebDriver.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _01_Selenium_WebDriver.Dados
+{
+    internal class LanceEFConfig : IEntityTypeConfiguration<Lance>
+    {
+        public void Configure(EntityTypeBuilder<Lance> builder)
+        {
+            builder.HasOne<Leilao>(lance => lance.Leilao)
+                .WithMany(leilao => leilao.Lances);
+            builder.HasOne<Interessada>(lance => lance.Cliente)
+                .WithMany(i => i.Lances);
+        }
+    }
+}
