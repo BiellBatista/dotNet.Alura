55bdc5374a9238cf09e18bd0f3521298258f7a5b
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-04 08:26:40 -0300

    conclusão do módulo 01 da aula 02 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

---
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/02-XX-Selenium-WebDriver.Selenium/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace _02_XX_Selenium_WebDriver.Selenium
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            // act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            // assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+    }
+}
