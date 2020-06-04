bf23081d9cf926e65d90dee96f18a5a1cb79d8ac
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-04 08:35:06 -0300

    conclusão do módulo 03 da aula 02 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

---
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/02-XX-Selenium-WebDriver.Selenium/AoNavegarParaHome.cs
@@ -0,0 +1,36 @@
+﻿using _02_XX_Selenium_WebDriver.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace _02_XX_Selenium_WebDriver.Selenium
+{
+    public class AoNavegarParaHome
+    {
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
+        {
+            // arrange
+            IWebDriver driver = new ChromeDriver(TestHelper.PastaDoExecutavel);
+
+            // act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            // assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarProximosLeiloesNaPagina()
+        {
+            // arrange
+            IWebDriver driver = new ChromeDriver(TestHelper.PastaDoExecutavel);
+
+            // act
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            // assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
