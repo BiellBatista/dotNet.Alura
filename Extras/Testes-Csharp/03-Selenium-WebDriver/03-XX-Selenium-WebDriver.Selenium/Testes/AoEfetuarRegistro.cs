e774d184c2676fc940fd93072f8c64fe0dbc5da5
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-08 07:38:05 -0300

    conclusão do módulo 03 da aula 03 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

--- Extras/Testes-Csharp/03-Selenium-WebDriver/03-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarRegistro.cs
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/03-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarRegistro.cs
@@ -15,16 +15,28 @@ namespace _03_XX_Selenium_WebDriver.Selenium.Testes
         }
 
         [Fact]
-        public void Teste()
+        public void DadoInformacoesValidasDeveIrParaPaginaDeAgradecimento()
         {
             //arrange - dado chrome aberto na página inicial do sistema
             //dados de registros válidos
-            driver.Navigate().GoToUrl("http://localhost:500");
+            driver.Navigate().GoToUrl("http://localhost:5000");
 
+            var inputNome = driver.FindElement(By.Id("Nome"));
+            var inputEmail = driver.FindElement(By.Id("Email"));
+            var inputSenha = driver.FindElement(By.Id("Password"));
+            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
+            var botaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys("Gabriel Batista");
+            inputEmail.SendKeys("gabriel.texteis@gmail.com");
+            inputSenha.SendKeys("123");
+            inputConfirmSenha.SendKeys("123");
 
             //act - efetuo o registro
+            botaoRegistro.Click();
 
             //assert - devo ser redirecionado par auma página de agradecimento
+            Assert.Contains("Obrigado", driver.PageSource);
         }
     }
 }
