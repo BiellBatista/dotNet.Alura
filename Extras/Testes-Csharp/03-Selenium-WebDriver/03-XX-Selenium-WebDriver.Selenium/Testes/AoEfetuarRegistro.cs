d2b5b88c7eee77c295f69ddd12f40bfbe5b445e5
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-08 07:26:19 -0300

    conclusão do módulo 01 da aula 03 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

---
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/03-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarRegistro.cs
@@ -0,0 +1,30 @@
+﻿using _03_XX_Selenium_WebDriver.Selenium.Fixtures;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace _03_XX_Selenium_WebDriver.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarRegistro
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarRegistro(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void Teste()
+        {
+            //arrange - dado chrome aberto na página inicial do sistema
+            //dados de registros válidos
+            driver.Navigate().GoToUrl("http://localhost:500");
+
+
+            //act - efetuo o registro
+
+            //assert - devo ser redirecionado par auma página de agradecimento
+        }
+    }
+}
