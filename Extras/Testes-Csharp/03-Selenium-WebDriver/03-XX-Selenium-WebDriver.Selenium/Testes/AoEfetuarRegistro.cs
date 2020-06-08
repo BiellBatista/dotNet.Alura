bd0fe36ac2a0067964936ad423d5e9a96b47d372
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-08 07:59:02 -0300

    conclusão do módulo 05 da aula 03 da seção "Selenium WebDriver e C# parte 1: Primeiros passos"

--- Extras/Testes-Csharp/03-Selenium-WebDriver/03-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarRegistro.cs
+++ Extras/Testes-Csharp/03-Selenium-WebDriver/03-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarRegistro.cs
@@ -15,7 +15,7 @@ namespace _03_XX_Selenium_WebDriver.Selenium.Testes
         }
 
         [Fact]
-        public void DadoInformacoesValidasDeveIrParaPaginaDeAgradecimento()
+        public void DadoInfoValidasDeveIrParaPaginaDeAgradecimento()
         {
             //arrange - dado chrome aberto na página inicial do sistema
             //dados de registros válidos
@@ -37,6 +37,39 @@ namespace _03_XX_Selenium_WebDriver.Selenium.Testes
 
             //assert - devo ser redirecionado par auma página de agradecimento
             Assert.Contains("Obrigado", driver.PageSource);
+        }
+
+        [Theory]
+        [InlineData("", "gabriel.texteis@gmail.com", "123", "123")]
+        [InlineData("Gabriel Batista", "gabriel", "123", "123")]
+        [InlineData("Gabriel Batista", "gabriel.texteis@gmail.com", "123", "456")]
+        [InlineData("Gabriel Batista", "gabriel.texteis@gmail.com", "123", "")]
+        public void DadoInfoInvalidasDeveContinuarNaHome(
+            string nome,
+            string email,
+            string senha,
+            string confirmSenha)
+        {
+            //arrange - dado chrome aberto na página inicial do sistema
+            //dados de registros válidos
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            var inputNome = driver.FindElement(By.Id("Nome"));
+            var inputEmail = driver.FindElement(By.Id("Email"));
+            var inputSenha = driver.FindElement(By.Id("Password"));
+            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
+            var botaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmSenha.SendKeys(confirmSenha);
+
+            //act - efetuo o registro
+            botaoRegistro.Click();
+
+            //assert - devo ser redirecionado par auma página de agradecimento
+            Assert.Contains("section-registro", driver.PageSource);
         }
     }
 }
