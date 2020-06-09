b6ca111f36ee1fe0083d68546ec03c080f3b7c05
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-09 00:07:27 -0300

    preparando ambiente para a aula 01 da seção "Selenium WebDriver e C# parte 2: Outros recursos"

---
+++ Extras/Testes-Csharp/04-Selenium-WebDriver/01-XX-Selenium-WebDriver.Selenium/Fixtures/TestFixture.cs
@@ -0,0 +1,26 @@
+﻿using _01_XX_Selenium_WebDriver.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_XX_Selenium_WebDriver.Selenium.Fixtures
+{
+    public class TestFixture : IDisposable
+    {
+        public IWebDriver Driver { get; private set; }
+
+        //Setup
+        public TestFixture()
+        {
+            Driver = new ChromeDriver(TestHelper.PastaDoExecutavel);
+        }
+
+        //TearDown
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
