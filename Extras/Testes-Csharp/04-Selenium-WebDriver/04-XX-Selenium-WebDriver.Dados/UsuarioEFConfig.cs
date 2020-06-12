2421105d3b6084cc124b495eb18078ac0861f8b9
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-12 01:59:27 -0300

    preparando ambiente para a aula 04 da seção "Selenium WebDriver e C# parte 2: Outros recursos"

---
+++ Extras/Testes-Csharp/04-Selenium-WebDriver/04-XX-Selenium-WebDriver.Dados/UsuarioEFConfig.cs
@@ -0,0 +1,16 @@
+﻿using _04_XX_Selenium_WebDriver.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _04_XX_Selenium_WebDriver.Dados
+{
+    internal class UsuarioEFConfig : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasData(new { Id = 1, Email = "fulano@example.org", Senha = "123", InteressadaId = 1 });
+            builder.HasData(new { Id = 2, Email = "mariana@example.org", Senha = "456", InteressadaId = 2 });
+            builder.HasData(new { Id = 3, Email = "admin@example.org", Senha = "123" });
+        }
+    }
+}
---
+++ Extras/Testes-Csharp/04-Selenium-WebDriver/04-XX-Selenium-WebDriver.Selenium/Testes/AoFiltrarLeiloes.cs
@@ -0,0 +1,43 @@
+﻿using _04_XX_Selenium_WebDriver.Selenium.Fixtures;
+using _04_XX_Selenium_WebDriver.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace _04_XX_Selenium_WebDriver.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoFiltrarLeiloes
+    {
+        private IWebDriver driver;
+
+        public AoFiltrarLeiloes(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginInteressadaDeveMostrarPainelResultado()
+        {
+            //arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+            var dashboardInteressadaPO = new DashboardInteressadaPO(driver);
+
+            //act
+            dashboardInteressadaPO.PesquisarLeiloes(
+                new List<string> { "Arte", "Coleções" },
+                "",
+                true);
+
+            //assert
+            Assert.Contains("Resultado da pesquisa", driver.PageSource);
+
+        }
+    }
+}
