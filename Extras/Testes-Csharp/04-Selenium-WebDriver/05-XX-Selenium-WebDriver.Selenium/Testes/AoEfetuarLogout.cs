d1842deaf2185c1a718f819e60d5a5291958f9a0
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-13 16:46:52 -0300

    preparando ambiente para a aula 05 da seção "Selenium WebDriver e C# parte 2: Outros recursos"

---
+++ Extras/Testes-Csharp/04-Selenium-WebDriver/05-XX-Selenium-WebDriver.Selenium/Testes/AoEfetuarLogout.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using _05_XX_Selenium_WebDriver.Selenium.Fixtures;
+using _05_XX_Selenium_WebDriver.Selenium.PageObjects;
+
+namespace _05_XX_Selenium_WebDriver.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarLogout(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoLoginValidoDeveIrParaHomeNaoLogada()
+        {
+            //arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Visitar();
+            loginPO.PreencheFormulario("fulano@example.org", "123");
+            loginPO.SubmeteFormulario();
+
+            var dashboardPO = new DashboardInteressadaPO(driver);
+
+            //act - efetuar logout
+            dashboardPO.EfetuarLogout();
+
+            //assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+
+        }
+    }
+}
