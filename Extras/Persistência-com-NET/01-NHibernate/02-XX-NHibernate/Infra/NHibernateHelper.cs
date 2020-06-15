560f1734b35738f557fd1b1ec7e40b887c6ac15f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-15 07:54:31 -0300

    preparando ambiente para a aula 02 da seção "NHibernate: Persistência de dados com C#"

---
+++ Extras/Persistência-com-NET/01-NHibernate/02-XX-NHibernate/Infra/NHibernateHelper.cs
@@ -0,0 +1,25 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System.Reflection;
+
+namespace _02_XX_NHibernate.Infra
+{
+    public class NHibernateHelper
+    {
+        public static Configuration RecuperaConfiguracao()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            return cfg;
+        }
+
+        public static void GeraSchema()
+        {
+            Configuration cfg = RecuperaConfiguracao();
+
+            new SchemaExport(cfg).Create(true, true);
+        }
+    }
+}
---
+++ Extras/Persistência-com-NET/01-NHibernate/02-XX-NHibernate/Program.cs
@@ -0,0 +1,15 @@
+﻿using _02_XX_NHibernate.Infra;
+using System;
+
+namespace _02_XX_NHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NHibernateHelper.GeraSchema();
+
+            Console.Read();
+        }
+    }
+}
