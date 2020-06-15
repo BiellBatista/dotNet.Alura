8c7d0a9c87f1e4e6eeee95044aec5eb65c848b98
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-15 07:44:37 -0300

    conclusão do módulo 01 da aula 01 da seção "NHibernate: Persistência de dados com C#"

---
+++ Extras/Persistência-com-NET/01-NHibernate/01-XX-NHibernate/Program.cs
@@ -0,0 +1,15 @@
+﻿using _01_XX_NHibernate.Infra;
+using System;
+
+namespace _01_XX_NHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NHibernateHelper.GeraSchema();
+
+            Console.Read();
+        }
+    }
+}
