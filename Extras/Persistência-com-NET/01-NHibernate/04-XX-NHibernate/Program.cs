a6b8b1636a6b83e194acc18ad2fc34c642dd49fb
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-17 05:02:27 -0300

    conclusão do módulo 01 da aula 04 da seção "NHibernate: Persistência de dados com C#"

--- Extras/Persistência-com-NET/01-NHibernate/04-XX-NHibernate/Program.cs
+++ Extras/Persistência-com-NET/01-NHibernate/04-XX-NHibernate/Program.cs
@@ -3,6 +3,7 @@ using _04_XX_NHibernate.Entidades;
 using _04_XX_NHibernate.Infra;
 using NHibernate;
 using NHibernate.Linq;
+using NHibernate.Transform;
 using System;
 using System.Collections.Generic;
 
@@ -12,34 +13,37 @@ namespace _04_XX_NHibernate
     {
         static void Main(string[] args)
         {
-            //Categoria umaCategoria = new Categoria();
-            //umaCategoria.Nome = "Uma Categoria";
-
-            //Produto novoProduto = new Produto();
-            //novoProduto.Nome = "Camiseta";
-            //novoProduto.Preco = 10;
-            //novoProduto.Categoria = umaCategoria;
-
-            //ISession session = NHibernateHelper.AbreSession();
-            //ITransaction transacao = session.BeginTransaction();
-
-            //session.Save(umaCategoria);
-            //session.Save(novoProduto);
-            //transacao.Commit();
-            //session.Close();
-
             ISession session = NHibernateHelper.AbreSession();
-            ITransaction transacao = session.BeginTransaction();
 
-            Categoria categoria = session.Load<Categoria>(1);
-            IList<Produto> produtos = categoria.Produtos;
+            //string hql = "from Produto";
+            //string hql = "from Produto p order by p.Nome";
+            // para declarar um parametro na minha consulta, uso o ?. A contagem começa no 0. Este é o método posicional
+            //string hql = "from Produto p where p.Preco > ?";
+            // parametro nomeado
+            //string hql = "from Produto p where p.Preco > :minimo and p.Categoria.Name = :categoria";
+            string hql = "select p.Categoria as Categoria, count(p) as NumeroDePedidos from Produto p group by p.Categoria";
+            IQuery query = session.CreateQuery(hql);
+            //query.SetParameter(0, 10); //falando que o parametro 0 será o 10
+            //query.SetParameter("minimo", 10); //falando que o parametro "minimo" será o 10
+            //query.SetParameter("categoria", "Uma categoria"); //falando que o parametro "categoria" será "Uma categoria"
+            //IList<Produto> produtos = query.List<Produto>();
 
-            transacao.Commit();
+            query.SetResultTransformer(Transformers.AliasToBean<ProdutosPorCategoria>());
+            IList<ProdutosPorCategoria> relatorio = query.List<ProdutosPorCategoria>();
+            //foreach (var produto in produtos)
+            //{
+            //    Console.WriteLine(produto.Nome);
+            //}
+
             session.Close();
-
-            produtos.ForEach(p => Console.WriteLine($"{p.Nome} - {p.Categoria.Nome}"));
 
             Console.Read();
         }
+    }
+
+    public class ProdutosPorCategoria
+    {
+        public Categoria Categoria { get; set; }
+        public long NumeroDePedidos { get; set; }
     }
 }
