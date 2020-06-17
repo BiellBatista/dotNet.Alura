49809992bfe05c62aa7dc7f748c3a21c571f4116
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-17 07:46:55 -0300

    preparando ambiente para a aula 08 da seção "NHibernate: Persistência de dados com C#"

---
+++ Extras/Persistência-com-NET/01-NHibernate/08-XX-NHibernate/Program.cs
@@ -0,0 +1,34 @@
+﻿using _08_XX_NHibernate.DAO;
+using _08_XX_NHibernate.Entidades;
+using _08_XX_NHibernate.Infra;
+using NHibernate;
+using NHibernate.Linq;
+using NHibernate.Transform;
+using System;
+using System.Collections.Generic;
+
+namespace _08_XX_NHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISession session = NHibernateHelper.AbreSession();
+            ISession session2 = NHibernateHelper.AbreSession();
+
+            // cacheado as query para diminuir as idas e vindas do banco de dados
+            //Categoria c = session.Get<Categoria>(1);
+            //Categoria c2 = session2.Get<Categoria>(1);
+
+            //Console.WriteLine(c.Produtos.Count);
+            //Console.WriteLine(c2.Produtos.Count);
+
+            //salvando so resultados de uma query
+            session.CreateQuery("from Usuario").SetCacheable(true).List<Usuario>();
+            session2.CreateQuery("from Usuario").SetCacheable(true).List<Usuario>();
+
+            session.Close();
+            Console.Read();
+        }
+    }
+}
