344db32713d07ec45e854cc2d85ad6cede743088
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-17 07:03:11 -0300

    preparando ambiente para a aula 06 da seção "NHibernate: Persistência de dados com C#"

---
+++ Extras/Persistência-com-NET/01-NHibernate/06-XX-NHibernate/DAO/UsuarioDAO.cs
@@ -0,0 +1,34 @@
+﻿using _06_XX_NHibernate.Entidades;
+using NHibernate;
+
+namespace _06_XX_NHibernate.DAO
+{
+    public class UsuarioDAO
+    {
+        private ISession _session;
+
+        public UsuarioDAO(ISession session)
+        {
+            _session = session;
+        }
+
+        public void Adiciona(Usuario usuario)
+        {
+            ITransaction transacao = _session.BeginTransaction();
+            _session.Save(usuario);
+            transacao.Commit();
+        }
+
+        public void Apagar(Usuario usuario)
+        {
+            ITransaction transacao = _session.BeginTransaction();
+            _session.Delete(usuario);
+            transacao.Commit();
+        }
+
+        public Usuario BuscaPorId(int id)
+        {
+            return _session.Get<Usuario>(id);
+        }
+    }
+}
---
+++ Extras/Persistência-com-NET/01-NHibernate/06-XX-NHibernate/Program.cs
@@ -0,0 +1,36 @@
+﻿using _06_XX_NHibernate.DAO;
+using _06_XX_NHibernate.Entidades;
+using _06_XX_NHibernate.Infra;
+using NHibernate;
+using NHibernate.Linq;
+using NHibernate.Transform;
+using System;
+using System.Collections.Generic;
+
+namespace _06_XX_NHibernate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISession session = NHibernateHelper.AbreSession();
+            // falando para o NHibernate trazer a categoria do produto. Devo fazer isso, porque o NHibernate faz consulta lazzy
+            // ele busca os produtos e depois as categoria, não os dois ao mesmo tempo
+            // devo usar o distict para não vim valores repetidos
+            IQuery query = session.CreateQuery("from Produto p join fetch p.Categoria");
+            IList<Produto> produtos = query.List<Produto>();
+
+            foreach (var produto in produtos)
+            {
+                // sem o join fetch, o NHibernate procuraria a categoria para cada produto
+                // ou seja, para cada produto, ele iria dar um select.. COm o join fetch eu resolvo isso, porque
+                // ele vem com as categorias
+                Console.WriteLine(produto.Nome + " - " + produto.Categoria.Nome);
+            }
+
+            session.Close();
+
+            Console.Read();
+        }
+    }
+}
