8a95b6e959b23839b20966657b07cbd26f1bb24c
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-17 07:18:04 -0300

    conclusão do módulo 01 da aula 06 da seção "NHibernate: Persistência de dados com C#"

---
+++ Extras/Persistência-com-NET/01-NHibernate/06-XX-NHibernate/DAO/ProdutoDAO.cs
@@ -0,0 +1,61 @@
+﻿using _06_XX_NHibernate.Entidades;
+using NHibernate;
+using NHibernate.Criterion;
+using System.Collections.Generic;
+
+namespace _06_XX_NHibernate.DAO
+{
+    public class ProdutoDAO
+    {
+        private ISession _session;
+
+        public ProdutoDAO(ISession session)
+        {
+            _session = session;
+        }
+
+        public void Adiciona(Produto produto)
+        {
+            ITransaction transacao = _session.BeginTransaction();
+            _session.Save(produto);
+            transacao.Commit();
+        }
+
+        public void Apagar(Produto produto)
+        {
+            ITransaction transacao = _session.BeginTransaction();
+            _session.Delete(produto);
+            transacao.Commit();
+        }
+
+        public Produto BuscaPorId(int id)
+        {
+            return _session.Get<Produto>(id);
+        }
+
+        public IList<Produto> BuscaPorNomePrecoMinimoECategoria(string nome, decimal precoMinimo, string nomeCategoria)
+        {
+            // criando um critério para busca, porque alguns parametros podem ser nulos
+            ICriteria criteria = _session.CreateCriteria<Produto>();
+
+            if (!string.IsNullOrEmpty(nome))
+            {
+                criteria.Add(Restrictions.Eq("Nome", nome));
+            }
+
+            if(precoMinimo > 0)
+            {
+                criteria.Add(Restrictions.Ge("Preco", precoMinimo));
+            }
+
+            if (!string.IsNullOrEmpty(nomeCategoria))
+            {
+                // colocando um criterio onde o atributo a ser verificado faz parte de outra tabela (a tabela está ligada com o relacionamento) 
+                ICriteria criteriaCategoria = criteria.CreateCriteria("Categoria");
+                criteriaCategoria.Add(Restrictions.Eq("Nome", nomeCategoria));
+            }
+
+            return criteria.List<Produto>();
+        }
+    }
+}
--- Extras/Persistência-com-NET/01-NHibernate/06-XX-NHibernate/Program.cs
+++ Extras/Persistência-com-NET/01-NHibernate/06-XX-NHibernate/Program.cs
@@ -14,18 +14,13 @@ namespace _06_XX_NHibernate
         static void Main(string[] args)
         {
             ISession session = NHibernateHelper.AbreSession();
-            // falando para o NHibernate trazer a categoria do produto. Devo fazer isso, porque o NHibernate faz consulta lazzy
-            // ele busca os produtos e depois as categoria, não os dois ao mesmo tempo
-            // devo usar o distict para não vim valores repetidos
-            IQuery query = session.CreateQuery("from Produto p join fetch p.Categoria");
-            IList<Produto> produtos = query.List<Produto>();
+
+            ProdutoDAO produtoDAO = new ProdutoDAO(session);
+            IList<Produto> produtos = produtoDAO.BuscaPorNomePrecoMinimoECategoria("", 20, "");
 
             foreach (var produto in produtos)
             {
-                // sem o join fetch, o NHibernate procuraria a categoria para cada produto
-                // ou seja, para cada produto, ele iria dar um select.. COm o join fetch eu resolvo isso, porque
-                // ele vem com as categorias
-                Console.WriteLine(produto.Nome + " - " + produto.Categoria.Nome);
+                Console.WriteLine("Nome: " + produto.Nome + " Preco: " + produto.Preco.ToString() + " Categoria: " + produto.Categoria.Nome);
             }
 
             session.Close();
