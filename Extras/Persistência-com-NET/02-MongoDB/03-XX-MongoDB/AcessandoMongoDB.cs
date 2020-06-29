fa37e386477b8f27e8af9df3f587319c43db2d3f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-29 07:56:38 -0300

    preparando ambiente para a aula 03 da seção ".Net e MongoDB parte 1: Primeiros passos para usar esse famoso banco NoSQL"

---
+++ Extras/Persistência-com-NET/02-MongoDB/03-XX-MongoDB/AcessandoMongoDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace _03_XX_MongoDB
+{
+    class AcessandoMongoDB
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            //criando um documento com um atributo
+            var doc = new BsonDocument
+            {
+                {"Título","Guerra dos Tronos" }
+            };
+            //adicionando novos atributos
+            doc.Add("Autor", "George R R Martin");
+            doc.Add("Ano", 1999);
+            doc.Add("Páginas", 856);
+            //criando um atributo do tipo array
+            var assuntosArray = new BsonArray();
+            assuntosArray.Add("Fantasia");
+            assuntosArray.Add("Ação");
+            //adicionando um novo atributo array
+            doc.Add("Assuntos", assuntosArray);
+
+            Console.WriteLine(doc);
+
+            //acesso ao servidor do MongoDB
+            string stringConexao = "mongodb://localhost:27017";
+            IMongoClient cliente = new MongoClient(stringConexao);
+
+            //acesso ao banco de dados
+            IMongoDatabase bancoDados = cliente.GetDatabase("Biblioteca");
+
+            //acesso a coleção
+            IMongoCollection<BsonDocument> colecao = bancoDados.GetCollection<BsonDocument>("Livros");
+
+            //incluindo documento
+            await colecao.InsertOneAsync(doc);
+
+            Console.WriteLine("Documento Incluído");
+        }
+    }
+}
---
+++ Extras/Persistência-com-NET/02-MongoDB/03-XX-MongoDB/Livro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace _03_XX_MongoDB
+{
+    public class Livro
+    {
+        //falando que o Id, no banco, será do tipo ObjectId
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+        public int Paginas { get; set; }
+        public List<string> Assuntos { get; set; }
+    }
+}
