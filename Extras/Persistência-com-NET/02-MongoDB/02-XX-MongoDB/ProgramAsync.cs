436c67ac661477ce52ef738e8e9a89259b18552c
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-29 07:18:37 -0300

    conclusão do módulo 03 da aula 02 da seção ".Net e MongoDB parte 1: Primeiros passos para usar esse famoso banco NoSQL"

--- Extras/Persistência-com-NET/02-MongoDB/02-XX-MongoDB/ProgramAsync.cs
+++ Extras/Persistência-com-NET/02-MongoDB/02-XX-MongoDB/ProgramAsync.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace _02_XX_MongoDB
 {
-    class Program
+    class ProgramAsync
     {
         static void Main(string[] args)
         {
-            MainSync(args);
+            Task T = MainSync(args);
             Console.WriteLine("Pressione ENTER");
             Console.ReadLine();
         }
 
-        static void MainSync(string[] args)
+        static async Task MainSync(string[] args)
         {
             Console.WriteLine("Esperando 10 segundos ....");
-            System.Threading.Thread.Sleep(10000);
+            await Task.Delay(10000);
             Console.WriteLine("Esperei 10 segundos ....");
         }
     }
