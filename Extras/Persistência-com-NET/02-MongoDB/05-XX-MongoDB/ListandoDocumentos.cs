c5e3dffb3fd1494be6d9fa3c6833c9e6693fd033
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-29 09:54:13 -0300

    preparando ambiente para a aula 05 da seção ".Net e MongoDB parte 1: Primeiros passos para usar esse famoso banco NoSQL"

---
+++ Extras/Persistência-com-NET/02-MongoDB/05-XX-MongoDB/ListandoDocumentos.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace _05_XX_MongoDB
+{
+    class ListandoDocumentos
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            //acesso ao servidor do MongoDB
+            var conexaoBiblioteca = new ConectandoMongoDB();
+            Console.WriteLine("Listando Documentos");
+
+            //buscando documentos, sem critério
+            var listaLivros = await conexaoBiblioteca.Livros.Find(new BsonDocument()).ToListAsync();
+
+            foreach (var livro in listaLivros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine("Fim da Lista");
+        }
+    }
+}
---
+++ Extras/Persistência-com-NET/02-MongoDB/05-XX-MongoDB/ListandoDocumentosFiltroClasse.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace _05_XX_MongoDB
+{
+    class ListandoDocumentosFiltroClasse
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            //acesso ao servidor do MongoDB
+            var conexaoBiblioteca = new ConectandoMongoDB();
+            Console.WriteLine("Listando Documentos com Filtro Complexo");
+
+            var construtor = Builders<Livro>.Filter;
+            //dentro da coleção livros, me retorne os com autor "Machado de Assis"
+            //var condicao = construtor.Eq(x => x.Autor, "Machado de Assis");
+            //dentro da coleção livros, me retorne os com o ano maior ou igual a 1999"
+            //var condicao = construtor.Gte(x => x.Ano, 1999);
+            //dentro da coleção livros, me retorne os com o ano maior ou igual a 1999 e que tenham mais ou igual de 300 páginas"
+            //var condicao = construtor.Gte(x => x.Ano, 1999) & construtor.Gte(x => x.Paginas, 300);
+            //dentro da coleção livros, me retorne de assunto ficção cientifica"
+            var condicao = construtor.AnyEq(x => x.Assuntos, "Ficção Científica");
+
+            //buscando documentos, com critério
+            var listaLivros = await conexaoBiblioteca.Livros.Find(condicao).ToListAsync();
+
+            foreach (var livro in listaLivros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine("Fim da Lista");
+        }
+    }
+}
