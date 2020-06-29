409792e1f2cae4c5455056f7b34d1ead05d811c8
Gabriel Batista <gabriel.texteis@gmail.com>
2020-06-29 09:31:01 -0300

    conclusão do módulo 1 da aula 04 da seção ".Net e MongoDB parte 1: Primeiros passos para usar esse famoso banco NoSQL"

---
+++ Extras/Persistência-com-NET/02-MongoDB/04-XX-MongoDB/ListandoDocumentosFiltroBson.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace _04_XX_MongoDB
+{
+    class ListandoDocumentosFiltroBson
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            //acesso ao servidor do MongoDB
+            var conexaoBiblioteca = new ConectandoMongoDB();
+            Console.WriteLine("Listando Documentos com Filtro");
+
+            var filtro = new BsonDocument { { "Autor", "Machado de Assis" } };
+
+            //buscando documentos, sem critério
+            var listaLivros = await conexaoBiblioteca.Livros.Find(filtro).ToListAsync();
+
+            foreach (var livro in listaLivros)
+            {
+                Console.WriteLine(livro.ToJson<Livro>());
+            }
+
+            Console.WriteLine("Fim da Lista");
+        }
+    }
+}
