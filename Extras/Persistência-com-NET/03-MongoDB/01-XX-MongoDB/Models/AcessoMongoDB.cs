79c67059aa6df8f1c542b4f073e96ca64baa47f9
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-01 07:36:03 -0300

    conclusão do módulo 04 da aula 01 da seção ".Net e MongoDB parte 2: Integre uma webapp com o banco NoSQL"

--- Extras/Persistência-com-NET/03-MongoDB/01-XX-MongoDB/Models/AcessoMongoDB.cs
+++ Extras/Persistência-com-NET/03-MongoDB/01-XX-MongoDB/Models/AcessoMongoDB.cs
@@ -1,10 +1,36 @@
-﻿namespace _01_XX_MongoDB.Models
+﻿using MongoDB.Driver;
+
+namespace _01_XX_MongoDB.Models
 {
     public class AcessoMongoDB
     {
+        public const string STRING_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "Blog";
+        public const string NOME_DA_COLECAO_PUBLICACAO = "Publicacoes";
+        public const string NOME_DA_COLECAO_USUARIO = "Usuarios";
 
-        // XXX TRABALHE AQUI
-        // Crie aqui a clase de conexão do MONGODB.
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _baseDados;
 
+        static AcessoMongoDB()
+        {
+            _client = new MongoClient(STRING_CONEXAO);
+            _baseDados = _client.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Publicacao> Publicacoes
+        {
+            get { return _baseDados.GetCollection<Publicacao>(NOME_DA_COLECAO_PUBLICACAO); }
+        }
+
+        public IMongoCollection<Usuario> Usuarios
+        {
+            get { return _baseDados.GetCollection<Usuario>(NOME_DA_COLECAO_USUARIO); }
+        }
     }
 }
