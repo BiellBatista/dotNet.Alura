85c822fd12d8007e72b698e5d5280aa4231f97e9
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-05 05:09:49 -0300

    conclusão do módulo 09 da aula 04 da seção ".Net e MongoDB parte 2: Integre uma webapp com o banco NoSQL"

---
+++ Extras/Persistência-com-NET/03-MongoDB/04-XX-MongoDB/ConectandoMongoDBGeo.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+
+namespace _04_XX_MongoDB
+{
+    class ConectandoMongoDBGeo
+    {
+        public const string STRING_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "geo";
+        public const string NOME_DA_COLECAO = "airports";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _baseDados;
+
+        static ConectandoMongoDBGeo()
+        {
+            _client = new MongoClient(STRING_CONEXAO);
+            _baseDados = _client.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Aeroporto> Airports
+        {
+            get { return _baseDados.GetCollection<Aeroporto>(NOME_DA_COLECAO); }
+        }
+    }
+}
