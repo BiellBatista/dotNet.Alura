919f6dec09f14fbbcf435aec5697e0412352a272
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-05 05:06:50 -0300

    conclusão do módulo 06 da aula 04 da seção ".Net e MongoDB parte 2: Integre uma webapp com o banco NoSQL"

---
+++ Extras/Persistência-com-NET/03-MongoDB/04-XX-MongoDB/Aeroporto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace _04_XX_MongoDB
+{
+    class Aeroporto
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> Loc { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Code { get; set; }
+    }
+}
