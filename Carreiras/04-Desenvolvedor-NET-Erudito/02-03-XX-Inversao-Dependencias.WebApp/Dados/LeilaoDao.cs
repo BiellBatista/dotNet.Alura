3302e8cc803230ef5d911e08bc2c1fb8579547b5
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-06 09:54:30 -0300

    preparando ambiente para a aula 03 da seção "SOLID com C#: Orientação a Objetos com C# (novo)"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/02-03-XX-Inversao-Dependencias.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using _02_03_XX_Inversao_Dependencias.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02_03_XX_Inversao_Dependencias.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+        public Leilao BuscarLeilaoPorId(int id)
+        {
+            return _context.Leiloes.Find(id);
+        }
+
+        public IEnumerable<Leilao> BuscarTodosLeiloes() => _context.Leiloes.Include(l => l.Categoria);
+
+        public IEnumerable<Categoria> BuscarTodasCategorias() => _context.Categorias;
+
+        public void IncluirLeilao(Leilao obj)
+        {
+            _context.Leiloes.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void AlterarLeilao(Leilao obj)
+        {
+            _context.Leiloes.Update(obj);
+            _context.SaveChanges();
+        }
+
+        public void ExcluirLeilao(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/02-03-XX-Inversao-Dependencias.WebApp/Models/SituacaoLeilao.cs
@@ -0,0 +1,9 @@
+﻿namespace _02_03_XX_Inversao_Dependencias.WebApp.Models
+{
+    public enum SituacaoLeilao
+    {
+        Rascunho,
+        Pregao,
+        Finalizado
+    }
+}
