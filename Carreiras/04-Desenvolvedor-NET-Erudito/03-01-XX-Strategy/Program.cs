b0f625cb05406e47b0f33109546baa46b7978ea4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-08 20:51:51 -0300

    preparando ambiente para a aula 01 da seção "Design Patterns C# I: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/03-01-XX-Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_01_XX_Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
