4fd9c5317be9bea5f5ac168cba6fe52d60aadecf
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-13 15:42:25 -0300

    conclusão do módulo 05 da aula 03 da seção "Design Patterns C# I: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/03-03-XX-Template-Method/IHIT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_03_XX_Template_Method
+{
+    public class IHIT : TemplateDeImpostoCondicional
+    {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+            IList<String> noOrcamento = new List<String>();
+
+            foreach (Item item in orcamento.Itens)
+            {
+                if (noOrcamento.Contains(item.Nome))
+                    return true;
+                else
+                    noOrcamento.Add(item.Nome);
+            }
+
+            return false;
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento) => orcamento.Valor * 0.13 + 100;
+        public override double MinimaTaxacao(Orcamento orcamento) => orcamento.Valor * (0.01 * orcamento.Itens.Count);
+    }
+}
