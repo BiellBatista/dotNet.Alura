0cb6bbf9863d2c0d52c00cb18560f9850f849d8f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-13 09:22:24 -0300

    conclusão do módulo 04 da aula 02 da seção "Design Patterns C# I: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/03-02-XX-Chain-Responsibility/DescontoPorVendaCasada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03_02_XX_Chain_Responsibility
+{
+    public class DescontoPorVendaCasada : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+        public double Desconta(Orcamento orcamento)
+        {
+            if (aconteceuVendaCasadaEm(orcamento)) return orcamento.Valor * 0.05;
+            else return Proximo.Desconta(orcamento);
+        }
+
+        private bool aconteceuVendaCasadaEm(Orcamento orcamento)
+        {
+            return existe("CANETA", orcamento) && existe("LAPIS", orcamento);
+        }
+
+        private bool existe(String nomeDoItem, Orcamento orcamento)
+        {
+            foreach (Item item in orcamento.Itens)
+            {
+                if (item.Nome.Equals(nomeDoItem)) return true;
+            }
+            return false;
+        }
+    }
+}
