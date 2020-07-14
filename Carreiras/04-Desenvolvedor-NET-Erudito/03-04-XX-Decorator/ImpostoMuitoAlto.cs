2164cf7a6d201a366de78db11791c7ed5a309357
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-13 22:48:07 -0300

    conclusão do módulo 04 da aula 04 da seção "Design Patterns C# I: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/03-04-XX-Decorator/ImpostoMuitoAlto.cs
@@ -0,0 +1,14 @@
+﻿namespace _03_04_XX_Decorator
+{
+    public class ImpostoMuitoAlto : Imposto
+    {
+        public ImpostoMuitoAlto(Imposto imposto) : base(imposto) { }
+
+        public ImpostoMuitoAlto() : base() { }
+
+        public override double Calcula(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.2 + CalculoDoOutroImposto(orcamento);
+        }
+    }
+}
