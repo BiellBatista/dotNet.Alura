3e6a67eaea306991c022d76b456599b47041c6a3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-15 12:36:09 -0300

    preparando ambiente para a aula 02 da seção "Design Patterns C# II: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/04-02-XX-Flyweight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_02_XX_Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
