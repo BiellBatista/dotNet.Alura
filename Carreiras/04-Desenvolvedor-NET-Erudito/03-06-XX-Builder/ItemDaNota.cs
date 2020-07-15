4e567f006149355a2e4c6e557c4c4798bff50a37
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-15 08:53:38 -0300

    conclusão do módulo 01 da aula 06 da seção "Design Patterns C# I: Boas práticas de programação"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/03-06-XX-Builder/ItemDaNota.cs
@@ -0,0 +1,14 @@
+﻿namespace _03_06_XX_Builder
+{
+    public class ItemDaNota
+    {
+        public string Descricao { get; private set; }
+        public double Valor { get; private set; }
+
+        public ItemDaNota(string descricao, double valor)
+        {
+            Descricao = descricao;
+            Valor = valor;
+        }
+    }
+}
