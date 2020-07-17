ff024123107b9d1669587522f41497a718f05cf3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-17 08:56:40 -0300

    conclusão do módulo 05 da aula 04 da seção "Design Patterns C# II: Boas práticas de programação"

--- Carreiras/04-Desenvolvedor-NET-Erudito/04-04-XX-Interpreter/Program.cs
+++ Carreiras/04-Desenvolvedor-NET-Erudito/04-04-XX-Interpreter/Program.cs
@@ -18,3 +18,11 @@ namespace _04_04_XX_Interpreter
         }
     }
 }
+
+/**
+ * Você consegue dar algum exemplo de onde o Interpreter pode ser útil?
+ * 
+ * O padrão Interpreter é geralmente útil para interpretar DSLs. É comum que, ao ler a string (como por exemplo 2+3/4), o programa transforme-a em uma estrutura de dados melhor (como as nossas classes Expressao) e aí interprete essa árvore.
+ * 
+ * É realmente um padrão de projeto bem peculiar, e com utilização bem específica.
+ */
