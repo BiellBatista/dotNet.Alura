b4da81e4a1d57dc4ed0f2d949d2d3897d51f5fd9
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-18 19:17:05 -0300

    conclusão do módulo 01 da aula 08 da seção "Design Patterns C# II: Boas práticas de programação"

--- Carreiras/04-Desenvolvedor-NET-Erudito/04-08-XX-Adapter/Program.cs
+++ Carreiras/04-Desenvolvedor-NET-Erudito/04-08-XX-Adapter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _04_08_XX_Adapter.Adapter;
+using System;
 
 namespace _04_08_XX_Adapter
 {
@@ -6,7 +7,32 @@ namespace _04_08_XX_Adapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente();
+            cliente.Nome = "victor";
+            cliente.Endereco = "Rua Vergueiro";
+            cliente.DataDeNascimento = DateTime.Now;
+
+            GeradorDeXml gerador = new GeradorDeXml();
+            String xml = gerador.GeraXml(cliente);
+
+            Console.ReadLine();
         }
     }
 }
+
+/**
+ * Adapter e outros padrões de projeto
+ * Veja que a implementação do Adapter é muito parecida com a implementação do Command e do Strategy, a diferença está realmente no propósito do padrão, o problema que está sendo resolvido.
+ * 
+ * Command é utilizado quando queremos guardar um bloco de código que será executado posteriormente, como feito no exemplo da fila de trabalho implementada anteriormente.
+ * 
+ * No Strategy estamos tentando implementar uma estratégia diferente para resolver um determinado problema que temos no código. Por exemplo, imagine que a aplicação precisa enviar buscas para diversos bancos de dados diferentes, nesse caso, o código que será executado em cada banco de dados será levemente diferente, mas o propósito é o mesmo: Fazer o acesso aos dados.
+ * 
+ * Já no Adapter, nós temos uma biblioteca ou um sistema antigo cuja interface não se encaixa perfeitamente no sistema atual, nesse caso, podemos adaptar essa interface definida pela biblioteca ou sistema legado utilizando uma nova classe dentro de nosso sistema. Esse é um propósito bem diferente do que o dos outros padrões.
+ * 
+ * Adapter e Strategy
+ * A diferença que existe entre os padrões é o propósito. No Adapter estamos adaptando a interface de um sistema antigo para que ela possa se encaixar em um sistema novo. Já no Strategy a ideia é utilizar diferentes estratégias para resolver um dado problema no sistema.
+ * 
+ * Adapter e Command
+ * Como dito no exercício anterior, o Adapter serve para adaptar o código de um sistema legado ou biblioteca para que ele possa ser utilizado no novo sistema. Já o command serve para guardarmos um trecho de código que precisa ser executado posteriormente.
+ */
