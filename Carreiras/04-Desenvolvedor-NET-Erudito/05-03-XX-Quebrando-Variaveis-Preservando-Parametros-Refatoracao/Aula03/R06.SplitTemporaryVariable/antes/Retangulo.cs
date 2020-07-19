54aedb0cb0a63dd59fb326cb60212151e1fac597
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-18 23:21:27 -0300

    conclusão do módulo 01 da aula 03 da seção "C# Refatoração Parte 1: Praticando refatoração de código"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/05-03-XX-Quebrando-Variaveis-Preservando-Parametros-Refatoracao/Aula03/R06.SplitTemporaryVariable/antes/Retangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05_03_XX_Quebrando_Variaveis_Preservando_Parametros_Refatoracao.Aula03.R06.SplitTemporaryVariable.antes
+{
+    class Retangulo
+    {
+        public Retangulo(double altura, double largura)
+        {
+            double temp = 2 * (altura + largura);
+            System.Console.WriteLine($"Perímetro: {temp}");
+
+            temp = altura * largura;
+            System.Console.WriteLine($"Área: {temp}");
+        }
+    }
+}
