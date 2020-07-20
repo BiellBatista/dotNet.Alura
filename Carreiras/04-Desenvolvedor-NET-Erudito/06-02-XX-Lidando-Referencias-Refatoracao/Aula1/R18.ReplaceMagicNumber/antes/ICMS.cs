799e39aba2f2081e19c11e932ad0a8b51f162702
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-20 16:36:14 -0300

    preparando ambiente para a aula 02 da seção "C# Refatoração Parte 2: Praticando refatoração de código"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/06-02-XX-Lidando-Referencias-Refatoracao/Aula1/R18.ReplaceMagicNumber/antes/ICMS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_02_XX_Lidando_Referencias_Refatoracao.R18.ReplaceMagicNumber.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            var valorImposto = ICMS.CalcularValor(1000, "SP");
+        }
+    }
+
+    class ICMS
+    {
+        public static decimal CalcularValor(decimal valorProdutos, string uf)
+        {
+            if (valorProdutos < 0)
+            {
+                throw new ArgumentOutOfRangeException("Valor não pode ser negativo");
+            }
+
+            if (uf == "SP")
+            {
+                return valorProdutos * 0.18m;
+            }
+            return valorProdutos * 0.15m;
+        }
+    }
+}
