819442683d9105db5a976e5f8b045bd02987ae6e
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-22 09:18:25 -0300

    preparando ambiente para a aula 04 da seção "C# Refatoração Parte 2: Praticando refatoração de código"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/06-04-XX-Simplificando-Expressoes-Condicionais/FormCalculadoraDepois.cs
@@ -0,0 +1,46 @@
+﻿using _06_03_XX_Dos_Codigos_Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_03_XX_Dos_Codigos_Classes
+{
+    public partial class FormCalculadoraDepois : Form, ICalculadoraObserver
+    {
+        CalculadoraIMC calculadora;
+
+        public FormCalculadoraDepois()
+        {
+            InitializeComponent();
+
+            calculadora = new CalculadoraIMC();
+            calculadora.Adicionar(this);
+        }
+
+        public void ResultadoIMC(double imc)
+        {
+            txtIMC.Text = $"IMC calculado: {imc:0.00}";
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double.TryParse(txtAltura.Text, out double altura);
+            double.TryParse(txtPeso.Text, out double peso);
+
+            try
+            {
+                calculadora.Calcular(altura, peso);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
