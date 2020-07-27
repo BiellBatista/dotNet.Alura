81bba62c57aba3dbe6010e89f8203aa223c9b010
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-27 13:39:36 -0300

    preparando ambiente para a aula 06 da seção "C# Refatoração Parte 3: Hierarquias de classes"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-06-XX-Aplicando-Heranca-Delegacao-Refatoracao/Aula1/R40.RenameMethod/depois/IPessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace _07_05_XX_Extraindo_Superclasse_Subclasse_Interfaces_Refatoracao.R40.RenameMethod.depois
+{
+    interface IPessoa
+    {
+        decimal GetCreditoDisponivel();
+        string GetNome();
+    }
+}
---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-06-XX-Aplicando-Heranca-Delegacao-Refatoracao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_05_XX_Extraindo_Superclasse_Subclasse_Interfaces_Refatoracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
