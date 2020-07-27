19ca9c090acba2ac075f81661c2329d4c4cc7477
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-27 10:32:28 -0300

    preparando ambiente para a aula 03 da seção "C# Refatoração Parte 3: Hierarquias de classes"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-03-XX-Trabalhando-Melhor-Codigos-Erro-Excecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_03_XX_Trabalhando_Melhor_Codigos_Erro_Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
