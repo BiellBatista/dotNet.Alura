0b3cbf91f7e80aa413620aae69759af9f871d859
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-27 09:07:29 -0300

    preparando ambiente para a aula 02 da seção "C# Refatoração Parte 3: Hierarquias de classes"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-02-XX-Criando-Parametros-Simples-Refatoracao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_02_XX_Criando_Parametros_Simples_Refatoracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
