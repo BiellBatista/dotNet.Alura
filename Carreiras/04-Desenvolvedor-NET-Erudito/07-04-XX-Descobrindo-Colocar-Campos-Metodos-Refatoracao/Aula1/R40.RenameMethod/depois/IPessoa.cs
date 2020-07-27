7590360c3bb93af1bdd16b2facffb98157aade8e
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-27 11:05:17 -0300

    preparando ambiente para a aula 04 da seção "C# Refatoração Parte 3: Hierarquias de classes"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-04-XX-Descobrindo-Colocar-Campos-Metodos-Refatoracao/Aula1/R40.RenameMethod/depois/IPessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace _07_04_XX_Descobrindo_Colocar_Campos_Metodos_Refatoracao.R40.RenameMethod.depois
+{
+    interface IPessoa
+    {
+        decimal GetCreditoDisponivel();
+        string GetNome();
+    }
+}
---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-04-XX-Descobrindo-Colocar-Campos-Metodos-Refatoracao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_04_XX_Descobrindo_Colocar_Campos_Metodos_Refatoracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
