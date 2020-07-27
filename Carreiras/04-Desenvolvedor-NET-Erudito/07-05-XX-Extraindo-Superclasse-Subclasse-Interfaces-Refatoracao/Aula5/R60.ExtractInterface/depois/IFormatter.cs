f54c1ef6c9c603db93956e5fc2143d29830145a1
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-27 13:34:54 -0300

    conclusão do módulo 01 da aula 05 da seção "C# Refatoração Parte 3: Hierarquias de classes"

---
+++ Carreiras/04-Desenvolvedor-NET-Erudito/07-05-XX-Extraindo-Superclasse-Subclasse-Interfaces-Refatoracao/Aula5/R60.ExtractInterface/depois/IFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace _07_05_XX_Extraindo_Superclasse_Subclasse_Interfaces_Refatoracao.R60.ExtractInterface.depois
+{
+    public interface IFormatter
+    {
+        bool CanBeFormatted(string value);
+        string Format(string value);
+        bool IsFormatted(string value);
+        string Unformat(string value);
+    }
+}
