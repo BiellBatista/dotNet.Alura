6c09d6875ddf2c5fd04dcf198ed248c5f8cbd1ef
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 07:41:06 -0300

    conclusão do módulo 01 da aula 01 da seção "C# Brasil: Formate datas, cpf e números nacionais"

--- Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/Brasil-Formate/01-XX-Validacao-Formatacao/Program.cs
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/Brasil-Formate/01-XX-Validacao-Formatacao/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Caelum.Stella.CSharp.Validation;
+using System;
+using System.Diagnostics;
 
 namespace _01_XX_Validacao_Formatacao
 {
@@ -10,6 +8,26 @@ namespace _01_XX_Validacao_Formatacao
     {
         static void Main(string[] args)
         {
+            string cpf1 = "86288366757";
+            string cpf2 = "98745366797";
+            string cpf3 = "22222222222";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF válido: " + cpf);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CPF inválido: " + cpf + " : " + exc.ToString());
+            }
         }
     }
 }
