f08c9b9600c4308424e4ef41f459584fc50132a5
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 08:39:55 -0300

    conclusão do módulo 03 da aula 04 da seção "C# Brasil: Formate datas, cpf e números nacionais"

--- Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/04-XX-Buscando-CEP-Internet/Program.cs
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/04-XX-Buscando-CEP-Internet/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using Caelum.Stella.CSharp.Http;
+using System.Diagnostics;
 using System.Net.Http;
 
 namespace _04_XX_Buscando_CEP_Internet
@@ -8,11 +9,34 @@ namespace _04_XX_Buscando_CEP_Internet
         static void Main(string[] args)
         {
             string cep = "01001000";
-            string url = $"https://viacep.com.br/ws/{cep}/json/";
-
-            string result = new HttpClient().GetStringAsync(url).Result;
+            string result = GetEndereco(cep);
 
             Debug.WriteLine(result);
+
+            // extração do endereço no formato JSON
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Debug.WriteLine(enderecoJson);
+
+            // extração do endereço no formato XML
+            string enderecoXml = viaCEP.GetEnderecoXml(cep);
+            Debug.WriteLine(enderecoXml);
+
+            // obtendo um resultado assíncrono
+            var task = viaCEP.GetEnderecoJsonAsync(cep);
+            Debug.WriteLine(task.Result);
+
+            //pegar somente o logradouro e o bairro
+            var endereco = viaCEP.GetEndereco(cep);
+            Debug.WriteLine(string.Format("Logradouro: , Bairro: "));
+            Debug.WriteLine(string.Format("Logradouro: {0}, Bairro: {1}", endereco.Logradouro, endereco.Bairro));
+        }
+
+        private static string GetEndereco(string cep)
+        {
+            string url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+            return new HttpClient().GetStringAsync(url).Result;
         }
     }
 }
