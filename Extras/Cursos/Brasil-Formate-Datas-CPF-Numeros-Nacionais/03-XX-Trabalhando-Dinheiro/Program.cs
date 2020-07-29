e136f8fe219147ecfd07b0439851051849aba8e8
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 08:24:02 -0300

    conclusão do módulo 01 da aula 03 da seção "C# Brasil: Formate datas, cpf e números nacionais"

--- Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/03-XX-Trabalhando-Dinheiro/Program.cs
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/03-XX-Trabalhando-Dinheiro/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Caelum.Stella.CSharp.Vault;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace _03_XX_Trabalhando_Dinheiro
 {
@@ -10,6 +8,33 @@ namespace _03_XX_Trabalhando_Dinheiro
     {
         static void Main(string[] args)
         {
+            //soma e subtração
+            Money money = 10.00;
+            Debug.WriteLine(money);
+
+            double valor1 = 10.00;
+            double valor2 = 20.00;
+            Money total = valor1 + valor2;
+            Debug.WriteLine(total);
+
+            decimal minuendo = 20m;
+            decimal subtraendo = 15m;
+            Money diferenca = minuendo - subtraendo;
+            Debug.WriteLine(diferenca);
+
+            //formatação de dinheiro
+            Money euro = new Money(Currency.EUR, 1000);
+            Debug.WriteLine(euro);
+
+            Money dolar = new Money(Currency.USD, 1000);
+            Debug.WriteLine(dolar);
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            Debug.WriteLine(dolar);
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+            Money somaMoedasDiferentes = euro + dolar;
+            Debug.WriteLine(somaMoedasDiferentes);
         }
     }
 }
