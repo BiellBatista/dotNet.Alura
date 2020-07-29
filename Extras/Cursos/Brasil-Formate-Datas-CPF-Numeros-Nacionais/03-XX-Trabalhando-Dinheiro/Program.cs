ddcc19c97afb7955c368dfc49ee5767955ebf948
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 08:18:49 -0300

    preparando ambiente para a aula 03 da seção "C# Brasil: Formate datas, cpf e números nacionais"

---
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/03-XX-Trabalhando-Dinheiro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_XX_Trabalhando_Dinheiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
