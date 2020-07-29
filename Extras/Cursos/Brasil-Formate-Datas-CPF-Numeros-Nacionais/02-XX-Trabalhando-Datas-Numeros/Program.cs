0c8698301a8e1e992ce5c8adee4e82b1153a061d
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 08:03:23 -0300

    conclusão do módulo 01 da aula 02 da seção "C# Brasil: Formate datas, cpf e números nacionais"

--- Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/02-XX-Trabalhando-Datas-Numeros/Program.cs
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/02-XX-Trabalhando-Datas-Numeros/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace _02_XX_Trabalhando_Datas_Numeros
 {
@@ -10,6 +8,20 @@ namespace _02_XX_Trabalhando_Datas_Numeros
     {
         static void Main(string[] args)
         {
+            //formatando datas
+            DateTime data = new DateTime(2017, 9, 23);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("d"));
+            Debug.WriteLine(data.ToString("d", new CultureInfo("pt-BR")));
+            Debug.WriteLine(data.ToString("dd/MM"));
+            Debug.WriteLine(data.ToString("dd/MM/yy"));
+
+            //formatando horas
+            data = new DateTime(2017, 9, 23, 13, 14, 15, 987);
+            Debug.WriteLine(data);
+            Debug.WriteLine(data.ToString("HH:mm"));
+            Debug.WriteLine(data.ToString("HH:mm:ss.fff"));
+
         }
     }
 }
