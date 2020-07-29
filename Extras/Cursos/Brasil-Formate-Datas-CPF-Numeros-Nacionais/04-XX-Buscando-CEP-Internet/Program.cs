1e834861fb6540d6d937cc5d6d74190dd29e340a
Gabriel Batista <gabriel.texteis@gmail.com>
2020-07-29 08:26:18 -0300

    preparando ambiente para a aula 04 da seção "C# Brasil: Formate datas, cpf e números nacionais"

---
+++ Extras/Cursos/Brasil-Formate-Datas-CPF-Numeros-Nacionais/04-XX-Buscando-CEP-Internet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_XX_Buscando_CEP_Internet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
