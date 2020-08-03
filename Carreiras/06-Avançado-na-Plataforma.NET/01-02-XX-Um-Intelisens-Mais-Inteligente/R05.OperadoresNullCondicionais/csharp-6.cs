a933a2675eb96f619150c73affa71ace4a489a6b
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-03 07:56:44 -0300

    conclusão do módulo 03 da aula 02 da seção "C# 6: Melhorias em Coleções, Propriedades, Exceções e Strings"

--- Carreiras/06-Avançado-na-Plataforma.NET/01-02-XX-Um-Intelisens-Mais-Inteligente/R05.OperadoresNullCondicionais/csharp-6.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/01-02-XX-Um-Intelisens-Mais-Inteligente/R05.OperadoresNullCondicionais/csharp-6.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 using static System.Console;
 using static System.String;
 using static System.DateTime;
+using System.Collections.ObjectModel;
 
-namespace _01_02_XX_Um_Intelisens_Mais_Inteligentes.R04
+namespace _01_02_XX_Um_Intelisens_Mais_Inteligentes.R05
 {
     class Programa
     {
         public void Main()
         {
-            WriteLine("4. Using Static");
+            WriteLine("5. Operadores Null-Condicionais");
 
             Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12))
             {
@@ -26,6 +27,22 @@ namespace _01_02_XX_Um_Intelisens_Mais_Inteligentes.R04
             WriteLine(aluno.NomeCompleto);
             WriteLine("Idade: {0}", aluno.GetIdade());
             WriteLine(aluno.DadosPessoais);
+
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "Geografia", 8));
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "Matemática", 7));
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "História", 9));
+            ImprimirMelhorNota(aluno);
+
+            Aluno aluno2 = new Aluno("Bart", "Simpson");
+
+            ImprimirMelhorNota(aluno2);
+
+        }
+
+        private static void ImprimirMelhorNota(Aluno aluno)
+        {
+            Console.WriteLine("Melhor nota: {0}",
+                aluno?.MelhorAvaliacao?.Nota);
         }
     }
 
@@ -60,5 +77,30 @@ namespace _01_02_XX_Um_Intelisens_Mais_Inteligentes.R04
             this.DataNascimento = dataNascimento;
         }
 
+        private IList<Avaliacao> avaliacoes = new List<Avaliacao>();
+        public IReadOnlyCollection<Avaliacao> Avaliacoes
+            => new ReadOnlyCollection<Avaliacao>(avaliacoes);
+
+        public void AdicionarAvaliacao(Avaliacao avaliacao)
+        {
+            avaliacoes.Add(avaliacao);
+        }
+
+        public Avaliacao MelhorAvaliacao =>
+            avaliacoes.OrderBy(a => a.Nota).LastOrDefault();
+    }
+
+    class Avaliacao
+    {
+        public Avaliacao(int bimestre, string materia, double nota)
+        {
+            Bimestre = bimestre;
+            Materia = materia;
+            Nota = nota;
+        }
+
+        public int Bimestre { get; }
+        public string Materia { get; }
+        public double Nota { get; }
     }
 }
