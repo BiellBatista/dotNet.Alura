6a3af1d61049b7ea3c66b09bd2d0e8bad47fb0f6
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-03 10:52:57 -0300

    preparando ambiente para a aula 01 da seção "C# 7: Tuplas, parâmetros, condicionais, e estilo de código"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/02-01-XX-Melhorias-Parametros/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _02_01_XX_Melhorias_Parametros
+{
+    public abstract class MenuItem
+    {
+        public abstract void Main();
+    }
+}
