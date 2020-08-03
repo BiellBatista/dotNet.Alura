46a4c60188881412aa6aa4569b3d2c0dc8b7b89e
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-03 07:43:53 -0300

    preparando ambiente para a aula 02 da seção "C# 6: Melhorias em Coleções, Propriedades, Exceções e Strings"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/01-02-XX-Um-Intelisens-Mais-Inteligente/R03.MembrosComSintaxeDeExpressao/csharp-6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01_02_XX_Um_Intelisens_Mais_Inteligentes.R03
+{
+    class Programa
+    {
+        public void Main()
+        {
+            Console.WriteLine("3. Membros Com Corpo De Expressão");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+
+            Console.WriteLine(aluno.NomeCompleto);
+            Console.WriteLine("Idade: {0}", aluno.GetIdade());
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; }
+
+        public string Sobrenome { get; }
+
+        public DateTime DataNascimento { get; } = new DateTime(1990, 1, 1);
+
+        public string NomeCompleto => Nome + " " + Sobrenome;
+
+        public int GetIdade()
+            => (int)(((DateTime.Now - DataNascimento).TotalDays) / 365.242199);
+
+        public Aluno(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento) :
+            this(nome, sobrenome)
+        {
+            this.DataNascimento = dataNascimento;
+        }
+
+    }
+}
