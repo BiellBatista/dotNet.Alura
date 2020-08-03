b10bc231df2a818d8e8a7feccbf60f7dd781cc2b
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-03 08:23:04 -0300

    preparando ambiente para a aula 04 da seção "C# 6: Melhorias em Coleções, Propriedades, Exceções e Strings"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/01-04-XX-Excecoes-Aprimoradas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _01_04_XX_Excecoes_Aprimoradas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menus = new string[] {
+                "1. Propriedades Automáticas Somente-Leitura",
+                "2. Inicializadores De Propriedade Automática",
+                "3. Membros Com Corpo De Expressão"
+            };
+
+            Console.WriteLine("ÍNDICE DE PROGRAMAS");
+            Console.WriteLine("===================");
+
+
+            int programa = 0;
+            string line;
+            do
+            {
+                foreach (var menu in menus)
+                {
+                    Console.WriteLine(menu);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Escolha um programa:");
+
+                line = Console.ReadLine();
+                Int32.TryParse(line, out programa);
+                switch (programa)
+                {
+                    case 1:
+                        new R01.Programa().Main();
+                        break;
+                    case 2:
+                        new R02.Programa().Main();
+                        break;
+                    case 3:
+                        new R03.Programa().Main();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PRESSIONE UMA TECLA PARA CONTINUAR...");
+                Console.ReadKey();
+                Console.Clear();
+            } while (line.Length > 0);
+        }
+    }
+}
---
+++ Carreiras/06-Avançado-na-Plataforma.NET/01-04-XX-Excecoes-Aprimoradas/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01_04_XX_Excecoes_Aprimoradas.R01
+{
+    class Programa
+    {
+        public void Main()
+        {
+            Console.WriteLine("1. Propriedades Automáticas Somente-Leitura");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+
+            //aluno.Nome = "Biff";
+            //aluno.Sobrenome = "Tannen";
+            //Console.WriteLine(aluno.Nome);
+            //Console.WriteLine(aluno.Sobrenome);
+
+            Console.WriteLine(aluno.ToString());
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; }
+
+        public string Sobrenome { get; }
+
+        public DateTime DataNascimento { get; }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+        {
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.DataNascimento = dataNascimento;
+        }
+
+        //public override string ToString()
+        //{
+        //    this.Nome = "novo nome";
+        //    this.Sobrenome = "novo sobrenome";
+        //    return string.Format("{0} {1}", Nome, Sobrenome);
+        //}
+    }
+}
