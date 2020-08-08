4aef7bd55fc365b9760149360a603af4fbeba0c6
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 15:29:00 -0300

    conclusão do módulo 01 da aula 02 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-02-XX-Expression-Variables/ExpressionVariables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03_02_XX_Expression_Variables
+{
+    class AplicacaoWeb
+    {
+        public static string Porta = "8080";
+        //campo expression variables
+        public static bool EstaRodandoEmPortaAlta = int.TryParse(Porta, out int portaComoInt) && portaComoInt > 1024;
+    }
+
+    class ExpressionVariables
+    {
+        public static void TestaExpressionVariables()
+        {
+            var idadeComoTexto = "15";
+
+            if (int.TryParse(idadeComoTexto, out int idade) && idade > 18)
+            {
+                Console.WriteLine("Você pode entrar");
+            }
+        }
+
+        public static bool ValidaIdade(string idadeComoTexto) => int.TryParse(idadeComoTexto, out int idade) && idade > 18;
+    }
+}
