c978f04685a95a2b79b29e55a333f6534e6df875
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 17:03:19 -0300

    conclusão do módulo 01 da aula 05 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-05-XX-Melhorias-Resolucao-Sobrecarga/ResolucaoSobrecarga.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _03_05_XX_Melhorias_Resolucao_Sobrecarga
+{
+    class ResolucaoSobrecarga
+    {
+        public void TestaSObreCarga()
+        {
+            this.EscreveMensagem(null); //chamando o método de instancia (1)
+            ResolucaoSobrecarga.EscreveMensagem(null); //chamando o método static (2)
+        }
+
+        //1 - metodo de instancia
+        public void EscreveMensagem(StringBuilder stringBuilder)
+        {
+            Console.WriteLine(stringBuilder);
+        }
+
+
+        //2 - metodo static
+        public static void EscreveMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
