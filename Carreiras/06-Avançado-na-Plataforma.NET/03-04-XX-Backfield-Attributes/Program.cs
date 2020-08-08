9a29a3fafda32c5e916cba181fe7a1b1ab309748
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 16:32:32 -0300

    preparando ambiente para a aula 04 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-04-XX-Backfield-Attributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_04_XX_Backfield_Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
