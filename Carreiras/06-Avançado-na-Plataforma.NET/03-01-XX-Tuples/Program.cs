583d0eb582c73f7aa4f3421c6f07d772ec0cc762
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 14:05:32 -0300

    conclusão do módulo 03 da aula 01 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

--- Carreiras/06-Avançado-na-Plataforma.NET/03-01-XX-Tuples/Program.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-01-XX-Tuples/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            TesteTuples.TesteTuple();
+            TesteTuples.TestaIgualdade();
         }
     }
 }
--- Carreiras/06-Avançado-na-Plataforma.NET/03-01-XX-Tuples/TesteTuples.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-01-XX-Tuples/TesteTuples.cs
@@ -2,7 +2,7 @@
 {
     static class TesteTuples
     {
-        public static void TesteTuple()
+        public static void TestaIgualdade()
         {
             var origem = ("Avenida Paulista", "900", "São Paulo");
             var destino = ("Avenida Paulista", "300", "São Paulo");
@@ -13,6 +13,30 @@
             //    origem.Item3 == destino.Item3;
 
             //isso foi adicionado no C# 7.3, é equivalente ao de cima
+            var mesmoLugar = origem == destino;
+
+            System.Console.WriteLine(mesmoLugar);
+        }
+
+        public static void TestaIgualdade2()
+        {
+            var origem = (
+                rua: "Avenida Paulista",
+                numero: "900",
+                cidade: "São Paulo"
+                );
+
+            var destino = (
+                numero: "900",
+                rua: "Avenida Paulista",
+                cidade: "São Paulo"
+                );
+
+            /**
+             * Isso será falso, porque o C# não levará em consideração os nomes (rua, numero, cidade) dos itens. Por baixo dos pano ele irá comparar
+             * x.Item1 == y.Item1 (onde o x.item1 é a rua e o y.item1 é o numero)
+             * Devo tomar cuidado ao utilizar esse acuca sintatico
+             */
             var mesmoLugar = origem == destino;
 
             System.Console.WriteLine(mesmoLugar);
