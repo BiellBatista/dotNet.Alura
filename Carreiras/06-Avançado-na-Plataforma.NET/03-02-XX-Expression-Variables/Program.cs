45b9a48277df262ec9de148a640cdfe8c6d75cf6
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 14:53:21 -0300

    preparando ambiente para a aula 02 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-02-XX-Expression-Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_02_XX_Expression_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
