4430a89c2812cecd1cfd28a5175294df98b63066
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-08 16:50:14 -0300

    preparando ambiente para a aula 05 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-05-XX-Melhorias-Resolucao-Sobrecarga/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_05_XX_Melhorias_Resolucao_Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
