3f7f0d0f59148c769aa27b78631f19d76ddb407a
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-09 00:46:56 -0300

    conclusão do módulo 02 da aula 05 da seção "C# 7.3: Igualdade em tuplas, expression variables e refs"

--- Carreiras/06-Avançado-na-Plataforma.NET/03-05-XX-Melhorias-Resolucao-Sobrecarga/ResolucaoSobrecarga.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/03-05-XX-Melhorias-Resolucao-Sobrecarga/ResolucaoSobrecarga.cs
@@ -24,4 +24,22 @@ namespace _03_05_XX_Melhorias_Resolucao_Sobrecarga
             Console.WriteLine(mensagem);
         }
     }
+
+    class MelhoriaDeSobrecarga
+    {
+        void TestaMelhoria()
+        {
+            EscreveMensagem("mensagem", 2);
+        }
+
+        public void EscreveMensagem<T>(T objeto, int numero) where T : IDisposable
+        {
+            Console.WriteLine(objeto);
+        }
+
+        public void EscreveMensagem<T>(T objeto, short numero)
+        {
+            Console.WriteLine(objeto);
+        }
+    }
 }
