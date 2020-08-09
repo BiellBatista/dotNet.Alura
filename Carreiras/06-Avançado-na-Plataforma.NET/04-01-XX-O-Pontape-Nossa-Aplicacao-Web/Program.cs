70b4d8fec05ce04f4ca7cde192560415afb809b4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-09 01:09:13 -0300

    preparando ambiente para a aula 01 da seção "C# Reflection parte 1: Metadados do seu código .NET"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-01-XX-O-Pontape-Nossa-Aplicacao-Web/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_01_XX_O_Pontape_Nossa_Aplicacao_Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
