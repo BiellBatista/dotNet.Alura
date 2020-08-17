655a87c8fa7abfec0525712d0bf490e0adbbc6bc
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-17 09:25:43 -0300

    preparando ambiente para a aula 02 da seção "C# Reflection parte 1: Metadados do seu código .NET"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-02-XX-Usando-Assembly-Dinamicamente-Convencoes/Intraestrutura/WebApplication.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+
+namespace _04_02_XX_Usando_Assembly_Dinamicamente_Convencoes.Intraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+
+        public WebApplication(string[] prefixos)
+        {
+            if (prefixos == null)
+            {
+                throw new ArgumentNullException(nameof(prefixos));
+            }
+
+            _prefixos = prefixos;
+        }
+
+        public void Iniciar()
+        {
+            while (true)
+            {
+                ManipularRequisicao();
+            }
+        }
+
+        private void ManipularRequisicao()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefixo in _prefixos)
+            {
+                httpListener.Prefixes.Add(prefixo);
+            }
+
+            httpListener.Start();
+
+            var contexto = httpListener.GetContext();
+            var requisicao = contexto.Request;
+            var response = contexto.Response;
+
+            var path = requisicao.Url.AbsolutePath;
+
+            if (path == "/Assets/css/styles.css")
+            {
+                //retornando o assembly que está em execução no momento da chamada. O assembly retornado é o que chamou o método
+                var assembly = Assembly.GetExecutingAssembly();
+                var nomeResource = "04-01-XX-O-Pontape-Nossa-Aplicacao-Web.Assets.css.styles.cs";
+                var resourceStream = assembly.GetManifestResourceStream(nomeResource);
+                var bytesResource = new byte[resourceStream.Length];
+
+                resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
+                response.ContentType = "text/css; charset=utf-8";
+                response.StatusCode = 200;
+                response.ContentLength64 = resourceStream.Length;
+
+                response.OutputStream.Write(bytesResource, 0, bytesResource.Length);
+                response.OutputStream.Close();
+
+                httpListener.Stop();
+            }
+            else if (path == "/Assets/js/main.js")
+            {
+                //retornando o assembly que está em execução no momento da chamada. O assembly retornado é o que chamou o método
+                var assembly = Assembly.GetExecutingAssembly();
+                var nomeResource = "04-01-XX-O-Pontape-Nossa-Aplicacao-Web.Assets.js.main.js";
+                var resourceStream = assembly.GetManifestResourceStream(nomeResource);
+                var bytesResource = new byte[resourceStream.Length];
+
+                resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
+                response.ContentType = "application/js; charset=utf-8";
+                response.StatusCode = 200;
+                response.ContentLength64 = resourceStream.Length;
+
+                response.OutputStream.Write(bytesResource, 0, bytesResource.Length);
+                response.OutputStream.Close();
+
+                httpListener.Stop();
+            }
+        }
+    }
+}
---
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-02-XX-Usando-Assembly-Dinamicamente-Convencoes/Program.cs
@@ -0,0 +1,15 @@
+﻿using _04_02_XX_Usando_Assembly_Dinamicamente_Convencoes.Intraestrutura;
+
+namespace _04_02_XX_Usando_Assembly_Dinamicamente_Convencoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefixo = new string[] { "http://localhost:5341/" };
+            var webApplication = new WebApplication(prefixo);
+
+            webApplication.Iniciar();
+        }
+    }
+}
