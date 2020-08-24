d01d386f24462604423b7be7e17b840dfda2443c
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-24 08:33:40 -0300

    conclusão do módulo 05 da aula 03 da seção "C# Reflection parte 1: Metadados do seu código .NET"

--- Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Controller/CambioController.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Controller/CambioController.cs
@@ -1,11 +1,10 @@
-﻿using _04_03_XX_Service;
+﻿using _04_03_XX_Invocando_Metodos_Dinamicamente.Intraestrutura;
+using _04_03_XX_Service;
 using _04_03_XX_Service.Cambio;
-using System.IO;
-using System.Reflection;
 
 namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Controller
 {
-    public class CambioController
+    public class CambioController : ControllerBase
     {
         private ICambioService _cambioService;
 
@@ -17,11 +16,7 @@ namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Controller
         public string MXN()
         {
             var valorFinal = _cambioService.Calcular("MXN", "BRL", 1);
-            var nomeCompletoResource = "_04_03_XX_Invocando_Metodos_Dinamicamente.View.Cambio.MXN.html";
-            var assembly = Assembly.GetExecutingAssembly();
-            var streamRecurso = assembly.GetManifestResourceStream(nomeCompletoResource);
-            var streamLeitura = new StreamReader(streamRecurso);
-            var textoPagina = streamLeitura.ReadToEnd();
+            var textoPagina = View();
             var textoResultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
 
             return textoResultado;
@@ -30,11 +25,7 @@ namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Controller
         public string USD()
         {
             var valorFinal = _cambioService.Calcular("USD", "BRL", 1);
-            var nomeCompletoResource = "_04_03_XX_Invocando_Metodos_Dinamicamente.View.Cambio.USD.html";
-            var assembly = Assembly.GetExecutingAssembly();
-            var streamRecurso = assembly.GetManifestResourceStream(nomeCompletoResource);
-            var streamLeitura = new StreamReader(streamRecurso);
-            var textoPagina = streamLeitura.ReadToEnd();
+            var textoPagina = View();
             var textoResultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
 
             return textoResultado;
---
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Intraestrutura/ControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Intraestrutura
+{
+    public abstract class ControllerBase
+    {
+        //o primeiro método possui o nome do arquivo que chamou este método
+        protected string View([CallerMemberName] string nomeArquivo = default)
+        {
+            var type = GetType();
+            var diretorioName = type.Name.Replace("Controller", "");
+            var nomeCompletoResource = $"_04_03_XX_Invocando_Metodos_Dinamicamente.View.{diretorioName}.{nomeArquivo}.html";
+            var assembly = Assembly.GetExecutingAssembly();
+            var streamRecurso = assembly.GetManifestResourceStream(nomeCompletoResource);
+            var streamLeitura = new StreamReader(streamRecurso);
+            var textoPagina = streamLeitura.ReadToEnd();
+
+            return textoPagina;
+        }
+    }
+}
