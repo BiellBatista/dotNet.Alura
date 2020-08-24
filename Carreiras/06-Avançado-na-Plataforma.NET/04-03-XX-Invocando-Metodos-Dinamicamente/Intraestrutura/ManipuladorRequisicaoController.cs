119cac7882475c3cbda89a21702d7cbe1297e3c3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-24 07:24:13 -0300

    conclusão do módulo 01 da aula 03 da seção "C# Reflection parte 1: Metadados do seu código .NET"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Intraestrutura/ManipuladorRequisicaoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Intraestrutura
+{
+    public class ManipuladorRequisicaoController
+    {
+        public void Manipular(HttpListener response, string path)
+        {
+
+        }
+    }
+}
--- Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Intraestrutura/WebApplication.cs
+++ Carreiras/06-Avançado-na-Plataforma.NET/04-03-XX-Invocando-Metodos-Dinamicamente/Intraestrutura/WebApplication.cs
@@ -47,28 +47,8 @@ namespace _04_03_XX_Invocando_Metodos_Dinamicamente.Intraestrutura
 
             if (Utilidades.EhArquivo(path))
             {
-                //retornando o assembly que está em execução no momento da chamada. O assembly retornado é o que chamou o método
-                var assembly = Assembly.GetExecutingAssembly();
-                var nomeResource = Utilidades.ConverterPathParaNomeAssembly(path);
-                var resourceStream = assembly.GetManifestResourceStream(nomeResource);
-
-                if (resourceStream == null)
-                {
-                    response.StatusCode = 404;
-                    response.OutputStream.Close();
-                }
-                else
-                {
-                    var bytesResource = new byte[resourceStream.Length];
-
-                    resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
-                    response.ContentType = Utilidades.ObterTipoDeConteudo(path);
-                    response.StatusCode = 200;
-                    response.ContentLength64 = resourceStream.Length;
-
-                    response.OutputStream.Write(bytesResource, 0, bytesResource.Length);
-                    response.OutputStream.Close();
-                }
+                var manipulador = new ManipuladorRequisicaoArquivo();
+                manipulador.Manipular(response, path);
             }
             else if (path == "/Cambio/MXN")
             {
