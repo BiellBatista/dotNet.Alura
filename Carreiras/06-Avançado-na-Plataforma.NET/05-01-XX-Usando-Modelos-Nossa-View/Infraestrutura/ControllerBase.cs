6e861109903e51a94f972d4c4b9e01b48ebab586
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-24 23:44:56 -0300

    preparando ambiente para a aula 01 da seção "C# Reflection parte 2: Implementando injeção de dependência do zero"

---
+++ Carreiras/06-Avançado-na-Plataforma.NET/05-01-XX-Usando-Modelos-Nossa-View/Infraestrutura/ControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace _05_01_XX_Usando_Modelos_Nossa_View.Infraestrutura
+{
+    public abstract class ControllerBase
+    {
+        protected string View([CallerMemberName] string nomeArquivo = null)
+        {
+            var type = GetType();
+            var diretorioNome = type.Name.Replace("Controller", "");
+
+            var nomeCompletoResource = $"_05_01_XX_Usando_Modelos_Nossa_View.View.{diretorioNome}.{nomeArquivo}.html";
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var streamRecurso = assembly.GetManifestResourceStream(nomeCompletoResource);
+
+            var streamLeitura = new StreamReader(streamRecurso);
+            var textoPagina = streamLeitura.ReadToEnd();
+
+            return textoPagina;
+        }
+    }
+}
---
+++ Carreiras/06-Avançado-na-Plataforma.NET/05-01-XX-Usando-Modelos-Nossa-View/Program.cs
@@ -0,0 +1,14 @@
+﻿using _05_01_XX_Usando_Modelos_Nossa_View.Infraestrutura;
+
+namespace _05_01_XX_Usando_Modelos_Nossa_View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prefixos = new string[] { "http://localhost:5341/" };
+            var webApplication = new WebApplication(prefixos);
+            webApplication.Iniciar();
+        }
+    }
+}
