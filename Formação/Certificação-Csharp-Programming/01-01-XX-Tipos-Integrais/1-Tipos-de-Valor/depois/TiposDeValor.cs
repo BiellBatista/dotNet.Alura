04083e218c03e5bd0d873ff38cb563690c93baf4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-31 21:13:25 -0300

    conclusão do módulo 05 da aula 01 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-01-XX-Tipos-Integrais/1-Tipos-de-Valor/depois/TiposDeValor.cs
+++ Formação/Certificação-Csharp-Programming/01-01-XX-Tipos-Integrais/1-Tipos-de-Valor/depois/TiposDeValor.cs
@@ -1,10 +1,14 @@
-﻿namespace _01_01_XX_Tipos_Integrais.Depois
+﻿using System;
+
+namespace _01_01_XX_Tipos_Integrais.Depois
 {
     class TiposDeValor : IAulaItem
     {
         public void Executar()
         {
-
+            int idade;
+            idade = 30;
+            Console.WriteLine(idade);
         }
     }
 }
