ae144d43697bf54fbe094d8ee65e44a60a1ba3f4
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-31 23:18:24 -0300

    conclusão do módulo 08 da aula 03 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/12-Objetos/Depois/Objetos.cs
+++ Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/12-Objetos/Depois/Objetos.cs
@@ -8,6 +8,21 @@ namespace _01_03_XX_Criar_Tipos_Referencia.Depois
         {
             int pontuacao = 10;
             Console.WriteLine($"pontuacao: {pontuacao}");
+
+            Console.WriteLine("OBJECT COM VALOR PRIMITIVO");
+            object meuObjeto;
+            meuObjeto = pontuacao;
+            Console.WriteLine($"meuObjeto: {meuObjeto}");
+
+            Console.WriteLine($"meuObjeto.GetType(): {meuObjeto.GetType()}");
+            Console.WriteLine();
+            Console.WriteLine("OBJECT COM REFERÊNCIA DE OBJETO");
+
+            meuObjeto = new Jogador();
+            Jogador classRef;
+            classRef = (Jogador)meuObjeto; //conversão explícita, ou "cast"
+
+            Console.WriteLine($"classRef.Pontuacao: {classRef.Pontuacao}");
         }
     }
 
