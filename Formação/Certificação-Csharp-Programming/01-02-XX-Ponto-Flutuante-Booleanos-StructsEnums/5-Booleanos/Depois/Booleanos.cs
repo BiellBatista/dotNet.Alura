38b35d0100992ff6ac66eb8d0e9eb386fb0008b3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-31 21:50:33 -0300

    conclusão do módulo 04 da aula 02 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-02-XX-Ponto-Flutuante-Booleanos-StructsEnums/5-Booleanos/Depois/Booleanos.cs
+++ Formação/Certificação-Csharp-Programming/01-02-XX-Ponto-Flutuante-Booleanos-StructsEnums/5-Booleanos/Depois/Booleanos.cs
@@ -1,9 +1,30 @@
-﻿namespace _01_02_XX_Ponto_Flutuante_Booleanos_StructsEnums.Depois
+﻿using System;
+
+namespace _01_02_XX_Ponto_Flutuante_Booleanos_StructsEnums.Depois
 {
     class Booleanos : IAulaItem
     {
         public void Executar()
         {
+            //bool possuiSaldo = 1;
+            bool possuiSaldo = true;
+
+            Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+
+            int dias = DateTime.Now.Day;
+            Console.WriteLine($"dias: {dias}");
+
+            //Atribui a diasPar o valor de uma expressão booleana
+            bool diasPar = (dias % 2 == 0);
+
+            if (diasPar)
+            {
+                Console.WriteLine("dias é um número par");
+            }
+            else
+            {
+                Console.WriteLine("dias é um número ímpar");
+            }
         }
     }
 }
