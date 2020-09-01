ec889767d6a8f39f6d58c75859bfd4b808bff0ad
Gabriel Batista <gabriel.texteis@gmail.com>
2020-08-31 23:13:38 -0300

    conclusão do módulo 06 da aula 03 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/11-Delegates/Depois/Delegates.cs
+++ Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/11-Delegates/Depois/Delegates.cs
@@ -10,6 +10,8 @@ namespace _01_03_XX_Criar_Tipos_Referencia.Depois
         }
     }
 
+    delegate double MetodoMultiplicacao(double input);
+
     class Calculadora
     {
         static double Duplicar(double input)
@@ -30,6 +32,29 @@ namespace _01_03_XX_Criar_Tipos_Referencia.Depois
 
             //Executa diretamente o método
             Console.WriteLine($"Triplicar(7.5): {Triplicar(7.5)}");
+
+            //associando o delegate ao método duplicar, pois eles possuem a mesma assinatura (retorno e parametros)
+            MetodoMultiplicacao metodoMultiplicacao = Duplicar;
+            Console.WriteLine(metodoMultiplicacao(7.5));
+
+            metodoMultiplicacao = Triplicar;
+            Console.WriteLine($"Triplicar(7.5): {metodoMultiplicacao(7.5)}");
+
+
+            //delegate como metodo anonimo
+            MetodoMultiplicacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+
+            double quadrado = metodoQuadrado(5);
+            Console.WriteLine("quadrado: {0}", quadrado);
+
+            //delegate com expressao lambda
+            MetodoMultiplicacao metodoCubo = input => input * input * input;
+            double cubo = metodoCubo(4.375);
+
+            Console.WriteLine("cubo: {0}", cubo);
         }
     }
 }
