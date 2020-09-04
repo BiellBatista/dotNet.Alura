f5bf10161c07850fad61a8eca174040d31315c57
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-03 22:48:25 -0300

    conclusão do módulo 09 da aula 03 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/13-Dynamic/Depois/Dinamicos.cs
+++ Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/13-Dynamic/Depois/Dinamicos.cs
@@ -1,10 +1,26 @@
-﻿namespace _01_03_XX_Criar_Tipos_Referencia.Depois
+﻿using System;
+
+namespace _01_03_XX_Criar_Tipos_Referencia.Depois
 {
     class Dinamicos : IAulaItem
     {
         public void Executar()
         {
+            object objeto = 1;
+            //objeto = objeto + 3;
 
+            //com o tipo dynamic, o tipo do objeto (variavel) é resolvido em tempo de execução
+            dynamic dinamico = 1;
+            dinamico = dinamico + 3;
+
+            Console.WriteLine(dinamico);
+
+            //este código dar erro, porque o tipo está definido como int, por causa das linhas de cima
+            dinamico.teste();
+
+            /**
+             * A existência dos membros de um objeto dynamic são verificados somente em tempo de execução.
+             */
         }
     }
 }
