9b17c6f9a1630b38e85219967562c2f37a732a72
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-03 22:55:25 -0300

    conclusão do módulo 11 da aula 03 da seção "Certificação C# Programming parte 1: Criar tipos"

--- Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/14-Strings/Depois/Strings.cs
+++ Formação/Certificação-Csharp-Programming/01-03-XX-Criar-Tipos-Referencia/14-Strings/Depois/Strings.cs
@@ -1,4 +1,6 @@
-﻿namespace _01_03_XX_Criar_Tipos_Referencia.Depois
+﻿using System;
+
+namespace _01_03_XX_Criar_Tipos_Referencia.Depois
 {
     class Strings : IAulaItem
     {
@@ -6,6 +8,15 @@
         {
             string a = "bom dia";
             string b = "b";
+            // Adiciona ao conteúdo de "b"
+            b = b + "om dia";
+            Console.WriteLine($"a == b: {a == b}"); //true
+            Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}"); //false, porque neste caso estamos verificando se as variaveis
+            //estão ocupando o mesmo ponto de referencia da memoria
+            string bomDia = "bom dia";
+            Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+            var caractere = bomDia[4];
+            Console.WriteLine($"caractere.GetType(): {caractere.GetType()}");
         }
     }
 }
