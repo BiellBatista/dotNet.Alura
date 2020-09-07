32b1d4be1ac7124bcf3f8cd2417c46458250fbeb
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-07 19:40:40 -0300

    preparando ambiente para a aula 02 da seção "Certificação C# Programming parte 2: Consumir tipos"

---
+++ Formação/Certificação-Csharp-Programming/02-02-XX-Projetar-Interfaces/02-Unboxing/Antes/Unboxing.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_02_XX_Projetar_Interfaces.Antes
+{
+    class Unboxing : IAulaItem
+    {
+        public void Executar()
+        {
+            int numero = 57;      // tipo de valor
+        }
+    }
+}
---
+++ Formação/Certificação-Csharp-Programming/02-02-XX-Projetar-Interfaces/IAulaItem.cs
@@ -0,0 +1,7 @@
+﻿namespace _02_02_XX_Projetar_Interfaces
+{
+    interface IAulaItem
+    {
+        void Executar();
+    }
+}
