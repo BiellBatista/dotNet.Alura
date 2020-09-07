212eb324ad501beafb61f9a9cb7d794ce2122be3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-07 19:23:13 -0300

    conclusão do módulo 03 da aula 01 da seção "Certificação C# Programming parte 2: Consumir tipos"

--- Formação/Certificação-Csharp-Programming/02-01-XX-Propriedades-Acessadores/01-Boxing/Depois/Boxing.cs
+++ Formação/Certificação-Csharp-Programming/02-01-XX-Propriedades-Acessadores/01-Boxing/Depois/Boxing.cs
@@ -1,10 +1,16 @@
-﻿namespace _02_01_XX_Propriedades_Acessadores.Depois
+﻿using System;
+
+namespace _02_01_XX_Propriedades_Acessadores.Depois
 {
     class Boxing : IAulaItem
     {
         public void Executar()
         {
             int numero = 57;
+
+            object caixa = numero;
+
+            Console.WriteLine(string.Concat("Resposta", numero, true));
         }
     }
 }
