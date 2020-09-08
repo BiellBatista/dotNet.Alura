df613de510fbb02a97287e96b29f166ca75a7c3b
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-07 22:15:42 -0300

    conclusão do módulo 06 da aula 04 da seção "Certificação C# Programming parte 2: Consumir tipos"

--- Formação/Certificação-Csharp-Programming/02-04-XX-Classe-Base/10-Resolução-de-sobrecarga/Depois/ResolucaoSobrecarga.cs
+++ Formação/Certificação-Csharp-Programming/02-04-XX-Classe-Base/10-Resolução-de-sobrecarga/Depois/ResolucaoSobrecarga.cs
@@ -1,4 +1,6 @@
-﻿namespace _02_04_XX_Classe_Base.Depois
+﻿using System;
+
+namespace _02_04_XX_Classe_Base.Depois
 {
     class ResolucaoSobrecarga : IAulaItem
     {
@@ -12,21 +14,31 @@
 
             double double1 = 123.45;
             double double2 = 456.78;
+
+            Console.WriteLine(Somar(int1, int2));
+            Console.WriteLine(Somar(short1, short2));
+            Console.WriteLine(Somar(double1, double2));
         }
 
-        int Somar(int parcela1, int parcela2)
+        //int Somar(int parcela1, int parcela2)
+        //{
+        //    return parcela1 + parcela2;
+        //}
+
+        //short Somar(short parcela1, short parcela2)
+        //{
+        //    return (short)(parcela1 + parcela2);
+        //}
+
+        //object Somar(object parcela1, object parcela2)
+        //{
+        //    return (double)parcela1 + (double)parcela2;
+        //}
+
+        //em tempo de execução, o dynamic é tratado como um object. Com isso, o compilador não consegue diferenciá-los
+        dynamic Somar(dynamic parcela1, dynamic parcela2)
         {
             return parcela1 + parcela2;
-        }
-
-        short Somar(short parcela1, short parcela2)
-        {
-            return (short)(parcela1 + parcela2);
-        }
-
-        object Somar(object parcela1, object parcela2)
-        {
-            return (double)parcela1 + (double)parcela2;
         }
     }
 }
