38675857a9cc49aa19e6b27cb5450fefc4ae361f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-07 21:09:00 -0300

    conclusão do módulo 03 da aula 03 da seção "Certificação C# Programming parte 2: Consumir tipos"

--- Formação/Certificação-Csharp-Programming/02-03-XX-Interfaces-Explicitas/07-Conversões-com-classes-auxiliares/Depois/MetodosAuxiliares.cs
+++ Formação/Certificação-Csharp-Programming/02-03-XX-Interfaces-Explicitas/07-Conversões-com-classes-auxiliares/Depois/MetodosAuxiliares.cs
@@ -1,9 +1,36 @@
-﻿namespace _02_03_XX_Interfaces_Explicitas.Depois
+﻿using System;
+using System.Globalization;
+
+namespace _02_03_XX_Interfaces_Explicitas.Depois
 {
     class MetodosAuxiliares : IAulaItem
     {
         public void Executar()
         {
+            string textoDigitado = "123";
+            int numeroConvertido = int.Parse(textoDigitado);
+            Console.WriteLine(numeroConvertido);
+
+            textoDigitado = "abc";
+            int.TryParse(textoDigitado, out numeroConvertido);
+
+            if (int.TryParse(textoDigitado, out numeroConvertido))
+            {
+                Console.WriteLine(numeroConvertido);
+            }
+            else
+            {
+                Console.WriteLine("texto não é um numero");
+            }
+
+            textoDigitado = "123.45";
+
+            decimal.TryParse(textoDigitado,
+                NumberStyles.Currency, //moeda
+                CultureInfo.CurrentCulture, //pt-BR
+                out decimal valorConvertido
+                );
+            Console.WriteLine(valorConvertido);
         }
     }
 }
