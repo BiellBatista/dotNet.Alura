f9cef0f664867ce15eb01817628a734b5da0c6ba
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-09 08:12:32 -0300

    conclusão do módulo 04 da aula 01 da seção "Certificação C# Programming parte 3: Encapsulamento e Hierarquia de Classes"

--- Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/01-Propriedades-Acessadores/depois/Program.cs
+++ Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/01-Propriedades-Acessadores/depois/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace _03_01_XX_Propriedades_Acessadores.Depois
+﻿using System;
+
+namespace _03_01_XX_Propriedades_Acessadores.Depois
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Salario = 1000;
+            Console.WriteLine(funcionario.Salario);
+
+            funcionario.Salario = -1200;
+        }
+    }
+
+    class Funcionario
+    {
+        decimal salario;
+
+        public decimal Salario //encapsulamento do campo salario
+        {
+            get
+            {
+                return salario;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("O salário não pode ser negativo.");
+                }
+
+                salario = value;
+            }
         }
     }
 }
