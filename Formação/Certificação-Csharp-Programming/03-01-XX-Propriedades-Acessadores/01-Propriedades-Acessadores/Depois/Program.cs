fcdcb3df1ff250ad769fc9682d19a548cbfa2c1f
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-09 08:30:20 -0300

    conclusão do módulo 09 da aula 01 da seção "Certificação C# Programming parte 3: Encapsulamento e Hierarquia de Classes"

--- Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/01-Propriedades-Acessadores/Depois/Program.cs
+++ Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/01-Propriedades-Acessadores/Depois/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace _03_01_XX_Propriedades_Acessadores.Depois
+﻿using System;
+
+namespace _03_01_XX_Propriedades_Acessadores.Depois
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Funcionario funcionario = new Funcionario(1000);
+            Console.WriteLine(funcionario.Salario);
+        }
+    }
+
+    class Funcionario
+    {
+        //aula 4
+        //decimal salario;
+
+        //public decimal Salario //encapsulamento do campo salario
+        //{
+        //    get
+        //    {
+        //        return salario;
+        //    }
+
+        //    set
+        //    {
+        //        if (value < 0)
+        //        {
+        //            throw new ArgumentOutOfRangeException("O salário não pode ser negativo.");
+        //        }
+
+        //        salario = value;
+        //    }
+        //}
+
+        //propriedade autoimplementada
+        //public decimal Salario { get; set; }
+
+        decimal salario;
+
+        public decimal Salario //encapsulamento do campo salario
+        {
+            get
+            {
+                return salario;
+            }
+            //sem o set, a propriedade fica somente leitura
+            //set
+            //{
+            //    if (value < 0)
+            //    {
+            //        throw new ArgumentOutOfRangeException("O salário não pode ser negativo.");
+            //    }
+
+            //    salario = value;
+            //}
+        }
+
+        public Funcionario(decimal salario)
+        {
+            if (salario < 0)
+            {
+                throw new ArgumentOutOfRangeException("O salário não pode ser negativo.");
+            }
+
+            this.salario = salario;
         }
     }
 }
