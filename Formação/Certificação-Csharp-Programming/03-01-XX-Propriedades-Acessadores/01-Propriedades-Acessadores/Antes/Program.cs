13b655894fc5c1396a3644294729de966c843d96
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-09 08:00:37 -0300

    preparando ambiente para a aula 01 da seção "Certificação C# Programming parte 3: Encapsulamento e Hierarquia de Classes"

---
+++ Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/01-Propriedades-Acessadores/Antes/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _03_01_XX_Propriedades_Acessadores.Antes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Formação/Certificação-Csharp-Programming/03-01-XX-Propriedades-Acessadores/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _03_01_XX_Propriedades_Acessadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
