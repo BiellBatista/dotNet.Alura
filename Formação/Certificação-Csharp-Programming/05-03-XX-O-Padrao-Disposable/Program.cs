4fd54d56825e7064834809133d1c23c403e2f5be
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-27 19:21:45 -0300

    conclusão do módulo 01 da aula 03 da seção "Certificação C# Programming parte 5: Strings e Ciclo de Vida de Objetos"

--- Formação/Certificação-Csharp-Programming/05-03-XX-O-Padrao-Disposable/Program.cs
+++ Formação/Certificação-Csharp-Programming/05-03-XX-O-Padrao-Disposable/Program.cs
@@ -1,15 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05_03_XX_O_Padrao_Disposable
 {
     class Program
     {
+        static IList<MenuItem> menuItems;
+
         static void Main(string[] args)
         {
+            IAulaItem itemSelecionado;
+
+            menuItems = GetMenuItems();
+
+            while (true)
+            {
+                ImprimirMenuItems(menuItems);
+                var opcao = Console.ReadLine();
+
+                int.TryParse(opcao, out int valorOpcao);
+
+                if (valorOpcao == 0)
+                {
+                    break;
+                }
+
+                if (valorOpcao > menuItems.Count)
+                {
+                    break;
+                }
+
+                itemSelecionado = Executar(valorOpcao);
+
+                Console.ReadKey();
+            }
         }
+
+        private static IAulaItem Executar(int valorOpcao)
+        {
+            IAulaItem itemSelecionado;
+            MenuItem menuItem = menuItems[valorOpcao - 1];
+            Type tipoClasse = menuItem.TipoClasse;
+            itemSelecionado = (IAulaItem)Activator.CreateInstance(tipoClasse);
+
+            Console.WriteLine();
+
+            string titulo = $"EXECUTANDO: {menuItem.Titulo}";
+            Console.WriteLine(titulo);
+            Console.WriteLine(new string('=', titulo.Length));
+
+            itemSelecionado.Executar();
+            Console.WriteLine();
+            Console.WriteLine("Tecle algo para continuar...");
+
+            return itemSelecionado;
+        }
+
+        private static void ImprimirMenuItems(IList<MenuItem> menuItems)
+        {
+            int i = 1;
+
+            Console.WriteLine("SELECIONE UMA OPÇÃO");
+            Console.WriteLine("===================");
+            Console.WriteLine("0 - Sair");
+
+            foreach (var menuItem in menuItems)
+            {
+                Console.WriteLine((i++).ToString() + " - " + menuItem.Titulo);
+            }
+        }
+
+        private static IList<MenuItem> GetMenuItems()
+        {
+            return new List<MenuItem>
+            {
+                new MenuItem("Gerenciamento de Memória (Tipos de Valor)", typeof(TiposDeValor)),
+                new MenuItem("Gerenciamento de Memória (Tipos de Referência)", typeof(TiposDeReferencia)),
+                new MenuItem("O Padrão Disposable (Tipos de Referência)", typeof(Disposable)),
+                new MenuItem("Concatenação, Escrita e Leitura de Strings (StringBuilder)", typeof(StringBuilder1)),
+                new MenuItem("Concatenação, Escrita e Leitura de Strings (StringReader)", typeof(StringReader1)),
+                new MenuItem("Concatenação, Escrita e Leitura de Strings (StringWriter)", typeof(StringWriter1)),
+                new MenuItem("Pesquisando Strings", typeof(Pesquisa)),
+                new MenuItem("Pesquisando Strings (Enumerador)", typeof(Enumerar)),
+            };
+        }
+    }
+
+    class MenuItem
+    {
+        public MenuItem(string titulo, Type tipoClasse)
+        {
+            Titulo = titulo;
+            TipoClasse = tipoClasse;
+        }
+
+        public string Titulo { get; set; }
+        public Type TipoClasse { get; set; }
     }
 }
