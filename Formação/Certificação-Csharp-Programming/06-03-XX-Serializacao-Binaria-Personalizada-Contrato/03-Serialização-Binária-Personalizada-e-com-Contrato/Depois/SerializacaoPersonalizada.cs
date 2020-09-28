176536d05d752847da1e4e4efd8e1b7ce3bf2a2e
Gabriel Batista <gabriel.texteis@gmail.com>
2020-09-28 10:57:52 -0300

    conclusão do módulo 08 da aula 03 da seção "Certificação C# Programming parte 6: Coleções, XML e JSON"

--- Formação/Certificação-Csharp-Programming/06-03-XX-Serializacao-Binaria-Personalizada-Contrato/03-Serialização-Binária-Personalizada-e-com-Contrato/Depois/SerializacaoPersonalizada.cs
+++ Formação/Certificação-Csharp-Programming/06-03-XX-Serializacao-Binaria-Personalizada-Contrato/03-Serialização-Binária-Personalizada-e-com-Contrato/Depois/SerializacaoPersonalizada.cs
@@ -9,20 +9,20 @@ namespace _06_03_XX_Serializacao_Binaria_Personalizada_Contrato.Depois
     {
         public void Executar()
         {
-            LojaDeFilmes4 loja = ObterDados();
+            LojaDeFilmes5 loja = ObterDados();
 
-            var serializer = new DataContractSerializer(typeof(LojaDeFilmes4));
+            var serializer = new DataContractSerializer(typeof(LojaDeFilmes5));
 
             using (var fileStream = new FileStream("Loja.xml", FileMode.Create, FileAccess.Write))
             {
                 serializer.WriteObject(fileStream, loja);
             }
 
-            LojaDeFilmes4 copiaDaLoja;
+            LojaDeFilmes5 copiaDaLoja;
 
             using (var fileStream = new FileStream("Loja.xml", FileMode.Open, FileAccess.Read))
             {
-                copiaDaLoja = (LojaDeFilmes4)serializer.ReadObject(fileStream);
+                copiaDaLoja = (LojaDeFilmes5)serializer.ReadObject(fileStream);
             }
 
             foreach (var filme in copiaDaLoja.Filmes)
@@ -33,100 +33,100 @@ namespace _06_03_XX_Serializacao_Binaria_Personalizada_Contrato.Depois
             Console.ReadKey();
         }
 
-        private static LojaDeFilmes4 ObterDados()
+        private static LojaDeFilmes5 ObterDados()
         {
-            return new LojaDeFilmes4
+            return new LojaDeFilmes5
             {
-                Diretores = new List<Diretor4>
+                Diretores = new List<Diretor5>
                 {
-                    new Diretor4 { Nome = "James Cameron", NumeroFilmes = 5 },
-                    new Diretor4 { Nome = "Francis Lawrence", NumeroFilmes = 3 },
-                    new Diretor4 { Nome = "Zack Snyder", NumeroFilmes = 6 },
-                    new Diretor4 { Nome = "Joss Whedon", NumeroFilmes = 7 },
-                    new Diretor4 { Nome = "Martin Scorsese", NumeroFilmes = 4 },
-                    new Diretor4 { Nome = "Christopher Nolan", NumeroFilmes = 8 },
-                    new Diretor4 { Nome = "Scott Derrickson", NumeroFilmes = 4 },
-                    new Diretor4 { Nome = "Gareth Edwards", NumeroFilmes = 3 },
-                    new Diretor4 { Nome = "Justin Kurzel", NumeroFilmes = 6 }
+                    new Diretor5 { Nome = "James Cameron", NumeroFilmes = 5 },
+                    new Diretor5 { Nome = "Francis Lawrence", NumeroFilmes = 3 },
+                    new Diretor5 { Nome = "Zack Snyder", NumeroFilmes = 6 },
+                    new Diretor5 { Nome = "Joss Whedon", NumeroFilmes = 7 },
+                    new Diretor5 { Nome = "Martin Scorsese", NumeroFilmes = 5 },
+                    new Diretor5 { Nome = "Christopher Nolan", NumeroFilmes = 8 },
+                    new Diretor5 { Nome = "Scott Derrickson", NumeroFilmes = 5 },
+                    new Diretor5 { Nome = "Gareth Edwards", NumeroFilmes = 3 },
+                    new Diretor5 { Nome = "Justin Kurzel", NumeroFilmes = 6 }
                 },
-                Filmes = new List<Filme4> {
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "James Cameron", NumeroFilmes = 5 },
+                Filmes = new List<Filme5> {
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "James Cameron", NumeroFilmes = 5 },
                         Titulo = "Avatar",
                         Ano = "2009"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Francis Lawrence", NumeroFilmes = 3 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Francis Lawrence", NumeroFilmes = 3 },
                         Titulo = "I Am Legend",
                         Ano = "2007"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Zack Snyder", NumeroFilmes = 6 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Zack Snyder", NumeroFilmes = 6 },
                         Titulo = "300",
                         Ano = "2006"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Joss Whedon", NumeroFilmes = 7 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Joss Whedon", NumeroFilmes = 7 },
                         Titulo = "The Avengers",
                         Ano = "2012"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Martin Scorsese", NumeroFilmes = 4 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Martin Scorsese", NumeroFilmes = 5 },
                         Titulo = "The Wolf of Wall Street",
                         Ano = "2013"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Christopher Nolan", NumeroFilmes = 8 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Christopher Nolan", NumeroFilmes = 8 },
                         Titulo = "Interstellar",
-                        Ano = "2014"
+                        Ano = "2015"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Game of Thrones",
                         Ano = "2011–"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Vikings",
                         Ano = "2013–"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Gotham",
-                        Ano = "2014–"
+                        Ano = "2015–"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Power",
-                        Ano = "2014–"
+                        Ano = "2015–"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Narcos",
                         Ano = "2015–"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Breaking Bad",
                         Ano = "2008–2013"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Scott Derrickson", NumeroFilmes = 4 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Scott Derrickson", NumeroFilmes = 5 },
                         Titulo = "Doctor Strange",
                         Ano = "2016"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Gareth Edwards", NumeroFilmes = 3 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Gareth Edwards", NumeroFilmes = 3 },
                         Titulo = "Rogue One: A Star Wars Story",
                         Ano = "2016"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "Justin Kurzel", NumeroFilmes = 6 },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "Justin Kurzel", NumeroFilmes = 6 },
                         Titulo = "Assassin's Creed",
                         Ano = "2016"
                     },
-                    new Filme4 {
-                        Diretor = new Diretor4 { Nome = "N/A" },
+                    new Filme5 {
+                        Diretor = new Diretor5 { Nome = "N/A" },
                         Titulo = "Luke Cage",
                         Ano = "2016–"
                     }
