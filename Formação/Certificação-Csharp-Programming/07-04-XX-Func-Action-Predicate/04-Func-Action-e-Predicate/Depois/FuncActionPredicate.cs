a9a8a866b3d81e250a6af17874984c26f9d05634
Gabriel Batista <gabriel.texteis@gmail.com>
2020-10-12 14:26:43 -0300

    conclusão do módulo 03 da aula 04 da seção "Certificação C# Programming parte 7: Eventos e LINQ"

--- Formação/Certificação-Csharp-Programming/07-04-XX-Func-Action-Predicate/04-Func-Action-e-Predicate/Depois/FuncActionPredicate.cs
+++ Formação/Certificação-Csharp-Programming/07-04-XX-Func-Action-Predicate/04-Func-Action-e-Predicate/Depois/FuncActionPredicate.cs
@@ -14,6 +14,7 @@ namespace _07_04_XX_Func_Action_Predicate.Depois
             Func<int, int, int> somar = (x, y) => x + y;
             Console.WriteLine(somar(3, 2));
 
+            //um delegado de Action nunca retorna um valor
             Action<string> imprimirMensagem
                 = (mensagem) =>
                 {
@@ -25,6 +26,7 @@ namespace _07_04_XX_Func_Action_Predicate.Depois
             var numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             Console.WriteLine("Números divisíveis por 3:");
+            //o Predicate não precisa de um tipo de retorno, porque ele retorna um valor bool
             Predicate<int> divisivelPor3 = (numero) => numero % 3 == 0;
 
             var divisiveis = Array.FindAll(numeros, divisivelPor3);
@@ -38,3 +40,9 @@ namespace _07_04_XX_Func_Action_Predicate.Depois
         }
     }
 }
+
+/**
+ * O delegado de função Func
+ * O delegado de função booleana Predicate
+ * O delegado de método sem retorno Action
+*/
