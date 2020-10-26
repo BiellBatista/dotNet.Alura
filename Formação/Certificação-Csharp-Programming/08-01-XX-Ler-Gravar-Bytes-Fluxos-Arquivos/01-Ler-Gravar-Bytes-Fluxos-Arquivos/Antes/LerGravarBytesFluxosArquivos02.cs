9f012c30116ea3db77e456edc6ff20d04867d7c3
Gabriel Batista <gabriel.texteis@gmail.com>
2020-10-26 09:14:21 -0300

    conclusão do módulo 06 da aula 01 da seção "Certificação C# Programming parte 8: Entrada e Saída de Dados"

---
+++ Formação/Certificação-Csharp-Programming/08-01-XX-Ler-Gravar-Bytes-Fluxos-Arquivos/01-Ler-Gravar-Bytes-Fluxos-Arquivos/Antes/LerGravarBytesFluxosArquivos02.cs
@@ -0,0 +1,10 @@
+﻿namespace _08_01_XX_Ler_Gravar_Bytes_Fluxos_Arquivos.Antes
+{
+    public class LerGravarBytesFluxosArquivos02 : IAulaItem
+    {
+        public void Executar()
+        {
+            // GRAVANDO UM ARQUIVO
+        }
+    }
+}
