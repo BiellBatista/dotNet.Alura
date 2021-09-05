d1347915e3e2e5c7ea74605c8f6b3a12f856ce42
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-09-05 18:10:37 -0300

    conclusão do módulo 05 da aula 05 da seção "Certificação C# Programming parte 8: Entrada e Saída de Dados"

---
+++ Formação/Certificação-Csharp-Programming/08-05-XX-Acessando-Web-Forma-Assincrona/05-Acessando-Web-Forma-Assincrona/Antes/AcessandoWebFormaAssincrona04.cs
@@ -0,0 +1,17 @@
+﻿namespace _08_05_XX_Acessando_Web_Forma_Assincrona.Antes
+{
+    public class AcessandoWebFormaAssincrona04 : IAulaItem //HttpClient
+    {
+        public void Executar()
+        {
+            //TAREFA:
+            //Conectar-se site da caelum (http://www.caelum.com.br)
+            //de forma ASSÍNCRONA, porém o código precisa rodar em
+            //
+            // - Aplicações Windows (Windows Forms, WPF)
+            // - Aplicações Web (ASP.NET e ASP.NET Core)
+            // - Xamarin (aplicativos de celular / tablet)
+            // - Windows Universal Application Platform
+        }
+    }
+}
