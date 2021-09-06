5d43bf044816a66e676b732f7f22e0a3e5cde6e0
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-09-05 21:48:57 -0300

    conclusão do módulo 01 da aula 07 da seção "Certificação C# Programming parte 8: Entrada e Saída de Dados"

---
+++ Formação/Certificação-Csharp-Programming/08-07-XX-Consumindo-Servicos-Interpretando-Dados/07-Consumindo-Servicos-Interpretando-Dados/Depois/ConsumindoServicosInterpretandoDados01.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace _08_07_XX_Consumindo_Servicos_Interpretando_Dados.Depois
+{
+    public class ConsumindoServicosInterpretandoDados01 : IAulaItem
+    {
+        public void Executar()
+        {
+            //TAREFA:
+            //CONSULTAR OS DADOS DO CEP 04101-300
+            //NO SERVIÇO DA http://viacep.com.br
+            //E EXIBIR SEUS DADOS
+            string cep = "04101300";
+            string url = $"http://viacep.com.br/ws/{cep}/json/";
+
+            using (var cliente = new HttpClient())
+            {
+                var json = cliente.GetStringAsync(url).Result;
+                var endereco = JsonConvert.DeserializeObject<Endereco>(json);
+
+                Console.WriteLine(
+                    $"Logradouro: {endereco.logradouro}" +
+                    $"\nBairro: {endereco.bairro}" +
+                    $"\nMunicípio: {endereco.localidade}" +
+                    $"\nUF: {endereco.uf}" +
+                    $"\nCEP: {endereco.cep}");
+            }
+        }
+
+        private class Endereco
+        {
+            public string cep { get; set; }
+            public string logradouro { get; set; }
+            public string bairro { get; set; }
+            public string localidade { get; set; }
+            public string uf { get; set; }
+        }
+    }
+}
