00dce1b4aa5923cced15b9e00173f7b9823f746b
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-09-12 22:08:57 -0300

    preparando ambiente para a aula 04 da seção "Certificação C# Programming parte 9: Depurar Aplicativos"

---
+++ Formação/Certificação-Csharp-Programming/09-04-XX-Compilacao-Condicional/01-Gerenciando-Assemblies/Depois/Dados/Filme.cs
@@ -0,0 +1,14 @@
+﻿namespace _09_04_XX_Compilacao_Condicional.Depois
+{
+    public class Filme
+    {
+        public string Diretor { get; set; }
+        public string Titulo { get; set; }
+
+        public Filme(string diretor, string titulo)
+        {
+            Diretor = diretor;
+            Titulo = titulo;
+        }
+    }
+}
---
+++ Formação/Certificação-Csharp-Programming/09-04-XX-Compilacao-Condicional/IAulaItem.cs
@@ -0,0 +1,7 @@
+﻿namespace _09_04_XX_Compilacao_Condicional
+{
+    internal interface IAulaItem
+    {
+        void Executar();
+    }
+}
