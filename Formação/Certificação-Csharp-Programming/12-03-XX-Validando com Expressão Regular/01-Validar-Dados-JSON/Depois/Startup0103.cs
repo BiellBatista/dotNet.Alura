e3d80511339223a3ecc87a796e07af96062f062a
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-09-21 01:43:59 -0300

    preparando ambiente para a aula 03 da seção "Certificação C# Programming parte 12: Validar Segurança"

---
+++ Formação/Certificação-Csharp-Programming/12-03-XX-Validando com Expressão Regular/01-Validar-Dados-JSON/Depois/Startup0103.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace _12_03_XX_Validando_com_Expressão_Regular.Depois
+{
+    public class Startup0103 : IAulaItem
+    {
+        public void Executar()
+        {
+            string json = "{" +
+                        "\"Diretor\":\"James Cameron\"," +
+                        "\"Titulo\":\"Titanic\"," +
+                        "\"DuracaoMinutos\":abc" +
+                    "}";
+
+            try
+            {
+                Filme03 filme = JsonConvert.DeserializeObject<Filme03>(json);
+
+                Console.WriteLine("Dados do objeto Filme: ");
+                Console.WriteLine(filme);
+            }
+            catch (JsonReaderException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class Filme03
+    {
+        public string Diretor { get; set; }
+        public string Titulo { get; set; }
+        public int DuracaoMinutos { get; set; }
+
+        public override string ToString()
+        {
+            return Diretor + " - " + Titulo + " - " + DuracaoMinutos.ToString() + " minutos";
+        }
+
+        public Filme03()
+        {
+        }
+
+        public Filme03(string diretor, string titulo, int duracaoMinutos)
+        {
+            Diretor = diretor;
+            Titulo = titulo;
+            DuracaoMinutos = duracaoMinutos;
+        }
+    }
+}
---
+++ Formação/Certificação-Csharp-Programming/12-03-XX-Validando com Expressão Regular/IAulaItem.cs
@@ -0,0 +1,7 @@
+﻿namespace _12_03_XX_Validando_com_Expressão_Regular
+{
+    internal interface IAulaItem
+    {
+        void Executar();
+    }
+}
