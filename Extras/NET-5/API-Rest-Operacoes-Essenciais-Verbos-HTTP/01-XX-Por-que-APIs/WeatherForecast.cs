f1fc835675acadcd4e768dd9cefbd8c8f0fe767a
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 16:36:13 -0300

    preparando ambiente para a aula 01 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

---
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/01-XX-Por-que-APIs/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _01_XX_Por_que_APIs
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
