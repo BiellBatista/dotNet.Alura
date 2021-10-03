fa79a8a7483bcf7f59a517deb986ef1759e1c841
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 20:58:02 -0300

    conclusão do módulo 02 da aula 05 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

--- Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/05-XX-Melhorando-codigo/Controllers/FilmeController.cs
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/05-XX-Melhorando-codigo/Controllers/FilmeController.cs
@@ -1,6 +1,8 @@
 ﻿using _05_XX_Melhorando_codigo.Data;
+using _05_XX_Melhorando_codigo.Data.Dtos;
 using _05_XX_Melhorando_codigo.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,8 +20,16 @@ namespace _05_XX_Melhorando_codigo.Controllers
         }
 
         [HttpPost]
-        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
+            var filme = new Filme
+            {
+                Titulo = filmeDto.Titulo,
+                Genero = filmeDto.Genero,
+                Duracao = filmeDto.Duracao,
+                Diretor = filmeDto.Diretor
+            };
+
             _context.Filmes.Add(filme);
             _context.SaveChanges();
 
@@ -37,22 +47,35 @@ namespace _05_XX_Melhorando_codigo.Controllers
         {
             var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
 
-            if (filme is not null) return Ok(filme);
+            if (filme is not null)
+            {
+                ReadFilmeDto filmeDto = new ReadFilmeDto
+                {
+                    Id = filme.Id,
+                    Titulo = filme.Titulo,
+                    Genero = filme.Genero,
+                    Duracao = filme.Duracao,
+                    Diretor = filme.Diretor,
+                    HoraDaConsulta = DateTime.Now
+                };
+
+                return Ok(filmeDto);
+            }
 
             return NotFound();
         }
 
         [HttpPut("{id}")]
-        public IActionResult AtualizaFilme(int id, [FromBody] Filme filmeNovo)
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
         {
             var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
 
             if (filme is null) return NotFound();
 
-            filme.Titulo = filmeNovo.Titulo;
-            filme.Genero = filmeNovo.Genero;
-            filme.Duracao = filmeNovo.Duracao;
-            filme.Diretor = filmeNovo.Diretor;
+            filme.Titulo = filmeDto.Titulo;
+            filme.Genero = filmeDto.Genero;
+            filme.Duracao = filmeDto.Duracao;
+            filme.Diretor = filmeDto.Diretor;
 
             _context.SaveChanges();
 
