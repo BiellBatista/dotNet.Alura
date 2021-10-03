f844291a476c8ae7cbfd8471590ea6565ff41a1a
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 20:19:11 -0300

    conclusão do módulo 08 da aula 04 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

--- Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/04-XX-Conexao-Banco-API/Controllers/FilmeController.cs
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/04-XX-Conexao-Banco-API/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using _04_XX_Conexao_Banco_API.Models;
+﻿using _04_XX_Conexao_Banco_API.Data;
+using _04_XX_Conexao_Banco_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,28 +10,32 @@ namespace _04_XX_Conexao_Banco_API.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private readonly FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
 
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperaFilmes()
+        public IEnumerable<Filme> RecuperaFilmes()
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorId(int id)
         {
-            var filme = filmes.FirstOrDefault(f => f.Id == id);
+            var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
 
             if (filme is not null) return Ok(filme);
 
