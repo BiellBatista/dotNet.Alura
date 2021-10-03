a606c997e77ac02fbbf330ead3ca998bc9441664
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 18:54:42 -0300

    conclusão da aula 03 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

--- Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/03-XX-Retornando-dados/Controllers/FilmeController.cs
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/03-XX-Retornando-dados/Controllers/FilmeController.cs
@@ -1,6 +1,5 @@
 ﻿using _03_XX_Retornando_dados.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,24 +13,28 @@ namespace _03_XX_Retornando_dados.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
 
-            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
+        public IActionResult RecuperaFilmes()
         {
-            return filmes;
+            return Ok(filmes);
         }
 
         [HttpGet("{id}")]
-        public Filme RecuperaFilmesPorId(int id)
+        public IActionResult RecuperaFilmesPorId(int id)
         {
-            return filmes.FirstOrDefault(f => f.Id == id);
+            var filme = filmes.FirstOrDefault(f => f.Id == id);
+
+            if (filme is not null) return Ok(filme);
+
+            return NotFound();
         }
     }
 }
