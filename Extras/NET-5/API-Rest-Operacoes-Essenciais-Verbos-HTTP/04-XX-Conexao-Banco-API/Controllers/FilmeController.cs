a6da7e2aa3a2c7ac8875e872901d95ae93fee064
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 19:01:42 -0300

    preparando ambiente para a aula 04 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

---
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/04-XX-Conexao-Banco-API/Controllers/FilmeController.cs
@@ -0,0 +1,40 @@
+﻿using _04_XX_Conexao_Banco_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_XX_Conexao_Banco_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(f => f.Id == id);
+
+            if (filme is not null) return Ok(filme);
+
+            return NotFound();
+        }
+    }
+}
