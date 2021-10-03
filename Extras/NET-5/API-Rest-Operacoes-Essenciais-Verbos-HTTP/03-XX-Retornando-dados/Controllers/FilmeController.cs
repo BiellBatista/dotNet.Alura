0599b2f5b0dbe5bb0d93d1694028f3ca18fbafbb
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-03 18:27:23 -0300

    preparando ambiente para a aula 03 da seção "API Rest com .NET 5: Operações essenciais com verbos HTTP"

---
+++ Extras/NET-5/API-Rest-Operacoes-Essenciais-Verbos-HTTP/03-XX-Retornando-dados/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using _03_XX_Retornando_dados.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace _03_XX_Retornando_dados.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
