622b46c0f6b5461b6b1f011907fab0815432b3d2
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-17 20:22:31 -0300

    conclusão da aula 04 da seção ".NET 5 e EF Core: Relacionando entidades"

--- Extras/NET-5/EF-Core-Relacionando-Entidades/04-XX-Relacionamento-N-N/Data/Dtos/Sessao/ReadSessaoDto.cs
+++ Extras/NET-5/EF-Core-Relacionando-Entidades/04-XX-Relacionamento-N-N/Data/Dtos/Sessao/ReadSessaoDto.cs
@@ -1,9 +1,13 @@
-﻿namespace _04_XX_Relacionamento_N_N.Data.Dtos.Sessao
+﻿using System;
+
+namespace _04_XX_Relacionamento_N_N.Data.Dtos.Sessao
 {
     public class ReadSessaoDto
     {
         public int SessaoId { get; set; }
         public Models.Cinema Cinema { get; set; }
         public Models.Filme Filme { get; set; }
+        public DateTime HorarioDeEncerramento { get; set; }
+        public DateTime HorarioDeInicio { get; set; }
     }
 }
--- Extras/NET-5/EF-Core-Relacionando-Entidades/04-XX-Relacionamento-N-N/Profiles/GerenteProfile.cs
+++ Extras/NET-5/EF-Core-Relacionando-Entidades/04-XX-Relacionamento-N-N/Profiles/GerenteProfile.cs
@@ -1,15 +1,26 @@
-﻿using _04_XX_Relacionamento_N_N.Data.Dtos.Sessao;
+﻿using _04_XX_Relacionamento_N_N.Data.Dtos.Gerente;
 using _04_XX_Relacionamento_N_N.Models;
 using AutoMapper;
+using System.Linq;
 
 namespace _04_XX_Relacionamento_N_N.Profiles
 {
-    public class SessaoProfile : Profile
+    public class GerenteProfile : Profile
     {
-        public SessaoProfile()
+        public GerenteProfile()
         {
-            CreateMap<CreateSessaoDto, Sessao>();
-            CreateMap<Sessao, ReadSessaoDto>();
+            CreateMap<CreateGerenteDto, Gerente>();
+            CreateMap<Gerente, ReadGerenteDto>()
+                    .ForMember(gerente => gerente.Cinemas, opts => opts
+                            //para que seja possível fazer isso, o atributo cinemas deve ser um object
+                            .MapFrom(gerente => gerente.Cinemas.Select(
+                                c => new
+                                {
+                                    c.Id,
+                                    c.Nome,
+                                    c.Endereco,
+                                    c.EnderecoId
+                                })));
         }
     }
 }
