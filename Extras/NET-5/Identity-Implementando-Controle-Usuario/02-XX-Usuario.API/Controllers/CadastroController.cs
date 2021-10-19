6147811e746cee5efe52fda676e2d077bf887077
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-18 23:18:11 -0300

    conclusão módulo 04 da aula 02 da seção ".NET 5 e Identity: Implementando controle de usuário"

---
+++ Extras/NET-5/Identity-Implementando-Controle-Usuario/02-XX-Usuario.API/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using _02_XX_Usuario.API.Data.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_XX_Usuario.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Extras/NET-5/Identity-Implementando-Controle-Usuario/02-XX-Usuario.API/Profiles/UsuarioProfile.cs
@@ -0,0 +1,14 @@
+﻿using _02_XX_Usuario.API.Data.Dtos;
+using _02_XX_Usuario.API.Models;
+using AutoMapper;
+
+namespace _02_XX_Usuario.API.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
