067432f6be1c7c42ce69f252f62001065110e51f
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-19 00:17:00 -0300

    conclusão da aula 03 da seção ".NET 5 e Identity: Implementando controle de usuário"

--- Extras/NET-5/Identity-Implementando-Controle-Usuario/03-XX-Usuario.API/Startup.cs
+++ Extras/NET-5/Identity-Implementando-Controle-Usuario/03-XX-Usuario.API/Startup.cs
@@ -37,6 +37,7 @@ namespace _03_XX_Usuario.API
             services.AddScoped<CadastroService, CadastroService>();
             services.AddScoped<LoginService, LoginService>();
             services.AddScoped<TokenService, TokenService>();
+            services.AddScoped<LogoutService, LogoutService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
