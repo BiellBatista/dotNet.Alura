81c5a389b770f936ac4a45c304fb1978d505369c
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-19 23:05:46 -0300

    conclusão do módulo 08 da aula 04 da seção "C# e Big Query: Manipulando dados"

--- Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
+++ Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
@@ -7,7 +7,8 @@ namespace _04_XX_Primeiros_Acessos
     {
         private static void Main(string[] args)
         {
-            CursoBQCSharp001();
+            //CursoBQCSharp001();
+            CursoBQCSharp002();
         }
 
         // Acesso ao projeto do BigQuery
@@ -17,6 +18,22 @@ namespace _04_XX_Primeiros_Acessos
             var cliente = BigQueryClient.Create(projetoId);
 
             Console.WriteLine("Conexão ao projeto " + projetoId + " realizado com sucesso.");
+            Console.ReadLine();
+        }
+
+        // Consultando dados de uma tabela
+        private static void CursoBQCSharp002()
+        {
+            string projetoId = "nome-do-projeto";
+            var cliente = BigQueryClient.Create(projetoId);
+
+            Console.WriteLine("Conexão ao projeto " + projetoId + " realizado com sucesso.");
+
+            string consultaSQL = "SELECT * FROM NOME-DO-PROJETO.CONJUNTO-DE-DADOS.TABELA;";
+
+            cliente.ExecuteQuery(consultaSQL, null);
+
+            Console.WriteLine("Consulta efetuada com sucesso.");
             Console.ReadLine();
         }
     }
