157de118e98e21dcd60113935f0e96f194d3be97
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-19 22:52:51 -0300

    conclusão do módulo 04 da aula 04 da seção "C# e Big Query: Manipulando dados"

--- Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
+++ Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace _04_XX_Primeiros_Acessos
+﻿using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+using Google.Cloud.BigQuery.V2;
+
+namespace _04_XX_Primeiros_Acessos
 {
     public class Program
     {
