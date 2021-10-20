995fcc8eae4032480a5542ced01a9d16455586ce
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-10-19 23:09:32 -0300

    conclusão do módulo 10 da aula 04 da seção "C# e Big Query: Manipulando dados"

--- Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
+++ Extras/Big-Query/Manipulando-Dados/04-XX-Primeiros-Acessos/Program.cs
@@ -8,7 +8,8 @@ namespace _04_XX_Primeiros_Acessos
         private static void Main(string[] args)
         {
             //CursoBQCSharp001();
-            CursoBQCSharp002();
+            //CursoBQCSharp002();
+            CursoBQCSharp003();
         }
 
         // Acesso ao projeto do BigQuery
@@ -29,11 +30,28 @@ namespace _04_XX_Primeiros_Acessos
 
             Console.WriteLine("Conexão ao projeto " + projetoId + " realizado com sucesso.");
 
-            string consultaSQL = "SELECT * FROM NOME-DO-PROJETO.CONJUNTO-DE-DADOS.TABELA;";
+            string consultaSQL = "SELECT * FROM `NOME-DO-PROJETO.CONJUNTO-DE-DADOS.TABELA`;";
 
             cliente.ExecuteQuery(consultaSQL, null);
 
             Console.WriteLine("Consulta efetuada com sucesso.");
+            Console.ReadLine();
+        }
+
+        // Inserido dados em uma tabela
+        private static void CursoBQCSharp003()
+        {
+            string projetoId = "nome-do-projeto";
+            var cliente = BigQueryClient.Create(projetoId);
+
+            Console.WriteLine("Conexão ao projeto " + projetoId + " realizado com sucesso.");
+
+            string comandoSQL = "INSERT INTO `NOME-DO-PROJETO.CONJUNTO-DE-DADOS.TABELA` VALUES ();";
+
+            // o ExecuteQuery serve para executar qualquer comando de SQL
+            cliente.ExecuteQuery(comandoSQL, null);
+
+            Console.WriteLine("Comando efetuado com sucesso.");
             Console.ReadLine();
         }
     }
