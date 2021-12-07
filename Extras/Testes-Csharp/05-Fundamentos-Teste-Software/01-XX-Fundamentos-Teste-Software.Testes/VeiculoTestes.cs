b452c2f4d35d73925004ab7686fc82c84c9fcb6c
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-12-07 01:06:24 -0300

    conclusão da aula 01 da seção "Testes em .NET: Fundamentos do Teste de Software"

--- Extras/Testes-Csharp/05-Fundamentos-Teste-Software/01-XX-Fundamentos-Teste-Software.Testes/VeiculoTestes.cs
+++ Extras/Testes-Csharp/05-Fundamentos-Teste-Software/01-XX-Fundamentos-Teste-Software.Testes/VeiculoTestes.cs
@@ -30,5 +30,17 @@ namespace _01_XX_Fundamentos_Teste_Software.Testes
             //Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+
+            //Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
     }
 }
