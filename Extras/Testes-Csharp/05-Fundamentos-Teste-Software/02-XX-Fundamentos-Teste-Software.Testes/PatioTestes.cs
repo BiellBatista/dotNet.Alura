d9eb88ccafa6406ae221d01e93f9a4f37f2534d6
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-12-27 12:31:58 -0300

    conclusão do módulo 02 da aula 02 da seção "Testes em .NET: Fundamentos do Teste de Software"

---
+++ Extras/Testes-Csharp/05-Fundamentos-Teste-Software/02-XX-Fundamentos-Teste-Software.Testes/PatioTestes.cs
@@ -0,0 +1,31 @@
+﻿using _02_XX_Fundamentos_Teste_Software.Modelos;
+using Xunit;
+
+namespace _02_XX_Fundamentos_Teste_Software.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = "Gabriel Batista";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "ASD-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
