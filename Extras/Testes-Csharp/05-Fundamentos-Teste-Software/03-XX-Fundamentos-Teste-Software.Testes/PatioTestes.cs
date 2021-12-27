1366687e5afcf3f4a6ced404a5bd8b0c88530e85
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-12-27 13:10:39 -0300

    peparando ambiente para a aula 03 da seção "Testes em .NET: Fundamentos do Teste de Software"

---
+++ Extras/Testes-Csharp/05-Fundamentos-Teste-Software/03-XX-Fundamentos-Teste-Software.Testes/PatioTestes.cs
@@ -0,0 +1,58 @@
+﻿using _03_XX_Fundamentos_Teste_Software.Modelos;
+using Xunit;
+
+namespace _03_XX_Fundamentos_Teste_Software.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = "Gabriel Batista";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "ASD-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+
+        [Theory]
+        [InlineData("Gabriel Batista", "ASD-9999", "Preto", "Gol")]
+        [InlineData("Gabriel Almeida", "EFG-0000", "Prata", "Golf")]
+        [InlineData("Letícia Baraldi", "CVF-4567", "Vermelho", "Honda Civic")]
+        [InlineData("Jose Silva", "QDC-4215", "Branco", "HB20")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = proprietario;
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Placa = placa;
+            veiculo.Modelo = modelo;
+            veiculo.Cor = cor;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
---
+++ Extras/Testes-Csharp/05-Fundamentos-Teste-Software/03-XX-Fundamentos-Teste-Software.Testes/VeiculoTestes.cs
@@ -0,0 +1,70 @@
+using _03_XX_Fundamentos_Teste_Software.Modelos;
+using Xunit;
+
+namespace _03_XX_Fundamentos_Teste_Software.Testes
+{
+    public class VeiculoTestes
+    {
+        //alterando o nome que ficará visível no gerenciador de testes
+        [Fact(DisplayName = "Teste nº1")]
+        [Trait("Funcionalidade", "Acelerar")]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact(DisplayName = "Teste nº2")]
+        [Trait("Funcionalidade", "Frear")]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Frear(10);
+
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact(DisplayName = "Teste nº3")]
+        public void TestaTipoVeiculo()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+
+            //Assert
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
+
+        //ignorando o teste de unidade
+        [Fact(DisplayName = "Teste nº4", Skip = "Teste ainda não implementado. Ignorar")]
+        public void ValidaNomeProprietario()
+        {
+        }
+
+        [Theory]
+        [ClassData(typeof(Veiculo))]
+        public void TestaVeiculoClass(Veiculo modelo)
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+            modelo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(modelo.VelocidadeAtual, veiculo.VelocidadeAtual);
+        }
+    }
+}
