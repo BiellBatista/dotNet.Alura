23c4766306809fd3c933ce2f7c325d30c43e77af
Gabriel Batista <gabriel.batista@xpi.com.br>
2021-12-27 12:47:36 -0300

    conclusão do módulo 03 da aula 02 da seção "Testes em .NET: Fundamentos do Teste de Software"

--- Extras/Testes-Csharp/05-Fundamentos-Teste-Software/02-XX-Fundamentos-Teste-Software.Testes/PatioTestes.cs
+++ Extras/Testes-Csharp/05-Fundamentos-Teste-Software/02-XX-Fundamentos-Teste-Software.Testes/PatioTestes.cs
@@ -27,5 +27,32 @@ namespace _02_XX_Fundamentos_Teste_Software.Testes
             //Assert
             Assert.Equal(2, faturamento);
         }
+
+        [Theory]
+        [InlineData("Gabriel Batista", "ASD-9999", "Preto", "Gol")]
+        [InlineData("Gabriel Almeida", "EFG-0000", "Prata", "Golf")]
+        [InlineData("Letícia Baraldi", "CVF-4567", "Vermelho", "Honda Civic")]
+        [InlineData("Jose Silva", "QDC-4215", "Branco", "HB20")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = proprietario;
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Placa = placa;
+            veiculo.Modelo = modelo;
+            veiculo.Cor = cor;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            var faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
     }
 }
