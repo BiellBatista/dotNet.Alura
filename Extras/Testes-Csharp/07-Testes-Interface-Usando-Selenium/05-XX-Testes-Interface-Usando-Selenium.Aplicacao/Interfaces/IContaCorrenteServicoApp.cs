ea6595c03da6f50f50714530eaa2422cae281922
Gabriel Batista <gabriel.batista@xpi.com.br>
2022-03-02 02:25:58 -0300

    peparando ambiente para a aula 05 da seção "Testes em .NET: Testes de interface usando Selenium"

---
+++ Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/05-XX-Testes-Interface-Usando-Selenium.Aplicacao/Interfaces/IContaCorrenteServicoApp.cs
@@ -0,0 +1,21 @@
+﻿using _05_XX_Testes_Interface_Usando_Selenium.Aplicacao.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace _05_XX_Testes_Interface_Usando_Selenium.Aplicacao.Interfaces
+{
+    public interface IContaCorrenteServicoApp : IDisposable
+    {
+        public List<ContaCorrenteDTO> ObterTodos();
+
+        public ContaCorrenteDTO ObterPorId(int id);
+
+        public ContaCorrenteDTO ObterPorGuid(Guid guid);
+
+        public bool Adicionar(ContaCorrenteDTO cliente);
+
+        public bool Atualizar(int id, ContaCorrenteDTO cliente);
+
+        public bool Excluir(int id);
+    }
+}
---
+++ Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/05-XX-Testes-Interface-Usando-Selenium.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _05_XX_Testes_Interface_Usando_Selenium.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
