01f41d5c80139bf77a6e584d4fa645052442316f
Gabriel Batista <gabriel.batista@xpi.com.br>
2022-03-02 02:41:05 -0300

    conclusão do módulo 02 da aula 05 da seção "Testes em .NET: Testes de interface usando Selenium"

--- Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/05-XX-Testes-Interface-Usando-Selenium.WebApp.Testes/AposRealizarLogin.cs
+++ Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/05-XX-Testes-Interface-Usando-Selenium.WebApp.Testes/AposRealizarLogin.cs
@@ -1,24 +1,19 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
+﻿using _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes.PageObjects;
+using _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes.Util;
+using OpenQA.Selenium;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes
 {
-    public class AposRealizarLogin
+    public class AposRealizarLogin : IClassFixture<Fixture>
     {
         private IWebDriver driver;
         public ITestOutputHelper SaidaConsoleTeste;
 
-        public AposRealizarLogin(ITestOutputHelper _saidaConsoleTeste)
+        public AposRealizarLogin(Fixture fixture, ITestOutputHelper _saidaConsoleTeste)
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(
-            Assembly.GetExecutingAssembly().Location));
-
+            driver = fixture.Driver;
             SaidaConsoleTeste = _saidaConsoleTeste;
         }
 
@@ -26,17 +21,27 @@ namespace _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes
         public void AposRealizarLoginVerificaSeExisteOpcaoAgenciaMenu()
         {
             //Arrange
+            //driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            //var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
+            //var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
+            //var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
 
-            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
-            var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
-            var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
-            var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
+            //login.SendKeys("andre@email.com");
+            //senha.SendKeys("senha01");
 
-            login.SendKeys("andre@email.com");
-            senha.SendKeys("senha01");
+            ////act - Faz o login
+            //btnLogar.Click();
 
-            //act - Faz o login
-            btnLogar.Click();
+            ////Assert
+            //Assert.Contains("Agência", driver.PageSource);
+
+            //Arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Navegar("https://localhost:44309/UsuarioApps/Login");
+
+            //Act
+            loginPO.PreencherCampos("andre@email.com", "senha01");
+            loginPO.Logar();
 
             //Assert
             Assert.Contains("Agência", driver.PageSource);
@@ -46,16 +51,28 @@ namespace _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes
         public void TentaRealizarLoginSemPreencherCampos()
         {
             //Arrange
-            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
-            var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
-            var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
-            var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
+            //driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            //var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
+            //var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
+            //var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
 
-            //login.SendKeys("andre@email.com");
-            //senha.SendKeys("senha01");
+            ////login.SendKeys("andre@email.com");
+            ////senha.SendKeys("senha01");
 
-            //act - Faz o login
-            btnLogar.Click();
+            ////act - Faz o login
+            //btnLogar.Click();
+
+            ////Assert
+            //Assert.Contains("The Email field is required.", driver.PageSource);
+            //Assert.Contains("The Senha field is required.", driver.PageSource);
+
+            //Arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Navegar("https://localhost:44309/UsuarioApps/Login");
+
+            //Act
+            loginPO.PreencherCampos("", "");
+            loginPO.Logar();
 
             //Assert
             Assert.Contains("The Email field is required.", driver.PageSource);
@@ -66,83 +83,105 @@ namespace _05_XX_Testes_Interface_Usando_Selenium.WebApp.Testes
         public void TentaRealizarLoginComSenhaInvalida()
         {
             //Arrange
-            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
-            var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
-            var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
-            var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
+            //driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            //var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
+            //var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
+            //var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
 
-            login.SendKeys("andre@email.com");
-            senha.SendKeys("senha010");//senha inválida.
+            //login.SendKeys("andre@email.com");
+            //senha.SendKeys("senha010");//senha inválida.
 
-            //act - Faz o login
-            btnLogar.Click();
+            ////act - Faz o login
+            //btnLogar.Click();
+
+            ////Assert
+            //Assert.Contains("Login", driver.PageSource);
+
+            //Arrange
+            var loginPO = new LoginPO(driver);
+            loginPO.Navegar("https://localhost:44309/UsuarioApps/Login");
+
+            //Act
+            loginPO.PreencherCampos("andre@email.com", "senha01x");
+            loginPO.Logar();
 
             //Assert
             Assert.Contains("Login", driver.PageSource);
         }
 
         [Fact]
-        public void RealizarLoginAcessaMenuECadastraCliente()
+        public void AposRealizarLoginAcessaMenuAgencia()
         {
+            ////Arrange
+            //driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            //var login = driver.FindElement(By.Id("Email"));//Selecionar elementos do HTML
+            //var senha = driver.FindElement(By.Id("Senha"));//Selecionar elementos do HTML
+            //var btnLogar = driver.FindElement(By.Id("btn-logar"));//Selecionar elementos do HTML
+
+            //login.SendKeys("andre@email.com");
+            //senha.SendKeys("senha01");
+            //btnLogar.Click();
+
+            //var linkMenu = driver.FindElement(By.Id("agencia"));
+
+            ////act - Clicar no Menu
+            //linkMenu.Click();
+
+            ////Assert
+            //Assert.Contains("Adicionar Agência", driver.PageSource);
+
             //Arrange
-            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
-
-            var login = driver.FindElement(By.Name("Email"));
-            var senha = driver.FindElement(By.Name("Senha"));
-
-            login.SendKeys("andre@email.com");
-            senha.SendKeys("senha01");
-
-            driver.FindElement(By.Id("btn-logar")).Click();
-
-            driver.FindElement(By.LinkText("Cliente")).Click();
-
-            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
-
-            driver.FindElement(By.Name("Identificador")).Click();
-            driver.FindElement(By.Name("Identificador")).
-                SendKeys("2df71922-ca7d-4d43-b142-0767b32f822a");
-            driver.FindElement(By.Name("CPF")).Click();
-            driver.FindElement(By.Name("CPF")).SendKeys("69981034096");
-            driver.FindElement(By.Name("Nome")).Click();
-            driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
-            driver.FindElement(By.Name("Profissao")).Click();
-            driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
+            var loginPO = new LoginPO(driver);
+            loginPO.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             //Act
-            driver.FindElement(By.CssSelector(".btn-primary")).Click();
-            driver.FindElement(By.LinkText("Home")).Click();
+            loginPO.PreencherCampos("andre@email.com", "senha01x");
+            loginPO.Logar();
 
             //Assert
-            Assert.Contains("Logout", driver.PageSource);
+            Assert.Contains("Adicionar Agência", driver.PageSource);
         }
 
         [Fact]
         public void RealizarLoginAcessaListagemDeContas()
         {
+            ////Arrange
+            //driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            //   //Login
+            //var login = driver.FindElement(By.Name("Email"));
+            //var senha = driver.FindElement(By.Name("Senha"));
+            //login.SendKeys("andre@email.com");
+            //senha.SendKeys("senha01");
+            //driver.FindElement(By.CssSelector(".btn")).Click();
+
+            ////Conta Corrente
+            //driver.FindElement(By.Id("contacorrente")).Click();
+            //// Capturando todos os elementos "a" da página.
+            //IReadOnlyCollection<IWebElement> elements = driver.FindElements(By.TagName("a"));
+
+            //var elemento = (from webElemento in elements
+            //                where webElemento.Text.Contains("Detalhes")
+            //                select webElemento).First();
+
+            ////Act
+            //elemento.Click();
+
+            ////Assert
+            //Assert.Contains("Voltar", driver.PageSource);
+
             //Arrange
-            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
-            var login = driver.FindElement(By.Name("Email"));
-            var senha = driver.FindElement(By.Name("Senha"));
-            login.SendKeys("andre@email.com");
-            senha.SendKeys("senha01");
-            driver.FindElement(By.Id("btn-logar")).Click();
-
-            //Conta Corrente
-            driver.FindElement(By.Id("contacorrente")).Click();
-
-            IReadOnlyCollection<IWebElement> elements =
-                driver.FindElements(By.TagName("a"));
-
-            var elemento = (from webElemento in elements
-                            where webElemento.Text.Contains("Detalhes")
-                            select webElemento).First();
+            var loginPO = new LoginPO(driver);
+            loginPO.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             //Act
-            elemento.Click();
+            loginPO.PreencherCampos("andre@email.com", "senha01");
+            loginPO.Logar();
+
+            var homePO = new HomePO(driver);
+            homePO.LinkContaCorrenteClick();
 
             //Assert
-            Assert.Contains("Voltar", driver.PageSource);
+            Assert.Contains("Adicionar Conta-Corrente", driver.PageSource);
         }
     }
 }
