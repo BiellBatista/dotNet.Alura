e833e7f3df3a8a58fb66bafcb02674ee4771151c
Gabriel Batista <gabriel.batista@xpi.com.br>
2022-03-01 22:09:44 -0300

    peparando ambiente para a aula 01 da seção "Testes em .NET: Testes de interface usando Selenium"

---
+++ Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/01-XX-Testes-Interface-Usando-Selenium.Apresentacao/Comandos/ContaCorrenteComando.cs
@@ -0,0 +1,51 @@
+﻿using _01_XX_Testes_Interface_Usando_Selenium.Aplicacao.AplicacaoServico;
+using _01_XX_Testes_Interface_Usando_Selenium.Aplicacao.DTO;
+using _01_XX_Testes_Interface_Usando_Selenium.Dados.Repositorio;
+using _01_XX_Testes_Interface_Usando_Selenium.Domain.Interfaces.Repositorios;
+using _01_XX_Testes_Interface_Usando_Selenium.Domain.Interfaces.Servicos;
+using _01_XX_Testes_Interface_Usando_Selenium.Domain.Services;
+using System.Collections.Generic;
+
+namespace _01_XX_Testes_Interface_Usando_Selenium.Apresentacao.Comandos
+{
+    internal class ContaCorrenteComando
+    {
+        private IContaCorrenteRepositorio _repositorio;
+        private IContaCorrenteServico _servico;
+        private IClienteServico _cliente;
+        private IAgenciaServico _agencia;
+        private ContaCorrenteServicoApp contaCorrenteServicoApp;
+
+        public ContaCorrenteComando()
+        {
+            _repositorio = new ContaCorrenteRepositorio();
+            _servico = new ContaCorrenteServico(_repositorio);
+            contaCorrenteServicoApp = new ContaCorrenteServicoApp(_servico, _agencia, _cliente);
+        }
+
+        public bool Adicionar(ContaCorrenteDTO conta)
+        {
+            return contaCorrenteServicoApp.Adicionar(conta);
+        }
+
+        public bool Atualizar(int id, ContaCorrenteDTO conta)
+        {
+            return contaCorrenteServicoApp.Atualizar(id, conta);
+        }
+
+        public bool Excluir(int id)
+        {
+            return contaCorrenteServicoApp.Excluir(id);
+        }
+
+        public ContaCorrenteDTO ObterPorId(int id)
+        {
+            return contaCorrenteServicoApp.ObterPorId(id);
+        }
+
+        public List<ContaCorrenteDTO> ObterTodos()
+        {
+            return contaCorrenteServicoApp.ObterTodos();
+        }
+    }
+}
---
+++ Extras/Testes-Csharp/07-Testes-Interface-Usando-Selenium/01-XX-Testes-Interface-Usando-Selenium.WebApp/Views/ViewModel/AgenciaViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace _01_XX_Testes_Interface_Usando_Selenium.WebApp.Views.ViewModel
+{
+    public class AgenciaViewModel
+    {
+    }
+}
