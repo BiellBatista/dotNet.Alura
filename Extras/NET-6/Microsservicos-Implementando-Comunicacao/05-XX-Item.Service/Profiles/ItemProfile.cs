e7a285284455ed843379fe27cb56609cc746d5d5
Gabriel Batista <gabrielbatista@bne.com.br>
2022-08-21 22:09:10 -0300

    conclusão da aula 05 da seção "Microsserviços e .NET6: implementando a comunicação"

--- Extras/NET-6/Microsservicos-Implementando-Comunicacao/05-XX-Item.Service/Profiles/ItemProfile.cs
+++ Extras/NET-6/Microsservicos-Implementando-Comunicacao/05-XX-Item.Service/Profiles/ItemProfile.cs
@@ -8,6 +8,9 @@ public class ItemProfile : Profile
 {
     public ItemProfile()
     {
+        CreateMap<RestauranteReadDto, Restaurante>()
+            .ForMember(dest => dest.IdExterno, opt => opt.MapFrom(ori => ori.Id));
+
         CreateMap<Restaurante, RestauranteReadDto>();
         CreateMap<ItemCreateDto, Item>();
         CreateMap<Item, ItemCreateDto>();
