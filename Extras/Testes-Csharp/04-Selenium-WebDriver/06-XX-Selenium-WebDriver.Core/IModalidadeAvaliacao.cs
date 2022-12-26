2507870cdede63cc0ebdf8159289e67bc39f0c79
Gabriel Batista <gabrielbatista@bne.com.br>
2022-12-26 17:34:15 -0300

    movimentando projetos de extra para alguma formação

--- Extras/Testes-Csharp/04-Selenium-WebDriver/06-XX-Selenium-WebDriver.Core/IModalidadeAvaliacao.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _06_XX_Selenium_WebDriver.Core
-{
-    public interface IModalidadeAvaliacao
-    {
-        Lance Avalia(Leilao leilao);
-    }
-}
--- Extras/Testes-Csharp/04-Selenium-WebDriver/06-XX-Selenium-WebDriver.WebApp/Models/PesquisaLeiloesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _06_XX_Selenium_WebDriver.WebApp.Models
-{
-    public class PesquisaLeiloesViewModel
-    {
-        public string Termo { get; set; }
-        public string[] Categorias { get; set; }
-        public string Andamento { get; set; }
-    }
-}
