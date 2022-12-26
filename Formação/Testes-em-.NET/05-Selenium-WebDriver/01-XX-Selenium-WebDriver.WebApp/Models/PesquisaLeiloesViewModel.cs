947b3fb99548c7d768321d8cc7fe9fe42ba8cd39
Gabriel Batista <gabrielbatista@bne.com.br>
2022-12-26 18:55:40 -0300

    movimentando projetos de extra para alguma formação

---
+++ Formação/Testes-em-.NET/05-Selenium-WebDriver/01-XX-Selenium-WebDriver.WebApp/Models/PesquisaLeiloesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_XX_Selenium_WebDriver.WebApp.Models
+{
+    public class PesquisaLeiloesViewModel
+    {
+        public string Termo { get; set; }
+        public string[] Categorias { get; set; }
+        public string Andamento { get; set; }
+    }
+}
---
+++ Formação/Testes-em-.NET/05-Selenium-WebDriver/02-XX-Selenium-WebDriver.Dados/RepositorioLeilao.cs
@@ -0,0 +1,19 @@
+﻿using _02_XX_Selenium_WebDriver.Core;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace _02_XX_Selenium_WebDriver.Dados
+{
+    public class RepositorioLeilao : RepositorioBase<Leilao>
+    {
+        public RepositorioLeilao(LeiloesContext context): base(context) { }
+
+        public override Leilao BuscarPorId(int id)
+        {
+            return _ctx.Leiloes
+                .Include(l => l.Lances)
+                .Include(l => l.Seguidores)
+                .FirstOrDefault(l => l.Id == id);
+        }
+    }
+}
