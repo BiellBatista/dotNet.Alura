63d008f67c0dfddaf618c7aaa6e6782eb55fafe6
Gabriel Batista <gabrielbatista@bne.com.br>
2023-01-01 14:33:28 -0300

    peparando ambiente para a aula 04 da seção ".NET 6: criando uma web API"

---
+++ Formação/ASP.Net-Core/02-NET-6-criando-uma-web-API/04-XX-Filmes.API/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using _04_XX_Filmes.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _04_XX_Filmes.API.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
