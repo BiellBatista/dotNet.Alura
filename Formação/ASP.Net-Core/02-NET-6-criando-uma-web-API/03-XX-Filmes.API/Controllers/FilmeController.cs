8875dbd6fc99c50cbb75283c0d8410c9908bd2a5
Gabriel Batista <gabrielbatista@bne.com.br>
2023-01-01 13:39:43 -0300

    peparando ambiente para a aula 03 da seção ".NET 6: criando uma web API"

---
+++ Formação/ASP.Net-Core/02-NET-6-criando-uma-web-API/03-XX-Filmes.API/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using _03_XX_Filmes.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_XX_Filmes.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
