bab62446b575e3f5384fb9169833fea224374d4d
Gabriel Batista <gabrielbatista@bne.com.br>
2023-01-01 19:50:08 -0300

    conclusão da aula 01 da seção "Build de uma aplicação .NET: construindo seus projetos com MSBuild"

--- Extras/Cursos/Build-aplicação-NET-MSBuild/01-XX-Build-aplicação-NET-MSBuild/Compile.bat
+++ Extras/Cursos/Build-aplicação-NET-MSBuild/01-XX-Build-aplicação-NET-MSBuild/Compile.bat
@@ -1,2 +1,2 @@
 mkdir bin
-csc -out:bin\Tarefas.exe Listatarefas.cs Tarefa.cs Categoria.cs -> cria a IL dos 3 arquivos .cs dentro do Tarefas.exe
+csc -out:bin\Tarefas.exe Listatarefas.cs Tarefa.cs Categoria.cs
