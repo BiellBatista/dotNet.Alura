b4dcfb47de0b096fe1dce489004cb1f2aff48a12
Gabriel Batista <gabrielbatista@bne.com.br>
2023-02-12 20:01:51 -0300

    peparando ambiente para a aula 01 da seção ".NET 6: relacionando entidades"

---
+++ Formação/ASP.Net-Core/03-NET-6-relacionando-entidades/01-XX-Filmes.API/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using _01_XX_Filmes.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _01_XX_Filmes.API.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
---
+++ Formação/ASP.Net-Core/03-NET-6-relacionando-entidades/01-XX-Filmes.API/Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using _01_XX_Filmes.API.Data.Dtos;
+using _01_XX_Filmes.API.Models;
+using AutoMapper;
+
+namespace _01_XX_Filmes.API.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+}
