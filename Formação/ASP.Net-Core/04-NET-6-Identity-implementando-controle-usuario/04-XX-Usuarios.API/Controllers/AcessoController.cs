53f7c30f68e31300654a16a3177301eb36e33a27
Gabriel Batista <gabriel.texteis@gmail.com>
2023-06-11 21:41:10 -0300

    conclusão da aula 04 da seção ".NET 6 e Identity: implementando controle de usuário"

---
+++ Formação/ASP.Net-Core/04-NET-6-Identity-implementando-controle-usuario/04-XX-Usuarios.API/Controllers/AcessoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04_XX_Usuarios.API.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class AcessoController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "IdadeMinima")]
+    public IActionResult Get()
+    {
+        return Ok("Acesso permitido!");
+    }
+}
