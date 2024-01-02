f24fc0e165cd5b7fbf125ea8a5ef2f59570692e3
Gabriel Batista <gabriel.texteis@gmail.com>
2024-01-02 20:23:51 -0300

    conclusão da aula 02 da seção ".NET: gerenciamento de memória para otimização de performance"

--- Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/02-XX-Lists-LinkedLists/UsuarioLib/Usuario.cs
+++ Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/02-XX-Lists-LinkedLists/UsuarioLib/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace _02_XX_Lists_LinkedLists.UsuarioLib;
+﻿using System.Diagnostics;
+
+namespace _02_XX_Lists_LinkedLists.UsuarioLib;
 
 public class Usuario
 {
@@ -7,12 +9,40 @@ public class Usuario
         Nome = nome;
         Email = email;
         Telefones = telefone;
+        ChavesDeAcesso = new List<Guid>(new Guid[10]);
+
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        for (int i = 0; i < 1000000; i++)
+            ChavesDeAcesso.Insert(1, Guid.NewGuid());
+
+        stopwatch.Stop();
+        Console.WriteLine($"Tempo total em ms: {stopwatch.Elapsed.TotalMilliseconds}");
+
+        ChavesDeAcessoLinkedList = [];
+        Stopwatch stopwatchLinkedList = new();
+
+        ChavesDeAcessoLinkedList.AddFirst(Guid.NewGuid());
+        stopwatchLinkedList.Start();
+
+        for (int i = 0; i < 1000000; i++)
+            _ = ChavesDeAcessoLinkedList.AddAfter(ChavesDeAcessoLinkedList.First!, Guid.NewGuid());
+
+        stopwatchLinkedList.Stop();
+
+        Console.WriteLine($"Tempo total LinkedList em ms: {stopwatchLinkedList.Elapsed.TotalMilliseconds}");
+
+        foreach (Guid i in ChavesDeAcessoLinkedList)
+            Console.WriteLine(i);
     }
 
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
     public List<string> Telefones { get; set; }
+    public List<Guid> ChavesDeAcesso { get; set; }
+    public LinkedList<Guid> ChavesDeAcessoLinkedList { get; set; }
 
     public void PadronizaTelefones()
     {
