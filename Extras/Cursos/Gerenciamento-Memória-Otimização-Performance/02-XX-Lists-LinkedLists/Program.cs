d48e3077ecb5cec82f4ebdd56abf4cef7b68b815
Gabriel Batista <gabriel.texteis@gmail.com>
2024-01-02 18:45:21 -0300

    peparando ambiente para a aula 02 da seção ".NET: gerenciamento de memória para otimização de performance"

---
+++ Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/02-XX-Lists-LinkedLists/Program.cs
@@ -0,0 +1,22 @@
+﻿using _02_XX_Lists_LinkedLists.UsuarioLib;
+
+Usuario usuario =
+    new Usuario(
+        "Daniel",
+        "daniel@email.com",
+        new List<string>() { "12345678" });
+
+Usuario usuario2 =
+    new Usuario(
+        "Luis",
+        "luis@email.com",
+        new List<string>() { "87654321" });
+
+//12345678
+usuario.ExibeTelefones();
+
+//efetuar a padronização
+usuario.PadronizaTelefones();
+
+//912345678
+usuario.ExibeTelefones();
---
+++ Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/02-XX-Lists-LinkedLists/UsuarioLib/Coordenada.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace _02_XX_Lists_LinkedLists.UsuarioLib;
+
+public class Coordenada
+{
+    public Coordenada(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public double Latitude;
+    public double Longitude;
+
+    public override string ToString()
+    {
+        return $"{Latitude.ToString(CultureInfo.InvariantCulture)}, {Longitude.ToString(CultureInfo.InvariantCulture)}";
+    }
+}
