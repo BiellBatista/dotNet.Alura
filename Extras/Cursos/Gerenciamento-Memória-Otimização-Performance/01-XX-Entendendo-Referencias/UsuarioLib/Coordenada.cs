6006377431f5b6f98456d1f2bee5acb6786da48d
Gabriel Batista <gabriel.texteis@gmail.com>
2024-01-02 18:02:41 -0300

    peparando ambiente para a aula 01 da seção ".NET: gerenciamento de memória para otimização de performance"

---
+++ Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/01-XX-Entendendo-Referencias/UsuarioLib/Coordenada.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace _01_XX_Entendendo_Referencias.UsuarioLib;
+
+public class Coordenada
+{
+    public Coordenada(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public double Latitude;
+    public double Longitude;
+
+    public override string ToString()
+    {
+        return $"{Latitude.ToString(CultureInfo.InvariantCulture)}, {Longitude.ToString(CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ Extras/Cursos/Gerenciamento-Memória-Otimização-Performance/01-XX-Entendendo-Referencias/UsuarioLib/Usuario.cs
@@ -0,0 +1,30 @@
+﻿namespace _01_XX_Entendendo_Referencias.UsuarioLib;
+
+public class Usuario
+{
+    public Usuario(string nome, string email, List<string> telefone)
+    {
+        Nome = nome;
+        Email = email;
+        Telefones = telefone;
+    }
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public List<string> Telefones { get; set; }
+
+    public void PadronizaTelefones()
+    {
+        Telefones = Telefones.Select(telefone =>
+            telefone.Length == 8 ?
+            telefone = "9" + telefone :
+            telefone
+        ).ToList();
+    }
+
+    public void ExibeTelefones()
+    {
+        Telefones.ForEach(telefone => Console.WriteLine(telefone));
+    }
+}
