9519c1f1b4bc7f974e1d1f0480fb28e4509b3d3d
Gabriel Batista <gabriel.texteis@gmail.com>
2024-02-25 18:17:29 -0300

    peparando ambiente para a aula 04 da seção ".NET: persistindo dados com Entity Framework Core"

---
+++ Formação/Csharp-Web-Crie-Aplicações-Usando-ASP.NET/01-04-XX-Migrations/Menus/MenuSair.cs
@@ -0,0 +1,12 @@
+﻿using _01_04_XX_Migrations.Banco;
+using _01_04_XX_Migrations.Modelos;
+
+namespace _01_04_XX_Migrations.Menus;
+
+internal class MenuSair : Menu
+{
+    public override void Executar(DAL<Artista> artistaDAL)
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
---
+++ Formação/Csharp-Web-Crie-Aplicações-Usando-ASP.NET/01-04-XX-Migrations/Modelos/Musica.cs
@@ -0,0 +1,24 @@
+﻿namespace _01_04_XX_Migrations.Modelos;
+
+internal class Musica
+{
+    public Musica(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; set; }
+    public int Id { get; set; }
+    public int? AnoLancamento { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+    }
+
+    public override string ToString()
+    {
+        return @$"Id: {Id}
+        Nome: {Nome}";
+    }
+}
