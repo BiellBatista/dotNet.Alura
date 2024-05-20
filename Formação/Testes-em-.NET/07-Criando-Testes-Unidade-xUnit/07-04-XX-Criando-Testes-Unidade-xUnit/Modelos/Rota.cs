0167db7ec63fb76e76aa05882a3afd74aeaaea1d
Gabriel Batista <gabriel.texteis@gmail.com>
2024-05-20 02:13:04 -0300

    peparando ambiente para a aula 04 da seção "Testes em .NET: criando testes de unidade com xUnit"

---
+++ Formação/Testes-em-.NET/07-Criando-Testes-Unidade-xUnit/07-04-XX-Criando-Testes-Unidade-xUnit/Modelos/Rota.cs
@@ -0,0 +1,28 @@
+﻿using _07_04_XX_Criando_Testes_Unidade_xUnit.Validador;
+
+namespace _07_04_XX_Criando_Testes_Unidade_xUnit.Modelos;
+
+public class Rota : Valida
+{
+    public int Id { get; set; }
+    public string Origem { get; set; }
+    public string Destino { get; set; }
+
+    public Rota(string origem, string destino)
+    {
+        Origem = origem;
+        Destino = destino;
+    }
+
+    protected override void Validar()
+    {
+        if (Origem is null || Origem.Equals(string.Empty))
+        {
+            Erros.RegistrarErro("A rota não pode possuir uma origem nula ou vazia.");
+        }
+        else if (Destino is null || Destino.Equals(string.Empty))
+        {
+            Erros.RegistrarErro("A rota não pode possuir um destino nulo ou vazio.");
+        }
+    }
+}
---
+++ Formação/Testes-em-.NET/07-Criando-Testes-Unidade-xUnit/07-04-XX-Criando-Testes-Unidade-xUnit/Validador/Valida.cs
@@ -0,0 +1,10 @@
+﻿namespace _07_04_XX_Criando_Testes_Unidade_xUnit.Validador;
+
+public abstract class Valida : IValidavel
+{
+    private readonly Erros erros = new();
+    public bool EhValido => erros.Count() == 0;
+    public Erros Erros => erros;
+
+    protected abstract void Validar();
+}
