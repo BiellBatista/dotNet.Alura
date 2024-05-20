7e3e8e07a84072db5136655461010cd56a467b1f
Gabriel Batista <gabriel.texteis@gmail.com>
2024-05-20 01:05:52 -0300

    peparando ambiente para a aula 02 da seção "Testes em .NET: criando testes de unidade com xUnit"

---
+++ Formação/Testes-em-.NET/07-Criando-Testes-Unidade-xUnit/07-02-XX-Criando-Testes-Unidade-xUnit/Modelos/Periodo.cs
@@ -0,0 +1,24 @@
+﻿using _07_02_XX_Criando_Testes_Unidade_xUnit.Validador;
+
+namespace _07_02_XX_Criando_Testes_Unidade_xUnit.Modelos;
+
+public class Periodo : Valida
+{
+    public DateTime DataInicial { get; set; }
+    public DateTime DataFinal { get; set; }
+
+    public Periodo(DateTime dataInicial, DateTime dataFinal)
+    {
+        DataInicial = dataInicial;
+        DataFinal = dataFinal;
+        Validar();
+    }
+
+    protected override void Validar()
+    {
+        if (DataInicial > DataFinal)
+        {
+            Erros.RegistrarErro("Erro: Data de ida não pode ser maior que a data de volta.");
+        }
+    }
+}
---
+++ Formação/Testes-em-.NET/07-Criando-Testes-Unidade-xUnit/07-02-XX-Criando-Testes-Unidade-xUnit/Modelos/ValidaInformacoes.cs
@@ -0,0 +1,5 @@
+﻿namespace _07_02_XX_Criando_Testes_Unidade_xUnit.Modelos;
+
+internal class ValidaInformacoes
+{
+}
