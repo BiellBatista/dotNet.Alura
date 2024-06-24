e8d8b8a8db483044947aeec4bdfc26cfc7071cb4
Gabriel Batista <gabriel.texteis@gmail.com>
2024-06-23 21:47:10 -0300

    peparando ambiente para a aula 01 da seção "Testes em .NET: testando integração com banco de dados"

---
+++ Formação/Testes-em-.NET-02/08-Testando-Integracao-Banco-Dados/08-01-XX-Definindo-Testes-Integracao/Modelos/Periodo.cs
@@ -0,0 +1,24 @@
+﻿using _08_01_XX_Definindo_Testes_Integracao.Validador;
+
+namespace _08_01_XX_Definindo_Testes_Integracao.Modelos;
+
+public class Periodo : Valida
+{
+    public DateTime DataInicial { get; set; }
+    public DateTime DataFinal { get; set; }
+
+    public Periodo(DateTime dataInicial, DateTime dataFinal)
+    {
+        DataInicial = dataInicial;
+        DataFinal = dataFinal;
+        Validar();
+    }
+
+    protected override void Validar()
+    {
+        if (DataInicial > DataFinal)
+        {
+            Erros.RegistrarErro("Erro: Data de ida não pode ser maior que a data de volta.");
+        }
+    }
+}
