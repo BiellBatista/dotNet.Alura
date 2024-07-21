e208506dc0cfd0bd97daa67525e48fe922f26f42
Gabriel Batista <gabriel.texteis@gmail.com>
2024-07-21 20:32:28 -0300

    conclusão do módulo 02 da aula 02 da seção "C# Reflection: manipule dinamicamente tipos e assemblies"

---
+++ Extras/Cursos/Manipule-Dinamicamente-Tipos-Assemblies/02-XX-Lendo-Objetos-Dinamicamente.Common/BoletosPorCedente.cs
@@ -0,0 +1,17 @@
+﻿namespace _02_XX_Lendo_Objetos_Dinamicamente.Common
+{
+    public class BoletosPorCedente
+    {
+        public string CedenteNome { get; set; }
+
+        public string CedenteCpfCnpj { get; set; }
+
+        public string CedenteAgencia { get; set; }
+
+        public string CedenteConta { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
---
+++ Extras/Cursos/Manipule-Dinamicamente-Tipos-Assemblies/02-XX-Lendo-Objetos-Dinamicamente.Common/NomeColunaAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace _02_XX_Lendo_Objetos_Dinamicamente.Common
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NomeColunaAttribute : Attribute
+    {
+        public string Header { get; }
+
+        public NomeColunaAttribute(string header)
+        {
+            Header = header;
+        }
+    }
+}
