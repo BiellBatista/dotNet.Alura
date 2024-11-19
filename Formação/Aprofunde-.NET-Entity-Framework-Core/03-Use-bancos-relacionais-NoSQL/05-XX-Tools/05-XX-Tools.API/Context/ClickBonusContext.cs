d2e80e2ec921bd0f8f366ea266069bd94409f121
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-18 23:54:47 -0300

    preparando ambiente para a aula 05 da seção "Persistência de dados em .NET: use bancos relacionais e NoSQL"

---
+++ Formação/Aprofunde-.NET-Entity-Framework-Core/03-Use-bancos-relacionais-NoSQL/05-XX-Tools/05-XX-Tools.API/Context/ClickBonusContext.cs
@@ -0,0 +1,36 @@
+﻿using _05_XX_Tools.API.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace _05_XX_Tools.API.Context;
+
+public partial class ClickBonusContext : DbContext
+{
+    public ClickBonusContext()
+    {
+    }
+
+    public ClickBonusContext(DbContextOptions<ClickBonusContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Oferta> Ofertas { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Oferta>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Ofertas__3214EC0795CD1321");
+
+            entity.Property(e => e.DataCriacao).HasDefaultValueSql("(getdate())");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    private partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Formação/Aprofunde-.NET-Entity-Framework-Core/03-Use-bancos-relacionais-NoSQL/05-XX-Tools/05-XX-Tools.API/Program.cs
@@ -0,0 +1,35 @@
+using _05_XX_Tools.API.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ClickBonusContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () =>
+{
+    return Results.Ok("Click Bonus! Cadastra-se e concorra.");
+})
+.WithTags("helloworld")
+.WithOpenApi();
+
+app.MapGet("/api/usuarios", async ([FromServices] ClickBonusContext context) =>
+{
+    var usuarios = await context.Usuarios.ToListAsync();
+    return Results.Ok(usuarios);
+});
+
+app.Run();
