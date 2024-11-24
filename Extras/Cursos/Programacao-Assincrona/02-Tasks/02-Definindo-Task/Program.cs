673724a863fa8ece457a181283b6a50fd3d6979d
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 18:22:47 -0300

    preparando ambiente para o módulo 02 da aula 02 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/02-Tasks/02-Definindo-Task/Program.cs
@@ -0,0 +1,37 @@
+﻿static void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt");
+
+    Thread.Sleep(new Random().Next(300, 8000));
+
+    Console.WriteLine($"Conteúdo: \n{conteudo}");
+}
+
+static void ExibirRelatorio()
+{
+    Console.WriteLine("Executando relatório de compra de passagens!");
+
+    Thread.Sleep(new Random().Next(300, 8000));
+}
+
+// permite executar um trecho de código em paralelo
+var thread1 = new Thread(() => LerArquivo());
+// permite executar um trecho de código em paralelo
+var thread2 = new Thread(() => ExibirRelatorio());
+
+void InicializarThreads()
+{
+    //disparando a thread e executando o trecho
+    thread1.Start();
+
+    while (thread1.IsAlive)
+        Console.WriteLine("Thread1 em execução.");
+
+    //disparando a thread e executando o trecho
+    thread2.Start();
+}
+
+InicializarThreads();
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
