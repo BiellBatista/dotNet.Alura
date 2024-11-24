a6ca832e02c2288ccd746185763e2430fd4e5619
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 18:18:51 -0300

    conclusão do módulo 06 da aula 01 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

--- Extras/Cursos/Programacao-Assincrona/01-Threads/06-Multiplas-Threads/Program.cs
+++ Extras/Cursos/Programacao-Assincrona/01-Threads/06-Multiplas-Threads/Program.cs
@@ -2,12 +2,36 @@
 {
     var conteudo = File.ReadAllText("voos.txt");
 
+    Thread.Sleep(new Random().Next(300, 8000));
+
     Console.WriteLine($"Conteúdo: \n{conteudo}");
 }
+
+static void ExibirRelatorio()
+{
+    Console.WriteLine("Executando relatório de compra de passagens!");
+
+    Thread.Sleep(new Random().Next(300, 8000));
+}
+
 // permite executar um trecho de código em paralelo
-var thread = new Thread(() => LerArquivo());
-//disparando a thread e executando o trecho
-thread.Start();
+var thread1 = new Thread(() => LerArquivo());
+// permite executar um trecho de código em paralelo
+var thread2 = new Thread(() => ExibirRelatorio());
+
+void InicializarThreads()
+{
+    //disparando a thread e executando o trecho
+    thread1.Start();
+
+    while (thread1.IsAlive)
+        Console.WriteLine("Thread1 em execução.");
+
+    //disparando a thread e executando o trecho
+    thread2.Start();
+}
+
+InicializarThreads();
 
 Console.WriteLine("Outras operações.");
 Console.ReadKey();
