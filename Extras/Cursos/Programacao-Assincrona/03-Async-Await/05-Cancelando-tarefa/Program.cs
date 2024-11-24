2d1aa8b153e0549ff00d5e244fb52831729b1cb3
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 20:07:18 -0300

    conclusão do módulo 05 da aula 03 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

--- Extras/Cursos/Programacao-Assincrona/03-Async-Await/05-Cancelando-tarefa/Program.cs
+++ Extras/Cursos/Programacao-Assincrona/03-Async-Await/05-Cancelando-tarefa/Program.cs
@@ -1,13 +1,19 @@
 ﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
 
 // uma função async sempre deve retornar uma Task<T>
-async Task LerArquivoAsync()
+async Task LerArquivoAsync(CancellationToken token)
 {
     try
     {
         await Task.Delay(new Random().Next(300, 8000));
 
         Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+
+        token.ThrowIfCancellationRequested();
+    }
+    catch (OperationCanceledException ex)
+    {
+        Console.WriteLine($"Tarefa cancelada: {ex.Message}");
     }
     catch (AggregateException ex)
     {
@@ -16,14 +22,26 @@ async Task LerArquivoAsync()
 }
 
 // uma função async sempre deve retornar uma Task<T>
-static async Task ExibirRelatorioAsync()
+static async Task ExibirRelatorioAsync(CancellationToken token)
 {
-    Console.WriteLine("Executando relatório de compra de passagens!");
+    try
+    {
+        Console.WriteLine("Executando relatório de compra de passagens!");
 
-    await Task.Delay(new Random().Next(300, 8000));
+        await Task.Delay(new Random().Next(300, 8000));
+
+        token.ThrowIfCancellationRequested();
+    }
+    catch (OperationCanceledException ex)
+    {
+        Console.WriteLine($"Tarefa cancelada: {ex.Message}");
+    }
 }
 
-await Task.WhenAll(LerArquivoAsync(), ExibirRelatorioAsync());
+var tokenDeCancelamento = new CancellationTokenSource();
+var tarefa = Task.WhenAll(LerArquivoAsync(tokenDeCancelamento.Token), ExibirRelatorioAsync(tokenDeCancelamento.Token));
+
+await Task.Delay(1000).ContinueWith(_ => tokenDeCancelamento.Cancel());
 
 Console.WriteLine("Outras operações.");
 Console.ReadKey();
