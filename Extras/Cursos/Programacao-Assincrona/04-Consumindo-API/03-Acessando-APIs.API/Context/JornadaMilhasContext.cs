7312b5096009b45a6c3520f2fb5e7576dfe43495
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 20:45:20 -0300

    preparando ambiente para o módulo 03 da aula 04 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/04-Consumindo-API/03-Acessando-APIs.API/Context/JornadaMilhasContext.cs
@@ -0,0 +1,13 @@
+﻿using _03_Acessando_APIs.API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace _03_Acessando_APIs.API.Context;
+
+public class JornadaMilhasContext : DbContext
+{
+    public JornadaMilhasContext(DbContextOptions<JornadaMilhasContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Voo> Voos { get; set; }
+}
