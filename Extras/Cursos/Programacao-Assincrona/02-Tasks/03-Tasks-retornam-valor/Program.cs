dcdbab82b0009eb6558b3c59a433104ae14300ca
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 18:39:41 -0300

    conclusão do módulo 03 da aula 02 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

--- Extras/Cursos/Programacao-Assincrona/02-Tasks/03-Tasks-retornam-valor/Program.cs
+++ Extras/Cursos/Programacao-Assincrona/02-Tasks/03-Tasks-retornam-valor/Program.cs
@@ -1,10 +1,10 @@
-﻿static void LerArquivo()
-{
-    var conteudo = File.ReadAllText("voos.txt");
+﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllText("voos.txt"));
 
+void LerArquivo()
+{
     Task.Delay(new Random().Next(300, 8000));
 
-    Console.WriteLine($"Conteúdo: \n{conteudo}");
+    Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
 }
 
 static void ExibirRelatorio()
@@ -14,9 +14,9 @@ static void ExibirRelatorio()
     Task.Delay(new Random().Next(300, 8000));
 }
 // executa uma tarefa em paralelo (encapsula toda complexidade de trabalhar com thread)
-var task1 = Task.Run(() => LerArquivo());
+Task task1 = Task.Run(() => LerArquivo());
 // executa uma tarefa em paralelo (encapsula toda complexidade de trabalhar com thread)
-var task2 = Task.Run(() => ExibirRelatorio());
+Task task2 = Task.Run(() => ExibirRelatorio());
 
 Console.WriteLine("Outras operações.");
 Console.ReadKey();
