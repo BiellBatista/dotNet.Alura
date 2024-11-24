1172ac07abbc87c14ab3eaefc1fb6ef01d7cd24d
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 17:56:27 -0300

    preparando ambiente para o módulo 04 da aula 01 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/01-Threads/04-Criando-Thread/Program.cs
@@ -0,0 +1,11 @@
+﻿void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt");
+
+    Console.WriteLine($"Conteúdo: \n{conteudo}");
+}
+
+LerArquivo();
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
