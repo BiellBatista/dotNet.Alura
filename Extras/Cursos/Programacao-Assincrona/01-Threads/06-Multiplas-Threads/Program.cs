89341d5df70ef5bd96ef440357896cc3fbaf4e90
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 18:04:15 -0300

    preparando ambiente para o módulo 06 da aula 01 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/01-Threads/06-Multiplas-Threads/Program.cs
@@ -0,0 +1,13 @@
+﻿static void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt");
+
+    Console.WriteLine($"Conteúdo: \n{conteudo}");
+}
+// permite executar um trecho de código em paralelo
+var thread = new Thread(() => LerArquivo());
+//disparando a thread e executando o trecho
+thread.Start();
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
