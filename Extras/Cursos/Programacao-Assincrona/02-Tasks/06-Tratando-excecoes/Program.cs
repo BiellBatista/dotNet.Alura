5b53dba3f937c81acfd3cab0526783ed32f74d9c
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 18:41:21 -0300

    preparando ambiente para o módulo 06 da aula 02 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/02-Tasks/06-Tratando-excecoes/Program.cs
@@ -0,0 +1,22 @@
+﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllText("voos.txt"));
+
+void LerArquivo()
+{
+    Task.Delay(new Random().Next(300, 8000));
+
+    Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+}
+
+static void ExibirRelatorio()
+{
+    Console.WriteLine("Executando relatório de compra de passagens!");
+
+    Task.Delay(new Random().Next(300, 8000));
+}
+// executa uma tarefa em paralelo (encapsula toda complexidade de trabalhar com thread)
+Task task1 = Task.Run(() => LerArquivo());
+// executa uma tarefa em paralelo (encapsula toda complexidade de trabalhar com thread)
+Task task2 = Task.Run(() => ExibirRelatorio());
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
