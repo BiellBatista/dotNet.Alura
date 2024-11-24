e78aa0f65950b432b73a2d0511e4a83431820e86
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 19:46:56 -0300

    preparando ambiente para o módulo 03 da aula 03 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/03-Async-Await/03-Async-void/Program.cs
@@ -0,0 +1,28 @@
+﻿Task<string> conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
+
+async void LerArquivoAsync()
+{
+    try
+    {
+        await Task.Delay(new Random().Next(300, 8000));
+
+        Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+    }
+    catch (AggregateException ex)
+    {
+        Console.WriteLine($"Aconteceu o erro: {ex.InnerException?.Message}");
+    }
+}
+
+static async void ExibirRelatorioAsync()
+{
+    Console.WriteLine("Executando relatório de compra de passagens!");
+
+    await Task.Delay(new Random().Next(300, 8000));
+}
+
+LerArquivoAsync();
+ExibirRelatorioAsync();
+
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
