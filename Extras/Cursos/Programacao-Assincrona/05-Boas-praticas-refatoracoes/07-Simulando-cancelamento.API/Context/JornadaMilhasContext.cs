88e55b95cc61f9426d085aa9ccb0ef10b885f673
Gabriel Batista <gabriel.texteis@gmail.com>
2024-11-24 23:12:00 -0300

    preparando ambiente para o módulo 07 da aula 05 do curso "C# Async: aplique recursos de programação assíncrona em seu projetos"

---
+++ Extras/Cursos/Programacao-Assincrona/05-Boas-praticas-refatoracoes/07-Simulando-cancelamento.API/Context/JornadaMilhasContext.cs
@@ -0,0 +1,13 @@
+﻿using _07_Simulando_cancelamento.API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace _07_Simulando_cancelamento.API.Context;
+
+public class JornadaMilhasContext : DbContext
+{
+    public JornadaMilhasContext(DbContextOptions<JornadaMilhasContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Voo> Voos { get; set; }
+}
