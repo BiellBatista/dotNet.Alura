680bd71935ff4e3cfdb1cad8076df4265832ece5
Gabriel Batista <gabriel.texteis@gmail.com>
2025-02-09 18:47:34 -0300

    conclusão do módulo 02 da aula 03 do curso "C#: Eventos, Delegates e Lambdas"

---
+++ Formação/Topicos-avancados-em-.NET/05-Eventos-Delegates-Lambdas/03-Eventos-formularios-controles/02-Eventos-Windows-Forms.CaixaEletronico/SaldoEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace _02_Eventos_Windows_Forms.CaixaEletronico
+{
+    public class SaldoEventArgs : EventArgs
+    {
+        public decimal Saldo { get; }
+
+        public SaldoEventArgs(decimal saldo)
+        {
+            Saldo = saldo;
+        }
+    }
+}
---
+++ Formação/Topicos-avancados-em-.NET/05-Eventos-Delegates-Lambdas/03-Eventos-formularios-controles/02-Eventos-Windows-Forms/Program.cs
@@ -0,0 +1,19 @@
+using WinFormsApp1;
+
+namespace _02_Eventos_Windows_Forms
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
