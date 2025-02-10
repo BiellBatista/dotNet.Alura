c735abba68c8217cec34940a9b9e0f5949757122
Gabriel Batista <gabriel.texteis@gmail.com>
2025-02-10 00:19:06 -0300

    conclusão do módulo 03 da aula 04 do curso "C#: Eventos, Delegates e Lambdas"

---
+++ Formação/Topicos-avancados-em-.NET/05-Eventos-Delegates-Lambdas/04-Introducao-Lambdas/03-Lambdas-expressao-instrucao/ICaixaEletronico.cs
@@ -0,0 +1,17 @@
+﻿namespace _03_Lambdas_expressao_instrucao
+{
+    public interface ICaixaEletronico
+    {
+        decimal Saldo { get; }
+
+        void Depositar(decimal valor);
+
+        void Sacar(decimal valor);
+
+        string Extrato();
+
+        event SaldoInsuficienteEventHandler OnSaldoInsuficiente;
+
+        event LimiteUtilizadoEventHandler OnLimiteUtilizado;
+    }
+}
---
+++ Formação/Topicos-avancados-em-.NET/05-Eventos-Delegates-Lambdas/04-Introducao-Lambdas/03-Lambdas-expressao-instrucao/SaldoEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace _03_Lambdas_expressao_instrucao
+{
+    public class SaldoEventArgs : EventArgs
+    {
+        public decimal Saldo { get; }
+
+        public SaldoEventArgs(decimal saldo)
+        {
+            Saldo = saldo;
+        }
+    }
+}
