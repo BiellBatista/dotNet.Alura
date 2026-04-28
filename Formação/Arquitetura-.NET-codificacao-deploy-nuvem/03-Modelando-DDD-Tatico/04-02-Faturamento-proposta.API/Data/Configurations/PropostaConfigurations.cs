2dea7f87a676321a6e340604bbcbeb6d9fdc7ea3
Gabriel Batista <gabriel.texteis@gmail.com>
2026-04-28 00:57:46 -0300

    fix

---
+++ Formação/Arquitetura-.NET-codificacao-deploy-nuvem/03-Modelando-DDD-Tatico/04-02-Faturamento-proposta.API/Data/Configurations/PropostaConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ContainRs.Api.Data.Configurations;
+
+public class PropostaConfigurations : IEntityTypeConfiguration<Proposta>
+{
+    public void Configure(EntityTypeBuilder<Proposta> builder)
+    {
+        builder.OwnsOne(p => p.ValorTotal, builder =>
+        {
+            builder.Property(v => v.Valor)
+            .HasColumnType("decimal(18,2)");
+        }); 
+
+        builder.OwnsOne(p => p.Situacao, status =>
+        {
+            status.Property(s => s.Status)
+                .HasColumnName("Status")
+                .HasConversion<string>();
+        });
+
+        builder.HasOne(p => p.Solicitacao)
+            .WithMany(s => s.Propostas)
+            .HasForeignKey(p => p.SolicitacaoId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
